e402f1fa645f99eb4c2c627344d2c23db66aa258
Miguel Ángel Florido Cifuentes <miguel.mangel.ez11@gmail.com>
2021-03-09 12:42:53 +0100

    Fixed lint code

--- examples/firstApp/next/pages/_app.js
+++ examples/firstApp/next/pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import propTypes from "prop-types";
+import React from "react";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, }) {
   return <Component {...pageProps} />
 }
+
+MyApp.propTypes = {
+  Component: propTypes.element.isRequired,
+  pageProps: propTypes.object.isRequired,
+};
 
 export default MyApp
--- examples/firstApp/next/pages/index.js
+++ examples/firstApp/next/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import React from "react";
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
