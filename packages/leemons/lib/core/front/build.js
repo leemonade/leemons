ce6a60b23303c4ef4277dcfced62d9811f071585
Miguel Ángel Florido Cifuentes <miguel.mangel.ez11@gmail.com>
2021-04-19 18:05:44 +0200

    When an error occurs installing deps or building front, try it again on next execution

--- packages/leemons/lib/core/front/build.js
+++ packages/leemons/lib/core/front/build.js
@@ -7,51 +7,68 @@ const hooks = require('leemons-hooks');
 /*
  * Global Next.js Dependencies Installation
  */
-function frontDeps() {
-  const installSpinner = ora('Installing frontend dependencies').start();
-  return execa
-    .command(`yarn --cwd ${leemons.dir.next} --force`)
-    .then(() => {
-      installSpinner.succeed('Frontend dependencies installed');
-    })
-    .catch((e) => {
-      installSpinner.fail("Frontend dependencies can't be installed");
-      throw e;
-    });
+async function frontDeps() {
+  // Check if the dependencies are installed
+  if (!fs.existsSync(path.resolve(leemons.dir.next, 'node_modules'))) {
+    leemons.frontNeedsUpdateDeps = true;
+  }
+
+  // Install dependencies when needed
+  if (leemons.frontNeedsUpdateDeps) {
+    await hooks.fireEvent('leemons::installDeps', { status: 'start' });
+    const installSpinner = ora('Installing frontend dependencies').start();
+    return execa
+      .command(`yarn --cwd ${leemons.dir.next} --force`)
+      .then(() => {
+        installSpinner.succeed('Frontend dependencies installed');
+      })
+      .catch((e) => {
+        installSpinner.fail("Frontend dependencies can't be installed");
+        fs.removeSync(path.resolve(leemons.dir.next, 'node_modules'));
+        throw e;
+      })
+      .finally(async () => {
+        await hooks.fireEvent('leemons::installDeps', { status: 'end' });
+      });
+  }
+  return null;
 }
 
 /*
  * Next.js Build
  */
-function buildNext() {
-  const spinner = ora('Building frontend').start();
-  return execa
-    .command(`yarn --cwd ${leemons.dir.next} build`)
-    .then(() => {
-      spinner.succeed('Frontend builded');
-    })
-    .catch((e) => {
-      spinner.fail(`Frontend can't be builded`);
-      throw e;
-    });
-}
-
-async function build() {
-  // If node_modules is not created, install deps
-  if (!fs.existsSync(path.resolve(leemons.dir.next, 'node_modules'))) {
+async function buildNext() {
+  // Check if the build exists
+  if (!fs.existsSync(path.resolve(leemons.dir.next, '.next'))) {
+    leemons.frontNeedsBuild = true;
+    // The error can be related to dependencies
     leemons.frontNeedsUpdateDeps = true;
   }
 
-  if (leemons.frontNeedsUpdateDeps) {
-    await hooks.fireEvent('leemons::installDeps', { status: 'start' });
-    await frontDeps();
-    await hooks.fireEvent('leemons::installDeps', { status: 'end' });
-  }
+  // Build if necessary
   if (leemons.frontNeedsBuild) {
     await hooks.fireEvent('leemons::build', { status: 'start' });
-    await buildNext();
-    await hooks.fireEvent('leemons::build', { status: 'end' });
+    const spinner = ora('Building frontend').start();
+    return execa
+      .command(`yarn --cwd ${leemons.dir.next} build`)
+      .then(() => {
+        spinner.succeed('Frontend builded');
+      })
+      .catch((e) => {
+        spinner.fail(`Frontend can't be builded`);
+        fs.removeSync(path.resolve(leemons.dir.next, '.next'));
+        throw e;
+      })
+      .finally(async () => {
+        await hooks.fireEvent('leemons::build', { status: 'end' });
+      });
   }
+  return null;
+}
+
+async function build() {
+  await frontDeps();
+  await buildNext();
 }
 
 module.exports = build;
