fc75a74dec30eee85dfcbfae0eb976a7388a9243
Johan Sosa <sosa.johan@gmail.com>
2021-06-21 12:55:22 +0200

    CREATE: Demo example

---
+++ examples/demo-layout/config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  dir: {
+    app: '/Users/Usuario/Sites/leemons.io/repo/examples/demo-layout',
+    plugins: 'plugins',
+    next: 'next',
+    env: '.env',
+  }
+};
---
+++ examples/demo-layout/next/pages/index.js
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div><h1>Hola Home</h1></div>
+}
