ad7b4deeb3e5fca4da4a2fc1f8e58e2e00e19b4e
Miguel Ángel Florido Cifuentes <miguel@leemons.io>
2021-06-21 12:00:17 +0200

    cleaned the services and validations

--- packages/leemons-plugin-multilanguage/services/locales.js
+++ packages/leemons-plugin-multilanguage/services/locales.js
@@ -1,19 +1,3 @@
 const locales = require('../src/services/locale');
 
-console.log();
-locales
-  .addMany([
-    ['es-ES', 'Español de España'],
-    ['en-EN', 'Español de España'],
-  ])
-  .then((r) => {
-    console.log('OK:', r);
-  })
-  .catch((e) => {
-    console.error('KO:', e);
-  })
-  .finally(() => {
-    console.log();
-  });
-
 module.exports = locales;
