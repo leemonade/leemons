7e8e5b236f8f7664ec1c06d16e9267a4f84ab12a
Jaime Gómez Cimarro <cerberupo@gmail.com>
2021-06-29 09:33:35 +0200

    Added default languages

--- packages/leemons-plugin-multilanguage/init.js
+++ packages/leemons-plugin-multilanguage/init.js
@@ -1,14 +1,31 @@
+const _ = require('lodash');
+const locale = require('locale-codes');
+
 const localesTable = leemons.query('plugins_multilanguage::locales');
 
+const { isValidLocaleCode } = require('./src/validations/locale');
 const Locales = require('./src/services/locale');
 
 async function init() {
-  const locales = new Locales({ model: localesTable });
-  await locales.addMany([
-    ['es-ES', 'Español de España'],
-    ['en', 'English'],
-    ['en-UK', 'English UK'],
-  ]);
+  try {
+    const goodLocales = _.map(
+      locale.all.filter((local) => local['iso639-1'] && isValidLocaleCode(local.tag)),
+      (local) => {
+        let name = local.local || local.name;
+        if (local.location) {
+          name += ` (${local.location})`;
+        }
+        return [local.tag, name];
+      }
+    );
+
+    const locales = new Locales({ model: localesTable });
+    if (!(await locales.has(_.last(goodLocales)[0]))) {
+      await locales.addMany(goodLocales);
+    }
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 module.exports = init;
