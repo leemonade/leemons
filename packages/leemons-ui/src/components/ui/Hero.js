a7447bd793a23495c5029b6bbd2906b96b4b895d
Johan Sosa <sosa.johan@gmail.com>
2021-07-22 18:31:25 +0200

    UPDATED: Hero

--- packages/leemons-ui/src/components/ui/Hero.js
+++ packages/leemons-ui/src/components/ui/Hero.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { useWindowHeight } from '@react-hook/window-size';
 
-function Hero({ children, className }) {
-  return <div className={`hero ${className || ''}`}>{children}</div>;
+function Hero({ children, className, maxHeight, minHeight }) {
+  const [height, setHeight] = useState(minHeight);
+  const windowHeight = useWindowHeight();
+
+  useEffect(() => {
+    setHeight(Math.min(Math.max(windowHeight, minHeight), maxHeight));
+  }, [windowHeight]);
+
+  return (
+    <div style={{ minHeight: `${height}px` }} className={`hero ${className || ''}`}>
+      {children}
+    </div>
+  );
 }
+
+Hero.defaultProps = {
+  minHeight: 0,
+  maxHeight: 1000000,
+};
 
 Hero.propTypes = {
   children: PropTypes.any,
   className: PropTypes.string,
+  cover: PropTypes.bool,
+  maxHeight: PropTypes.number,
+  minHeight: PropTypes.number,
 };
 
 export default Hero;
