90aec3e76e67c548500b9128f01445ade9394501
Jaime Gómez Cimarro <cerberupo@gmail.com>
2021-07-28 12:15:23 +0200

    Added page header component

--- packages/leemons-plugin-users/src/services/menu-builder/addWellcome.js
+++ packages/leemons-plugin-users/src/services/menu-builder/addWellcome.js
@@ -1,3 +1,27 @@
-async function addWellcome() {}
+const add = require('./add');
+
+async function addWellcome() {
+  return add(
+    {
+      key: 'welcome',
+      parentKey: 'users',
+      url: '/users/private/welcome',
+      label: {
+        en: 'Welcome',
+        es: 'Bienvenida',
+      },
+    },
+    [
+      {
+        permissionName: 'plugins.users.users',
+        actionNames: ['view'],
+      },
+      {
+        permissionName: 'plugins.users.profiles',
+        actionNames: ['view'],
+      },
+    ]
+  );
+}
 
 module.exports = addWellcome;
