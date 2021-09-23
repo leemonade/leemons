7b5a6704e6b80ef34bb5bff6da6580cca707f227
Miguel Ángel Florido Cifuentes <miguel.mangel.ez11@gmail.com>
2021-09-23 14:44:55 +0200

    Moved to an object

--- packages/leemons-plugin-classroom/controllers/settings.js
+++ packages/leemons-plugin-classroom/controllers/settings.js
@@ -3,61 +3,53 @@ const enableMenuItemService = require('../src/services/menu-builder/enableItem')
 const removeMenuItemService = require('../src/services/menu-builder/remove');
 const settingsSchema = require('../models/settings');
 
-async function findOne(ctx) {
-  const settings = await settingsService.findOne();
-  ctx.status = 200;
-  ctx.body = { status: 200, settings };
-}
-
-async function update(ctx) {
-  const validator = new global.utils.LeemonsValidator({
-    type: 'object',
-    properties: { ...settingsSchema.attributes },
-    required: [],
-    additionalProperties: false,
-  });
-  if (validator.validate(ctx.request.body)) {
-    const settings = await settingsService.update(ctx.request.body);
+module.exports = {
+  findOne: async (ctx) => {
+    const settings = await settingsService.findOne();
     ctx.status = 200;
     ctx.body = { status: 200, settings };
-  } else {
-    throw validator.error;
-  }
-}
-
-async function enableMenuItem(ctx) {
-  const validator = new global.utils.LeemonsValidator({
-    type: 'object',
-    properties: { key: { type: 'string' } },
-    required: ['key'],
-  });
-  if (validator.validate(ctx.request.body)) {
-    const item = await enableMenuItemService(ctx.request.body.key);
-    ctx.status = 200;
-    ctx.body = { status: 200, item };
-  } else {
-    throw validator.error;
-  }
-}
-
-async function removeMenuItem(ctx) {
-  const validator = new global.utils.LeemonsValidator({
-    type: 'object',
-    properties: { key: { type: 'string' } },
-    required: ['key'],
-  });
-  if (validator.validate(ctx.request.body)) {
-    const item = await removeMenuItemService(ctx.request.body.key);
-    ctx.status = 200;
-    ctx.body = { status: 200, item };
-  } else {
-    throw validator.error;
-  }
-}
-
-module.exports = {
-  findOne,
-  update,
-  enableMenuItem,
-  removeMenuItem,
+  },
+  update: async (ctx) => {
+    const validator = new global.utils.LeemonsValidator({
+      type: 'object',
+      properties: { ...settingsSchema.attributes },
+      required: [],
+      additionalProperties: false,
+    });
+    if (validator.validate(ctx.request.body)) {
+      const settings = await settingsService.update(ctx.request.body);
+      ctx.status = 200;
+      ctx.body = { status: 200, settings };
+    } else {
+      throw validator.error;
+    }
+  },
+  enableMenuItem: async (ctx) => {
+    const validator = new global.utils.LeemonsValidator({
+      type: 'object',
+      properties: { key: { type: 'string' } },
+      required: ['key'],
+    });
+    if (validator.validate(ctx.request.body)) {
+      const item = await enableMenuItemService(ctx.request.body.key);
+      ctx.status = 200;
+      ctx.body = { status: 200, item };
+    } else {
+      throw validator.error;
+    }
+  },
+  removeMenuItem: async (ctx) => {
+    const validator = new global.utils.LeemonsValidator({
+      type: 'object',
+      properties: { key: { type: 'string' } },
+      required: ['key'],
+    });
+    if (validator.validate(ctx.request.body)) {
+      const item = await removeMenuItemService(ctx.request.body.key);
+      ctx.status = 200;
+      ctx.body = { status: 200, item };
+    } else {
+      throw validator.error;
+    }
+  },
 };
