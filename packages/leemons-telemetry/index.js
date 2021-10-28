dc062ff789c8e013343669ba5d7b98b165f642f0
Miguel Ángel Florido Cifuentes <miguel.mangel.ez11@gmail.com>
2021-10-28 16:33:36 +0200

    Added telemetry package

---
+++ packages/leemons-telemetry/index.js
@@ -0,0 +1,26 @@
+const apm = require('elastic-apm-node');
+
+const { start } = apm;
+const { startTransaction } = apm;
+
+// Overload apm.start to handle already started apm
+apm.start = (name, options) => {
+  if (!apm.isStarted()) {
+    start.call(apm, {
+      captureBody: 'all',
+      logUncaughtExceptions: true,
+
+      ...options,
+      serviceName: name,
+      serverUrl: 'http://localhost:8200',
+    });
+  }
+};
+
+// Overload apm.startTransaction to use global transaction or parent if exists
+apm.startTransaction = (name, parent) =>
+  startTransaction.call(apm, name, {
+    childOf: parent || global.appTransaction,
+  });
+
+module.exports = apm;
