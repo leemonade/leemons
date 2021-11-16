e423e5ff444d0d7a0e35c99702d2fc829a9e6b56
Jaime Gómez Cimarro <cerberupo@gmail.com>
2021-11-16 15:39:04 +0100

    Modified classByIds for return all relation data

--- packages/leemons-plugin-academic-portfolio/src/services/classes/classByIds.js
+++ packages/leemons-plugin-academic-portfolio/src/services/classes/classByIds.js
@@ -20,18 +20,41 @@ async function classByIds(ids, { transacting } = {}) {
       table.class.find({ class_$in: _.isArray(ids) ? ids : [ids] }, { transacting }),
     ]);
 
+  const [
+    originalSubjectTypes,
+    originalKnowledges,
+    originalSubjects,
+    originalSubstages,
+    originalCourses,
+    originalGroups,
+  ] = await Promise.all([
+    table.subjectTypes.find({ id_$in: _.map(classes, 'subjectType') }, { transacting }),
+    table.knowledges.find({ id_$in: _.map(knowledges, 'id') }, { transacting }),
+    table.subjects.find({ id_$in: _.map(classes, 'subject') }, { transacting }),
+    table.groups.find({ id_$in: _.map(substages, 'id') }, { transacting }),
+    table.groups.find({ id_$in: _.map(courses, 'id') }, { transacting }),
+    table.groups.find({ id_$in: _.map(groups, 'id') }, { transacting }),
+  ]);
+
+  const subjectTypesById = _.keyBy(originalSubjectTypes, 'id');
+  const knowledgesById = _.keyBy(originalKnowledges, 'id');
+  const substagesById = _.keyBy(originalSubstages, 'id');
+  const subjectsById = _.keyBy(originalSubjects, 'id');
+  const coursesById = _.keyBy(originalCourses, 'id');
+  const groupsById = _.keyBy(originalGroups, 'id');
+
   const childClasses = _childClasses.length
     ? await classByIds(_.map(_childClasses, 'id'), { transacting })
     : [];
 
-  const childClassesByClass = _.groupBy(knowledges, 'class');
+  const childClassesByClass = _.groupBy(childClasses, 'class');
   const knowledgesByClass = _.groupBy(knowledges, 'class');
   const substagesByClass = _.groupBy(substages, 'class');
   const coursesByClass = _.groupBy(courses, 'class');
   const groupsByClass = _.groupBy(groups, 'class');
   const teachersByClass = _.groupBy(teachers, 'class');
   const studentsByClass = _.groupBy(students, 'class');
-  return _.map(classes, ({ id, ...rest }) => {
+  return _.map(classes, ({ id, subject, subjectType, ...rest }) => {
     let _students = studentsByClass[id] ? _.map(studentsByClass[id], 'student') : [];
     if (childClassesByClass[id]) {
       _.forEach(childClassesByClass[id], (childClass) => {
@@ -41,10 +64,13 @@ async function classByIds(ids, { transacting } = {}) {
     return {
       id,
       ...rest,
-      knowledges: knowledgesByClass[id] ? knowledgesByClass[id][0].knowledge : null,
-      substages: substagesByClass[id] ? substagesByClass[id][0].substage : null,
-      courses: coursesByClass[id] ? coursesByClass[id][0].course : null,
-      groups: groupsByClass[id] ? groupsByClass[id][0].group : null,
+      subject: subjectsById[subject],
+      subjectType: subjectTypesById[subjectType],
+      classes: childClassesByClass[id],
+      knowledges: knowledgesByClass[id] ? knowledgesById[knowledgesByClass[id][0].knowledge] : null,
+      substages: substagesByClass[id] ? substagesById[substagesByClass[id][0].substage] : null,
+      courses: coursesByClass[id] ? coursesById[coursesByClass[id][0].course] : null,
+      groups: groupsByClass[id] ? groupsById[groupsByClass[id][0].group] : null,
       students: _.uniq(_students),
       teachers: teachersByClass[id]
         ? _.map(teachersByClass[id], ({ teacher, type }) => ({ teacher, type }))
