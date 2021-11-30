1bdd9dc425b2e075cf024d50c28e1d69616e3c3c
Miguel Ángel Florido Cifuentes <miguel.mangel.ez11@gmail.com>
2021-11-30 16:25:45 +0100

    Added all queries

--- packages/leemons-connector-mongoose/queries/queries.js
+++ packages/leemons-connector-mongoose/queries/queries.js
@@ -36,6 +36,83 @@ function generateQueries(model) {
     return $extras(MongooseModel.findOne(finalQuery, undefined, { session: transacting }));
   }
 
+  async function count(query = {}, { transacting } = {}) {
+    const filters = parseFilters({ filters: query, model });
+    const { $extras, ...finalQuery } = buildQuery(model, filters);
+
+    return $extras(MongooseModel.countDocuments(finalQuery, { session: transacting }));
+  }
+
+  async function search(query = {}, page = 0, size = 10, { transacting } = {}) {
+    if (size < 1) {
+      throw new Error('The size should be at least 1');
+    }
+    if (page < 0) {
+      throw new Error('The page should be at least 0');
+    }
+
+    const totalCount = await count(query, { transacting });
+    const totalPages = Math.floor(totalCount / size);
+
+    if (page > totalPages) {
+      return {
+        items: [],
+        count: 0,
+        totalCount,
+        page,
+        size,
+        nextPage: page < totalPages ? page + 1 : null,
+        prevPage: page > 0 ? page - 1 : null,
+      };
+    }
+
+    const items = await find({ ...query, $limit: size, $offset: page * size }, { transacting });
+
+    return {
+      items,
+      count: items.length,
+      totalCount,
+      page,
+      size,
+      nextPage: page < totalPages ? page + 1 : null,
+      prevPage: page > 0 ? page - 1 : null,
+    };
+  }
+
+  async function update(query = {}, item, { transacting } = {}) {
+    const filters = parseFilters({ filters: query, model });
+    const { $extras, ...finalQuery } = buildQuery(model, filters);
+
+    return $extras(MongooseModel.updateOne(finalQuery, item, { session: transacting }));
+  }
+
+  async function updateMany(query = {}, item, { transacting } = {}) {
+    const filters = parseFilters({ filters: query, model });
+    const { $extras, ...finalQuery } = buildQuery(model, filters);
+
+    return $extras(MongooseModel.updateMany(finalQuery, item, { session: transacting }));
+  }
+
+  async function set(query = {}, item, { transacting } = {}) {
+    const filters = parseFilters({ filters: query, model });
+    const { $extras, ...finalQuery } = buildQuery(model, filters);
+
+    return $extras(
+      MongooseModel.updateOne(finalQuery, item, { session: transacting, upsert: true })
+    );
+  }
+
+  async function setMany(query = {}, item, { transacting } = {}) {
+    const filters = parseFilters({ filters: query, model });
+    const { $extras, ...finalQuery } = buildQuery(model, filters);
+
+    return $extras(
+      MongooseModel.updateMany(finalQuery, item, {
+        session: transacting,
+        upsert: true,
+      })
+    );
+  }
   async function deleteOne(query = {}, { transacting } = {}) {
     const filters = parseFilters({ filters: query, model });
     const finalQuery = buildQuery(model, filters);
@@ -70,6 +147,12 @@ function generateQueries(model) {
     createMany,
     find,
     findOne,
+    search,
+    count,
+    update,
+    updateMany,
+    set,
+    setMany,
     delete: deleteOne,
     deleteMany,
     transaction,
