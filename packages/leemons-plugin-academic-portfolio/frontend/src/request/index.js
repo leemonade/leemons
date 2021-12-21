ec3d7e4d118995a8043cad736651de759410a960
Jaime Gómez Cimarro <cerberupo@gmail.com>
2021-12-21 16:46:29 +0100

    Fixed error on getTree in academic portfolio.

--- packages/leemons-plugin-academic-portfolio/frontend/src/request/index.js
+++ packages/leemons-plugin-academic-portfolio/frontend/src/request/index.js
@@ -1,3 +1,5 @@
 import listPrograms from './listPrograms';
+import detailProgram from './detailProgram';
 
 export const listProgramsRequest = listPrograms;
+export const detailProgramRequest = detailProgram;
