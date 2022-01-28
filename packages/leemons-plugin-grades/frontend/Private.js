07eb9dd6781c539a2f07a7a2f5528f5bf0429018
Johan Sosa <sosa.johan@gmail.com>
2022-01-28 12:20:15 +0100

    ADDED: Grades frontend

---
+++ packages/leemons-plugin-grades/frontend/Private.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
+import loadable from '@loadable/component';
+import { useSession } from '@users/session';
+import { goLoginPage } from '@users/navigate';
+
+const Welcome = loadable(() => import('./src/pages/private/WelcomePage'));
+
+export default function Private() {
+  const { path } = useRouteMatch();
+  const session = useSession({ redirectTo: goLoginPage });
+
+  return (
+    <Switch>
+      <Route path={`${path}/welcome`}>
+        <Welcome session={session} />
+      </Route>
+    </Switch>
+  );
+}
--- packages/leemons-plugin-grades/src/i18n/en.js
+++ packages/leemons-plugin-grades/src/i18n/en.js
@@ -1,3 +1,26 @@
 module.exports = {
-  test: {},
+  welcome_page: {
+    page_title: 'Academic Rules',
+    page_description:
+      '"Academic Rules" allows you to create custom rules for your programs and courses: maximum or minimum core/elective credits, required subject areas, elective courses... thanks to these rules you will be able to create grade reports and know which students will be promoted.',
+    hide_info_label: `Ok, I've got it. When the configuration is complete, don't show this info anymore`,
+    step_evaluations: {
+      title: 'Evaluation Systems',
+      description:
+        'Whether you need an evaluation system based on a numerical scale, letter-based or a mix of both, we support it all.',
+      btn: 'Create systems',
+    },
+    step_promotions: {
+      title: 'Promotion rules',
+      description:
+        'Creates a set of evaluation rules based on credits or on required and elective subjects. You can also add rules for specific knowledge areas.',
+      btn: 'Add rules',
+    },
+    step_dependencies: {
+      title: 'Dependencies',
+      description:
+        'Configure the dependencies between subjects, based on your performance or evaluation system.',
+      btn: 'Setup dependencies',
+    },
+  },
 };
