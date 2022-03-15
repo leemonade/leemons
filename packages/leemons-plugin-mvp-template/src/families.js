ac5a9e87a1e7358656637dca88398b4efaa04498
Johan Sosa <sosa.johan@gmail.com>
2022-03-15 09:01:48 +0100

    ADDED: Import Families Emergency numbers in MVP Template bulk.

--- packages/leemons-plugin-mvp-template/src/families.js
+++ packages/leemons-plugin-mvp-template/src/families.js
@@ -18,32 +18,20 @@ async function initFamilies(profiles, users) {
     // FAMILY RELATIONSHIPS
 
     const families = await importFamilies(users);
-
-    /*
-{
-name: "Nombre de la familia",
-guardians: [{user: 'id-del-usuario', memberType: 'guardian'}],
-students: [{user: 'id-del-usuario', memberType: 'student'}],
-maritalStatus: 'married|divorced|domestic_partners|cohabitants|separated',
-emergencyPhoneNumbers: [{name: 'Nombre del contacto', phone: 'El telefono', relation: 'father|mother|legal_guardian|cualquier-texto-se-tomara-como-other'}]
-}
-    */
     const familiesData = [];
 
     keys(families).forEach((key) => {
-      const { name, ...family } = families[key];
-      const guardians = uniq(
-        family.relations.map((relation) => relation.guardian.userAgents[0].user)
-      ).map((userId) => ({ user: userId, memberType: 'guardian' }));
+      const { relations, ...family } = families[key];
+      const guardians = uniq(relations.map((relation) => relation.guardian.userAgents[0].user)).map(
+        (userId) => ({ user: userId, memberType: 'guardian' })
+      );
 
-      const students = uniq(
-        family.relations.map((relation) => relation.student.userAgents[0].user)
-      ).map((userId) => ({ user: userId, memberType: 'student' }));
+      const students = uniq(relations.map((relation) => relation.student.userAgents[0].user)).map(
+        (userId) => ({ user: userId, memberType: 'student' })
+      );
 
-      familiesData.push({ name, guardians, students });
+      familiesData.push({ ...family, guardians, students });
     });
-
-    console.dir(familiesData, { depth: null });
 
     await Promise.all(familiesData.map((family) => services.family.add(family)));
 
