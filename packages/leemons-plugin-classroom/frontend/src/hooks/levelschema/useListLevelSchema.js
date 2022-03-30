a74004bc31ea64d785f3f61fac2fac02fc3a8685
Jaime Gómez Cimarro <cerberupo@gmail.com>
2022-03-30 12:00:55 +0200

    Remove plugin-classroom.

--- packages/leemons-plugin-classroom/frontend/src/hooks/levelschema/useListLevelSchema.js
+++
@@ -1,8 +0,0 @@
-import listLevelSchemas from '../../services/levelSchemas/listLevelSchemas';
-import useAsync from '../request/useAsync';
-
-function useListLevelSchema(locale) {
-  return useAsync(listLevelSchemas, locale);
-}
-
-export default useListLevelSchema;
