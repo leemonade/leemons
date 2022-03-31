8629b8eabd00225eefb57f99243fc57f927d41e3
Miguel Ángel Florido Cifuentes <miguel.mangel.ez11@gmail.com>
2022-03-31 12:28:25 +0200

    Ensure task's subjects exists before parsing the object

--- packages/leemons-plugin-tasks/frontend/src/components/Assignment/AssignStudents/hooks/useSubjects.js
+++ packages/leemons-plugin-tasks/frontend/src/components/Assignment/AssignStudents/hooks/useSubjects.js
@@ -6,6 +6,10 @@ export default function useSubjects(task) {
   const { subjects } = task;
 
   useEffect(async () => {
+    if (!subjects?.length) {
+      return;
+    }
+
     const s = await Promise.all(subjects.map(({ subject }) => getSubjectDetails(subject)));
     setData(
       s.map(({ data: subjectData }) => ({
