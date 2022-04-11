7dec376f0f122ebe342818ae6e3967399b3bc6d6
Miguel Ángel Florido Cifuentes <miguel.mangel.ez11@gmail.com>
2022-04-11 12:43:26 +0200

    Added helpers to service

--- packages/leemons-plugin-common/services/versionControl.js
+++ packages/leemons-plugin-common/services/versionControl.js
@@ -3,6 +3,7 @@ const get = require('../src/services/versionControl/currentVersions/get');
 const list = require('../src/services/versionControl/currentVersions/list');
 const remove = require('../src/services/versionControl/currentVersions/remove');
 const update = require('../src/services/versionControl/currentVersions/update');
+const helpers = require('../src/services/versionControl/helpers');
 const { getVersion, publishVersion } = require('../src/services/versionControl/versions');
 const listVersionOfType = require('../src/services/versionControl/versions/listVersionOfType');
 const listVersions = require('../src/services/versionControl/versions/listVersions');
@@ -22,4 +23,6 @@ module.exports = {
   publishVersion,
   listVersions,
   listVersionOfType,
+
+  ...helpers,
 };
