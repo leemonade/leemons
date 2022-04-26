6c44510665ba7304d344d7e745eeeba44efee39c
Miguel Ángel Florido Cifuentes <miguel.mangel.ez11@gmail.com>
2022-04-26 17:51:32 +0200

    Added remove asset to leebrary services

--- packages/leemons-plugin-leebrary/services/assets.js
+++ packages/leemons-plugin-leebrary/services/assets.js
@@ -2,10 +2,12 @@ const { exists } = require('../src/services/assets/exists');
 const { add } = require('../src/services/assets/add');
 const { getByIds } = require('../src/services/assets/getByIds');
 const { update } = require('../src/services/assets/update');
+const { remove } = require('../src/services/assets/remove');
 
 module.exports = {
   exists,
   add,
   getByIds,
   update,
+  remove,
 };
