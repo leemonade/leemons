0e957936a0a0ae9d8508435752aa9fdd3539f6f1
Miguel Ángel Florido Cifuentes <miguel.mangel.ez11@gmail.com>
2022-04-26 15:19:25 +0200

    Fixed several bugs and exposed asset update to services

--- packages/leemons-plugin-leebrary/services/assets.js
+++ packages/leemons-plugin-leebrary/services/assets.js
@@ -1,9 +1,11 @@
 const { exists } = require('../src/services/assets/exists');
 const { add } = require('../src/services/assets/add');
 const { getByIds } = require('../src/services/assets/getByIds');
+const { update } = require('../src/services/assets/update');
 
 module.exports = {
   exists,
   add,
   getByIds,
+  update,
 };
