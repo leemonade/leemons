08e43ea0fa18a5f91936fc82e913a5d4ee95989f
Miguel Ángel Florido Cifuentes <miguel.mangel.ez11@gmail.com>
2022-05-10 13:31:56 +0200

    Changed where the assets gets removed and refresh front when deleting asset

--- packages/leemons-plugin-assignables/src/services/assignable/removeAssignables.js
+++ packages/leemons-plugin-assignables/src/services/assignable/removeAssignables.js
@@ -17,14 +17,15 @@ async function getAssignablesUsingAsset(asset, { transacting }) {
 module.exports = async function removeAssignables(assignables, { userSession, transacting: t }) {
   return global.utils.withTransaction(
     async (transacting) => {
-      const assets = [];
       await Promise.all(
         assignables.map(async (assignable) => {
           // EN: Get the assignable to validate ownership.
           // ES: Obtiene el asignable para validar la propiedad.
           const a = await getAssignable.call(this, assignable, { userSession, transacting });
 
-          assets.push(a.asset.id);
+          // EN: remove the assets
+          // ES: elimina los archivos
+          await removeAsset(a.asset.id, { userSession, transacting });
 
           // EN: Get the users that have access to the assignable.
           // ES: Obtiene los usuarios que tienen acceso al asignable.
@@ -57,20 +58,6 @@ module.exports = async function removeAssignables(assignables, { userSession, tr
             subjects.map((s) => s.id),
             { transacting }
           );
-        })
-      );
-
-      // EN: remove the assets
-      // ES: elimina los activos
-      await Promise.all(
-        assets.map(async (asset) => {
-          const assginablesusingAsset = await getAssignablesUsingAsset(asset, { transacting });
-
-          if (assginablesusingAsset.length === 0) {
-            return removeAsset(asset, { userSession, transacting });
-          }
-
-          return null;
         })
       );
 
