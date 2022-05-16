a33869d2f1e16fd487485250c265ddc187122077
Miguel Ángel Florido Cifuentes <miguel.mangel.ez11@gmail.com>
2022-05-16 10:36:53 +0200

    Sort and filter by dates

--- packages/leemons-plugin-assignables/src/services/assignableInstance/searchAssignableInstances.js
+++ packages/leemons-plugin-assignables/src/services/assignableInstance/searchAssignableInstances.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const dayjs = require('dayjs');
+const { getDates } = require('../dates');
 const { teachers, assignations } = require('../tables');
 
 async function searchTeacherAssignableInstances(query, { userSession, transacting } = {}) {
@@ -11,9 +14,74 @@ async function searchTeacherAssignableInstances(query, { userSession, transactin
 async function searchStudentAssignableInstances(query, { userSession, transacting } = {}) {
   const userAgents = userSession.userAgents.map((userAgent) => userAgent.id);
 
-  const results = await assignations.find({ user_$in: userAgents }, { transacting });
+  const studentAssignations = await assignations.find({ user_$in: userAgents }, { transacting });
 
-  return results.map((assignation) => assignation.instance);
+  let results = studentAssignations.map((assignation) => ({
+    instance: assignation.instance,
+    user: assignation.user,
+  }));
+
+  const instances = _.uniq(_.map(results, 'instance'));
+
+  const instancesData = await instances.reduce(async (obj, instance) => {
+    const dates = await getDates('assignableInstance', instance, { transacting });
+
+    return {
+      ...(await obj),
+      [instance]: {
+        dates,
+      },
+    };
+  }, {});
+
+  results = results.map((result) => ({
+    ...result,
+    dates: instancesData[result.instance].dates,
+  }));
+
+  // EN: Filter by dates visibility
+  // ES: Filtra por fechas de visibilidad
+  results = results.filter((result) => {
+    const { dates } = result;
+
+    const { start, deadline, visibility } = dates;
+
+    if (!start || !deadline) {
+      return true;
+    }
+
+    if (visibility) {
+      if (dayjs(visibility).isAfter(dayjs())) {
+        return false;
+      }
+    } else if (start && dayjs(start).isAfter(dayjs())) {
+      return false;
+    }
+
+    return true;
+  });
+
+  // TODO: CHECK ORDER
+  results.sort((a, b) => {
+    const { dates: aDates } = a;
+    const { dates: bDates } = b;
+
+    if (aDates.deadline && bDates.deadline) {
+      return dayjs(aDates.deadline).diff(dayjs(bDates.deadline));
+    }
+
+    if (aDates.deadline) {
+      return 1;
+    }
+
+    if (bDates.deadline) {
+      return -1;
+    }
+
+    return 0;
+  });
+
+  return results;
 }
 
 module.exports = async function searchAssignableInstances(
