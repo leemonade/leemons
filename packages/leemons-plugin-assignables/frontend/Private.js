0c13b1ee1ba571df9b519162ae51f0ef17fb7223
Miguel Ángel Florido Cifuentes <miguel.mangel.ez11@gmail.com>
2022-05-17 15:11:15 +0200

    Added session to assignables private frontend

--- packages/leemons-plugin-assignables/frontend/Private.js
+++ packages/leemons-plugin-assignables/frontend/Private.js
@@ -2,19 +2,24 @@ import React from 'react';
 import { Switch as Routes, Route, useRouteMatch, Redirect } from 'react-router-dom';
 import loadable from '@loadable/component';
 
+import { useSession } from '@users/session';
+import { goLoginPage } from '@users/navigate';
+
 const Details = loadable(() => import('./src/components/Details'));
 const Ongoing = loadable(() => import('./src/components/Ongoing'));
 
 export default function Private() {
+  const session = useSession({ redirectTo: goLoginPage });
+
   const { path } = useRouteMatch();
 
   return (
     <Routes>
       <Route path={`${path}/ongoing`}>
-        <Ongoing />
+        <Ongoing session={session} />
       </Route>
       <Route path={`${path}/details/:id`}>
-        <Details />
+        <Details session={session} />
       </Route>
 
       <Route path={`${path}/`}>
