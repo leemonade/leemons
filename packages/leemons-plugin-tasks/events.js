c91a51baaf34d3a3f77f74a86630184a0c7edf67
Miguel Ángel Florido Cifuentes <miguel.mangel.ez11@gmail.com>
2022-05-23 17:15:32 +0200

    Removed dashboard widgets for task ongoing

--- packages/leemons-plugin-tasks/events.js
+++ packages/leemons-plugin-tasks/events.js
@@ -32,29 +32,29 @@ async function events(isInstalled) {
         await initMenuBuilder();
       }
     );
-    leemons.events.once('plugins.dashboard:init-widget-zones', async () => {
-      await Promise.all(
-        _.map(constants.widgets.zones, (config) =>
-          leemons.getPlugin('widgets').services.widgets.addZone(config.key, {
-            name: config.name,
-            description: config.description,
-          })
-        )
-      );
-      leemons.events.emit('init-widget-zones');
-      await Promise.all(
-        _.map(constants.widgets.items, (config) =>
-          leemons
-            .getPlugin('widgets')
-            .services.widgets.addItemToZone(config.zoneKey, config.key, config.url, {
-              name: config.name,
-              description: config.description,
-              properties: config.properties,
-            })
-        )
-      );
-      leemons.events.emit('init-widget-items');
-    });
+    // leemons.events.once('plugins.dashboard:init-widget-zones', async () => {
+    //   await Promise.all(
+    //     _.map(constants.widgets.zones, (config) =>
+    //       leemons.getPlugin('widgets').services.widgets.addZone(config.key, {
+    //         name: config.name,
+    //         description: config.description,
+    //       })
+    //     )
+    //   );
+    //   leemons.events.emit('init-widget-zones');
+    //   await Promise.all(
+    //     _.map(constants.widgets.items, (config) =>
+    //       leemons
+    //         .getPlugin('widgets')
+    //         .services.widgets.addItemToZone(config.zoneKey, config.key, config.url, {
+    //           name: config.name,
+    //           description: config.description,
+    //           properties: config.properties,
+    //         })
+    //     )
+    //   );
+    //   leemons.events.emit('init-widget-items');
+    // });
 
     // EN: Register the assignable role
     // ES: Registrar el rol de asignable
