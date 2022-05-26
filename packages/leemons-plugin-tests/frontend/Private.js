4f0ddbff5b2c2d2a6cb1fdcd7db08ba4897b4cdc
Johan Sosa <sosa.johan@gmail.com>
2022-05-26 16:09:08 +0200

    ADDED: loadable fallback on plugin.tests

--- packages/leemons-plugin-tests/frontend/Private.js
+++ packages/leemons-plugin-tests/frontend/Private.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Route, Switch, useRouteMatch } from 'react-router-dom';
 import loadable from '@loadable/component';
+import pMinDelay from 'p-min-delay';
+import { LoadingOverlay } from '@bubbles-ui/components';
 import { useSession } from '@users/session';
 import { goLoginPage } from '@users/navigate';
 
-const TestsList = loadable(() => import('./src/pages/private/tests/List'));
-const TestsEdit = loadable(() => import('./src/pages/private/tests/Edit'));
-const TestsAssign = loadable(() => import('./src/pages/private/tests/Assign'));
-const TestsDetail = loadable(() => import('./src/pages/private/tests/Detail'));
-const TestsResult = loadable(() => import('./src/pages/private/tests/Result'));
-const QuestionBanksList = loadable(() => import('./src/pages/private/questions-banks/List'));
-const QuestionBankDetail = loadable(() => import('./src/pages/private/questions-banks/Detail'));
-const StudentInstance = loadable(() => import('./src/pages/private/tests/StudentInstance/index'));
+const TestsList = loadable(() => pMinDelay(import('./src/pages/private/tests/List'), 1000));
+const TestsEdit = loadable(() => pMinDelay(import('./src/pages/private/tests/Edit'), 1000));
+const TestsAssign = loadable(() => pMinDelay(import('./src/pages/private/tests/Assign'), 1000));
+const TestsDetail = loadable(() => pMinDelay(import('./src/pages/private/tests/Detail'), 1000));
+const TestsResult = loadable(() => pMinDelay(import('./src/pages/private/tests/Result'), 1000));
+const QuestionBanksList = loadable(() =>
+  pMinDelay(import('./src/pages/private/questions-banks/List'), 1000)
+);
+const QuestionBankDetail = loadable(() =>
+  pMinDelay(import('./src/pages/private/questions-banks/Detail'), 1000)
+);
+const StudentInstance = loadable(() =>
+  pMinDelay(import('./src/pages/private/tests/StudentInstance/index'), 1000)
+);
 
 export default function Private() {
   const { path } = useRouteMatch();
@@ -20,34 +28,34 @@ export default function Private() {
   return (
     <Switch>
       <Route path={`${path}/result/:id/:user`}>
-        <TestsResult session={session} />
+        <TestsResult session={session} fallback={<LoadingOverlay visible />} />
       </Route>
       <Route path={`${path}/result/:id`}>
-        <TestsResult session={session} />
+        <TestsResult session={session} fallback={<LoadingOverlay visible />} />
       </Route>
       <Route path={`${path}/student/:id/:user`}>
-        <StudentInstance session={session} />
+        <StudentInstance session={session} fallback={<LoadingOverlay visible />} />
       </Route>
       <Route path={`${path}/student/:id`}>
-        <StudentInstance session={session} />
+        <StudentInstance session={session} fallback={<LoadingOverlay visible />} />
       </Route>
       <Route path={`${path}/questions-banks/:id`}>
-        <QuestionBankDetail session={session} />
+        <QuestionBankDetail session={session} fallback={<LoadingOverlay visible />} />
       </Route>
       <Route path={`${path}/questions-banks`}>
-        <QuestionBanksList session={session} />
+        <QuestionBanksList session={session} fallback={<LoadingOverlay visible />} />
       </Route>
       <Route path={`${path}/detail/:id`}>
-        <TestsDetail session={session} />
+        <TestsDetail session={session} fallback={<LoadingOverlay visible />} />
       </Route>
       <Route path={`${path}/assign/:id`}>
-        <TestsAssign session={session} />
+        <TestsAssign session={session} fallback={<LoadingOverlay visible />} />
       </Route>
       <Route path={`${path}/:id`}>
-        <TestsEdit session={session} />
+        <TestsEdit session={session} fallback={<LoadingOverlay visible />} />
       </Route>
       <Route path={`${path}`}>
-        <TestsList session={session} />
+        <TestsList session={session} fallback={<LoadingOverlay visible />} />
       </Route>
     </Switch>
   );
