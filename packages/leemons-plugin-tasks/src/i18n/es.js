29c9d94c85c23b43666113c7d76048d3d8351da7
Miguel Ángel Florido Cifuentes <miguel.mangel.ez11@gmail.com>
2022-06-07 18:10:33 +0200

    Fixed misspelled label

--- packages/leemons-plugin-tasks/src/i18n/es.js
+++ packages/leemons-plugin-tasks/src/i18n/es.js
@@ -73,7 +73,7 @@ module.exports = {
         labels: {
           name: 'Nombre',
           tagline: 'Subtítulo',
-          description: 'Descipción',
+          description: 'Descripción',
           buttonNext: 'Siguiente',
         },
         placeholders: {
