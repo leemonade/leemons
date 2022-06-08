0e670edbecfa7b29add40fd0b1c7eb453d08b4ad
Miguel Ángel Florido Cifuentes <miguel.mangel.ez11@gmail.com>
2022-06-08 11:04:03 +0200

    Moved react-query

--- packages/multi-src-react/config/src/index.js
+++ packages/multi-src-react/config/src/index.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDom from 'react-dom';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import App from './App';
 import { Provider as GlobalProvider } from './contexts/global';
 
+const queryClient = new QueryClient();
+
+function ReactQueryDevtoolsWrapper() {
+  if (process.env.NODE_ENV === 'production') {
+    return null;
+  }
+
+  const [showQueryDevtools, setShowQueryDevtools] = useState(
+    window.localStorage.getItem('showReactQueryDevTools')
+  );
+
+  useEffect(() => {
+    window.toogleQueryDevtools = (persist) =>
+      setShowQueryDevtools((s) => {
+        if (persist) {
+          window.localStorage.setItem('showReactQueryDevTools', !s);
+        }
+        return !s;
+      });
+  }, [setShowQueryDevtools]);
+
+  if (showQueryDevtools) {
+    return <ReactQueryDevtools initialIsOpen={false} />;
+  }
+
+  return null;
+}
+
 ReactDom.render(
   <React.StrictMode>
-    <GlobalProvider>
-      <App />
-    </GlobalProvider>
+    <QueryClientProvider client={queryClient}>
+      <GlobalProvider>
+        <App />
+      </GlobalProvider>
+      <ReactQueryDevtoolsWrapper />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
