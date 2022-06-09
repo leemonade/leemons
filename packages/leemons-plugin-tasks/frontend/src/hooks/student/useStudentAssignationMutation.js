4917003241f94a4ffcbc1008137cc3b8b312c98f
Miguel Ángel Florido Cifuentes <miguel.mangel.ez11@gmail.com>
2022-06-09 11:13:42 +0200

    Adding react-query to task realization

---
+++ packages/leemons-plugin-tasks/frontend/src/hooks/student/useStudentAssignationMutation.js
@@ -0,0 +1,21 @@
+import { useMutation, useQueryClient } from 'react-query';
+import updateStudentRequest from '../../request/instance/updateStudent';
+
+export default function useStudentAssignationMutation(details = true) {
+  const queryClient = useQueryClient();
+  return useMutation(
+    'assignations',
+    async (newData) => {
+      await updateStudentRequest(newData);
+      return newData;
+    },
+    {
+      onSuccess: (data) => {
+        queryClient.invalidateQueries([
+          'assignations',
+          { instance: data.instance, user: data.student, details },
+        ]);
+      },
+    }
+  );
+}
