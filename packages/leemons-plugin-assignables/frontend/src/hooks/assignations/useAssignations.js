9cc3e9eea78ca06617d6197cbe5e4dbe75346624
Miguel Ángel Florido Cifuentes <miguel.mangel.ez11@gmail.com>
2022-06-09 11:44:17 +0200

    Fixed error where you can keep doing the task after time ends

--- packages/leemons-plugin-assignables/frontend/src/hooks/assignations/useAssignations.js
+++ packages/leemons-plugin-assignables/frontend/src/hooks/assignations/useAssignations.js
@@ -1,12 +1,13 @@
 import { useQueries } from 'react-query';
 import getAssignation from '../../requests/assignations/getAssignation';
 
-export default function useAssignations(instances, details = true) {
+export default function useAssignations(instances, details = true, { enabled = true } = {}) {
   const _instances = (Array.isArray(instances) ? instances : [instances])?.filter(Boolean);
   const queries = useQueries(
     _instances?.map(({ instance, user }) => ({
       queryKey: ['assignations', { instance, user, details }],
       queryFn: () => getAssignation({ id: instance, user, details }),
+      enabled,
     })) || []
   );
 
