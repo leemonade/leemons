7ce17b7a6068d1461de44bc94efa334e3af860dd
Miguel Ángel Florido Cifuentes <miguel.mangel.ez11@gmail.com>
2022-07-05 11:08:30 +0200

    Updated docker-compose to avoid some mysql errors and updated start.sh to use docker cli instead of old docker-compose

--- docker/start.sh
+++ docker/start.sh
@@ -1,3 +1,3 @@
-docker compose up -d
-docker-compose exec leemons-yarn-install sh installDeps.sh
-docker-compose logs -f
+docker compose up -d --remove-orphans
+docker compose exec leemons-yarn-install sh installDeps.sh
+docker compose logs -f
