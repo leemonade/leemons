5f4f2a342e1f4e903e3c6ac03ffa88dcf3a14ef4
Miguel Ángel Florido Cifuentes <miguel.mangel.ez11@gmail.com>
2022-07-07 13:37:12 +0200

    Fixed start.sh to add env file

--- docker/start.sh
+++ docker/start.sh
@@ -1,3 +1,3 @@
-docker compose up -d --remove-orphans --env-file ../examples/docker/.env
+docker compose --env-file ../examples/docker/.env up -d --remove-orphans
 docker compose exec leemons-yarn-install sh installDeps.sh
 docker compose logs -f
