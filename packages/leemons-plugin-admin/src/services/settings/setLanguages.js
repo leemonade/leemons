d56e29a238dbe73ecc1fc6d318472203e7d1ad8d
Jaime Gómez Cimarro <cerberupo@gmail.com>
2022-07-20 12:30:23 +0200

    Admin finished

--- packages/leemons-plugin-admin/src/services/settings/setLanguages.js
+++ packages/leemons-plugin-admin/src/services/settings/setLanguages.js
@@ -4,6 +4,7 @@ const { table } = require('../tables');
 const findOne = require('./findOne');
 const update = require('./update');
 const { STATUS } = require('../../../config/constants');
+
 // const { addLocales } = require('../locales/addLocales');
 
 /**
@@ -15,30 +16,18 @@ async function setLanguages(langs, defaultLang, { transacting: _transacting } = 
   return global.utils.withTransaction(
     async (transacting) => {
       const currentSettings = await findOne({ transacting });
-      if (currentSettings && currentSettings.configured) {
-        throw new Error('Settings already configured');
-      }
 
       const { services: userService } = leemons.getPlugin('users');
       const locales = flattenDeep([langs]);
       const currentLocales = await userService.platform.getLocales({ transacting });
 
-      console.log('currentLocales:');
-      console.dir(currentLocales, { depth: null });
-
       const localesToAdd = locales.filter(
         (locale) => !currentLocales.find((l) => l.code === locale.code)
       );
 
-      console.log('localesToAdd:');
-      console.dir(localesToAdd, { depth: null });
-
       const localesToRemove = currentLocales.filter(
         (locale) => !locales.find((l) => l.code === locale.code)
       );
-
-      console.log('localesToRemove:');
-      console.dir(localesToRemove, { depth: null });
 
       const localesAdded = await Promise.all(
         localesToAdd.map((lang) => userService.platform.addLocale(lang.code, lang.name))
@@ -46,8 +35,10 @@ async function setLanguages(langs, defaultLang, { transacting: _transacting } = 
 
       // await addLocales(localesAdded.map((locale) => locale.code));
 
-      if (defaultLang && isString(defaultLang) && !isEmpty(defaultLang)) {
-        await userService.platform.setDefaultLocale(defaultLang);
+      if (!currentSettings || !currentSettings.configured) {
+        if (defaultLang && isString(defaultLang) && !isEmpty(defaultLang)) {
+          await userService.platform.setDefaultLocale(defaultLang);
+        }
       }
 
       return update.call(
