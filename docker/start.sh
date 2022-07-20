ba326cf7b59df60a98cc3a3824b09d629aecc89d
Diego Gómez <diegogd@gmail.com>
2022-07-20 10:40:00 +0200

    Check initialization of yarn to execute docker
    Update start.sh to initialize the environment to let docker compose execute.
    [ref #5]

--- docker/start.sh
+++ docker/start.sh
@@ -1,3 +1,8 @@
+if [ ! -f ../examples/docker/monorepo_yarn.lock ]
+then
+    touch ../examples/docker/monorepo_yarn.lock
+    docker compose run leemons-yarn-install sh installDeps.sh
+fi
 docker compose --env-file ../examples/docker/.env up -d --remove-orphans
 docker compose exec leemons-yarn-install sh installDeps.sh
 docker compose logs -f
