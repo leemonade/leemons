e7a96fdb94527a6fffca7886a2af770517f20725
Jaime Gómez Cimarro <cerberupo@gmail.com>
2022-09-09 12:48:54 +0200

    fix: Remove group from tree if only one group configured.

--- packages/leemons-plugin-academic-portfolio/src/services/common/getTree.js
+++ packages/leemons-plugin-academic-portfolio/src/services/common/getTree.js
@@ -150,17 +150,28 @@ async function getTree(nodeTypes, { program, transacting } = {}) {
     nodeTypes.forEach((nodeType, index) => {
       if (classroom[nodeType]) {
         const id = _.isString(classroom[nodeType]) ? classroom[nodeType] : classroom[nodeType].id;
-        if (courses.length > 1 && nodeType === 'courses' && nodeTypes[index - 1] === 'cycles') {
-          const cycle = getCycleByCourse(id);
-          if (cycle) {
+        if (nodeType === 'courses') {
+          const proId = nodesByIds.courses[id].program;
+          const proCourses = _.filter(courses, { program: proId });
+          if (proCourses.length > 1 && nodeTypes[index - 1] === 'cycles') {
+            const cycle = getCycleByCourse(id);
+            if (cycle) {
+              nodes.push({
+                type: 'cycles',
+                id: cycle.id,
+              });
+            }
+          }
+          if (proCourses.length > 1) {
             nodes.push({
-              type: 'cycles',
-              id: cycle.id,
+              type: nodeType,
+              id,
             });
           }
         }
-        if (nodeType === 'courses') {
-          if (courses.length > 1) {
+        if (nodeType === 'groups') {
+          const pro = nodesByIds.program[nodesByIds.groups[id].program];
+          if (!pro.useOneStudentGroup) {
             nodes.push({
               type: nodeType,
               id,
