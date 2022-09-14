00e415434c82aa77171bb33d84cb408f60198c9e
Albert Araque Casaus <albert.araque.casaus@gmail.com>
2022-09-14 18:02:06 +0200

    feat: Now you can save and download the calendar to a pdf file in AcademicCalendar

--- packages/leemons-plugin-academic-calendar/frontend/src/pages/private/program/components/Step3.js
+++ packages/leemons-plugin-academic-calendar/frontend/src/pages/private/program/components/Step3.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import _ from 'lodash';
 import PropTypes from 'prop-types';
 import { BigCalendar } from '@bubbles-ui/calendars';
-import { ChevLeftIcon } from '@bubbles-ui/icons/outline';
-import { Box, Button, ContextContainer, Stack, TabPanel, Tabs } from '@bubbles-ui/components';
+import { ChevLeftIcon, DownloadIcon } from '@bubbles-ui/icons/outline';
+import {
+  Box,
+  Button,
+  ContextContainer,
+  Stack,
+  TabPanel,
+  Tabs,
+  IconButton,
+  Title,
+} from '@bubbles-ui/components';
 import { useLocale, useStore } from '@common';
 import { useProcessCalendarConfigForBigCalendar } from '@academic-calendar/helpers/useProcessCalendarConfigForBigCalendar';
 import { addErrorAlert, addSuccessAlert } from '@layout/alert';
 import useRequestErrorMessage from '@common/useRequestErrorMessage';
 import { saveConfig } from '@academic-calendar/request/config';
 import CalendarKey from '@academic-calendar/components/CalendarKey';
+import html2canvas from 'html2canvas';
+import { jsPDF } from 'jspdf';
 
 export default function Step3({ regionalConfigs, program, config, onPrev, onSave, t }) {
   const locale = useLocale();
@@ -18,6 +29,7 @@ export default function Step3({ regionalConfigs, program, config, onPrev, onSave
   const [store, render] = useStore({
     saving: false,
   });
+  const [isPrinting, setIsPrinting] = useState(false);
 
   const coursesForDates = config.allCoursesHaveSameDates ? [program?.courses[0]] : program?.courses;
 
@@ -40,9 +52,35 @@ export default function Step3({ regionalConfigs, program, config, onPrev, onSave
 
   if (!program) return null;
 
+  useEffect(() => {
+    if (!isPrinting) return;
+    const input = document.getElementById('calendarToPrint');
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      // eslint-disable-next-line new-cap
+      const pdf = new jsPDF();
+      pdf.addImage(imgData, 'JPEG', 0, 0);
+      pdf.save('download.pdf');
+    });
+    setIsPrinting(false);
+  }, [isPrinting]);
+
   return (
     <ContextContainer divided>
       <ContextContainer>
+        <Stack justifyContent="space-between" alignItems="center" style={{ marginInline: 8 }}>
+          <Title order={1}>{program.name}</Title>
+          <IconButton
+            icon={<DownloadIcon height={16} width={16} />}
+            color="primary"
+            rounded
+            label={t('downloadCalendar')}
+            onClick={() => {
+              setIsPrinting(true);
+            }}
+            loading={isPrinting}
+          />
+        </Stack>
         <Tabs forceRender>
           {coursesForDates.map((course) => {
             const bigCalendarConf = processCalendarConfigForBigCalendar(
@@ -71,6 +109,28 @@ export default function Step3({ regionalConfigs, program, config, onPrev, onSave
                   <BigCalendar {...bigCalendarConf} />
                   <CalendarKey />
                 </Box>
+                <Stack
+                  direction="column"
+                  justifyContent="space-between"
+                  alignItems="center"
+                  height="100%"
+                  id="calendarToPrint"
+                  style={{
+                    position: 'absolute',
+                    left: -9999,
+                    top: -9999,
+                    width: '210mm',
+                    minHeight: '297mm',
+                    padding: 16,
+                  }}
+                >
+                  <Box style={{ marginLeft: 89 }}>
+                    <BigCalendar {...bigCalendarConf} />
+                  </Box>
+                  <Box style={{ width: '100%' }}>
+                    <CalendarKey />
+                  </Box>
+                </Stack>
               </TabPanel>
             );
           })}
