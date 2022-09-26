6704a25aec7151f7e15f78aa67503594ae05c399
Albert Araque Casaus <albert.araque.casaus@gmail.com>
2022-09-26 17:28:36 +0200

    feat: Added time checks to set response

--- packages/leemons-plugin-feedback/src/services/feedback-responses/setQuestionResponse.js
+++ packages/leemons-plugin-feedback/src/services/feedback-responses/setQuestionResponse.js
@@ -1,9 +1,43 @@
+const dayjs = require('dayjs');
+const duration = require('dayjs/plugin/duration');
 const { table } = require('../tables');
 
+dayjs.extend(duration);
+
 async function setQuestionResponse(data, { userSession, transacting: _transacting } = {}) {
+  const { assignations: assignationsService, assignableInstances: assignableInstancesService } =
+    leemons.getPlugin('assignables').services;
+
   return global.utils.withTransaction(
     async (transacting) => {
       const { questionId, instanceId, value } = data;
+
+      const [{ timestamps, finished }, instance] = await Promise.all([
+        assignationsService.getAssignation(data.instance, userSession.userAgents[0].id, {
+          userSession,
+          transacting,
+        }),
+        assignableInstancesService.getAssignableInstance(data.instance, {
+          userSession,
+          details: true,
+          transacting,
+        }),
+      ]);
+
+      if (finished) {
+        throw new Error('Assignation finished');
+      }
+
+      if (instance.duration) {
+        // Check if not exceeding the duration
+        const [_value, unit] = instance.duration.split(' ');
+        const seconds = dayjs.duration({ [unit]: _value }).asSeconds();
+        const start = new Date(timestamps.start);
+        start.setSeconds(start.getSeconds() + seconds);
+        if (new Date() > start) {
+          throw new global.utils.HttpErrorWithCustomCode(400, 7001, 'Time used up');
+        }
+      }
 
       const response = await table.feedbackResponse.set(
         { question: questionId, instance: instanceId, userAgent: userSession.userAgents[0].id },
