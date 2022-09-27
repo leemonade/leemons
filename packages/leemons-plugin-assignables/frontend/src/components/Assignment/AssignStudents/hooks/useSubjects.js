3343a2bc63688cf8056cf2fe45754ab90f985f21
MIGUELez11 <miguel.mangel.ez11@gmail.com>
2022-09-27 09:14:28 +0200

    Fixed infinite fetching when assigning assignables without subjects

--- packages/leemons-plugin-assignables/frontend/src/components/Assignment/AssignStudents/hooks/useSubjects.js
+++ packages/leemons-plugin-assignables/frontend/src/components/Assignment/AssignStudents/hooks/useSubjects.js
@@ -1,20 +1,25 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import _ from 'lodash';
 import { getSubjectDetails } from '@academic-portfolio/request/subjects';
 import useSessionClasses from '@academic-portfolio/hooks/useSessionClasses';
 
 export default function useSubjects(task) {
   const [data, setData] = useState([]);
-  let { subjects } = task;
 
-  const { data: classes } = useSessionClasses({}, { enabled: !subjects?.length });
-  if (!subjects?.length) {
-    subjects =
+  const { data: classes } = useSessionClasses({}, { enabled: !task?.subjects?.length });
+
+  const subjects = useMemo(() => {
+    if (task?.subjects) {
+      return task?.subjects;
+    }
+
+    return (
       classes?.map((klass) => ({
         program: klass.program,
         subject: klass.subject.id,
-      })) || [];
-  }
+      })) || []
+    );
+  }, [task?.subjects, classes]);
 
   useEffect(() => {
     (async () => {
