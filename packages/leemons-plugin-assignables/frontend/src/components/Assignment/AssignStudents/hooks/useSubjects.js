a165350d96af81222ff5b257c40009d3d37dd801
MIGUELez11 <miguel.mangel.ez11@gmail.com>
2022-10-03 12:00:43 +0200

    feat(academic-portfolio): useSubjectDetails returns the subjects ordered by received ids array
    refactor(assignables): useSubjects for assigment now uses better hooks instead of promises

--- packages/leemons-plugin-assignables/frontend/src/components/Assignment/AssignStudents/hooks/useSubjects.js
+++ packages/leemons-plugin-assignables/frontend/src/components/Assignment/AssignStudents/hooks/useSubjects.js
@@ -1,42 +1,26 @@
-import { useState, useEffect, useMemo } from 'react';
-import _ from 'lodash';
-import { getSubjectDetails } from '@academic-portfolio/request/subjects';
+import { useMemo } from 'react';
 import useSessionClasses from '@academic-portfolio/hooks/useSessionClasses';
+import { useSubjectDetails } from '@academic-portfolio/hooks';
 
 export default function useSubjects(task) {
-  const [data, setData] = useState([]);
-
   const { data: classes } = useSessionClasses({}, { enabled: !task?.subjects?.length });
 
   const subjects = useMemo(() => {
     if (task?.subjects) {
-      return task?.subjects;
+      return task?.subjects?.map(({ subject }) => subject);
     }
 
-    return (
-      classes?.map((klass) => ({
-        program: klass.program,
-        subject: klass.subject.id,
-      })) || []
-    );
+    return classes?.map((klass) => klass.subject.id) || [];
   }, [task?.subjects, classes]);
 
-  useEffect(() => {
-    (async () => {
-      if (!subjects?.length) {
-        return;
-      }
+  const { data: subjectDetails } = useSubjectDetails(subjects, { enabled: !!subjects });
 
-      const subjectsData = (await getSubjectDetails(_.map(subjects, 'subject')))?.data;
-
-      setData(
-        subjectsData.map((subject) => ({
-          label: subject.name,
-          value: subject.id,
-        }))
-      );
-    })();
-  }, [subjects]);
-
-  return data;
+  return useMemo(
+    () =>
+      subjectDetails?.map((subject) => ({
+        label: subject.name,
+        value: subject.id,
+      })) || [],
+    [subjectDetails]
+  );
 }
