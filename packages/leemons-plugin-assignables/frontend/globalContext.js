ff69116f7c7340bbff9c08a1de4d1bfe86d75faf
MIGUELez11 <miguel.mangel.ez11@gmail.com>
2022-10-19 14:12:22 +0200

    feat(assignables): Add useIsStudent hook

--- packages/leemons-plugin-assignables/frontend/globalContext.js
+++ packages/leemons-plugin-assignables/frontend/globalContext.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import useIsTeacher from './src/components/Ongoing/AssignmentList/hooks/useIsTeacher';
+import useIsStudent from './src/components/Ongoing/AssignmentList/hooks/useIsStudent';
 import { Provider as LocalProvider } from './src/contexts/globalContext';
 
 export function Provider({ children }) {
   const isTeacher = useIsTeacher();
+  const isStudent = useIsStudent();
+
   return (
     <LocalProvider
       value={{
         isTeacher,
+        isStudent,
       }}
     >
       {children}
