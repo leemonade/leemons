eac59e0ab73f8033d3acfdd6361932972512e432
MIGUELez11 <miguel.mangel.ez11@gmail.com>
2022-11-25 12:48:27 +0100

    fix(common/versionControl): getVersion returns the version info for all the provided fullIds
    When some of the fullIds provided were sharing uuid, the same version was always returned
    
    fix(leebrary/getByCategory): Only return the latests assets

--- packages/leemons-plugin-leebrary/src/services/permissions/getByCategory.js
+++ packages/leemons-plugin-leebrary/src/services/permissions/getByCategory.js
@@ -1,4 +1,17 @@
-const { isEmpty, sortBy, intersection, uniqBy, uniq, forEach, findIndex, map } = require('lodash');
+const {
+  isEmpty,
+  sortBy,
+  intersection,
+  uniqBy,
+  uniq,
+  forEach,
+  findIndex,
+  map,
+  groupBy,
+  find,
+  omit,
+} = require('lodash');
+const semver = require('semver');
 const getRolePermissions = require('./helpers/getRolePermissions');
 const getAssetIdFromPermissionName = require('./helpers/getAssetIdFromPermissionName');
 const { getPublic } = require('./getPublic');
@@ -179,7 +192,41 @@ async function getByCategory(
       results = results.filter(({ asset }) => indexableAssetsObject[asset]);
     }
 
-    return uniqBy(results.concat(publicAssets), 'asset');
+    results = uniqBy(results.concat(publicAssets), 'asset');
+
+    if (preferCurrent) {
+      const versionControlServices = leemons.getPlugin('common').services.versionControl;
+
+      results = await Promise.all(
+        results.map(async (asset) => ({
+          ...(await versionControlServices.parseId(asset.asset, {
+            verifyVersion: false,
+            ignoreMissing: true,
+          })),
+          ...asset,
+        }))
+      );
+
+      // TODO: Remove and use setAsCurrent on asset creation
+      // EN: Filter by preferCurrent status
+      // ES: Filtrar por estado preferCurrent
+      const groupedAssets = groupBy(results, (asset) => asset.uuid);
+
+      // EN: Get the latest versions of each uuid
+      // ES: Obtener la última versión de cada uuid
+      results = map(groupedAssets, (values) => {
+        const versions = map(values, (id) => id.version);
+
+        const latest = semver.maxSatisfying(versions, '*');
+
+        return omit(
+          find(values, (id) => id.version === latest),
+          ['uuid', 'version', 'fullId']
+        );
+      });
+    }
+
+    return uniqBy(results, 'asset');
   } catch (e) {
     throw new global.utils.HttpError(500, `Failed to get permissions: ${e.message}`);
   }
