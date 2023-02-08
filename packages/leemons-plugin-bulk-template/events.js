9e97b9e7b868a701e3dcc00478b484dc90828ec9
Johan Sosa <sosa.johan@gmail.com>
2023-02-08 22:19:19 +0100

    feat(BulkLoad): Added Comunica permissions to Events

--- packages/leemons-plugin-bulk-template/events.js
+++ packages/leemons-plugin-bulk-template/events.js
@@ -85,6 +85,7 @@ async function events(isInstalled) {
         'plugins.scores:init-permissions',
         'plugins.academic-calendar:init-permissions',
         'plugins.content-creator:init-permissions',
+        'plugins.comunica:init-permissions',
         'plugins.leebrary:pluginDidLoadServices',
         'plugins.admin:pluginDidLoadServices',
         'plugins.bulk-template:init-providers',
