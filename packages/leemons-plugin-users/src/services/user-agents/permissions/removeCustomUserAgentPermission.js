361facdccdc53001ae6667b9ad67083133638f88
Jaime Gómez Cimarro <cerberupo@gmail.com>
2023-03-16 15:16:00 +0100

    fix(permissions): Remove cache if one item added or removed.

--- packages/leemons-plugin-users/src/services/user-agents/permissions/removeCustomUserAgentPermission.js
+++ packages/leemons-plugin-users/src/services/user-agents/permissions/removeCustomUserAgentPermission.js
@@ -3,6 +3,31 @@ const { validateUserRemoveCustomPermission } = require('../../../validations/per
 const { existUserAgent } = require('../existUserAgent');
 const { validatePermissionName } = require('../../../validations/exists');
 const { table } = require('../../tables');
+const { removeAllItemsCache } = require('../../item-permissions/removeAllItemsCache');
+
+async function _removeCustomPermission(userAgentId, data, { transacting } = {}) {
+  await existUserAgent({ id: userAgentId }, true, { transacting });
+
+  const query = {
+    permissionName: data.permissionName,
+    userAgent: userAgentId,
+    role_$null: true,
+  };
+
+  if (data.target !== undefined) {
+    query.target = data.target;
+  }
+
+  if (data.center !== undefined) {
+    query.center = data.center;
+  }
+
+  if (data.actionNames) query.actionName_$in = data.actionNames;
+
+  await table.userAgentPermission.deleteMany(query, { transacting });
+
+  return true;
+}
 
 /**
  * Remove permissions from userAgent
@@ -29,37 +54,17 @@ async function removeCustomUserAgentPermission(userAgentId, data, { transacting 
   // console.log(userAgentId, data);
 
   if (_.isArray(userAgentId)) {
-    return global.utils.settledResponseToManyResponse(
+    const response = await global.utils.settledResponseToManyResponse(
       await Promise.allSettled(
         _.map(userAgentId, (id) => _removeCustomPermission(id, data, { transacting }))
       )
     );
+    await removeAllItemsCache();
+    return response;
   }
-  return _removeCustomPermission(userAgentId, data, { transacting });
-}
-
-async function _removeCustomPermission(userAgentId, data, { transacting } = {}) {
-  await existUserAgent({ id: userAgentId }, true, { transacting });
-
-  const query = {
-    permissionName: data.permissionName,
-    userAgent: userAgentId,
-    role_$null: true,
-  };
-
-  if (data.target !== undefined) {
-    query.target = data.target;
-  }
-
-  if (data.center !== undefined) {
-    query.center = data.center;
-  }
-
-  if (data.actionNames) query.actionName_$in = data.actionNames;
-
-  await table.userAgentPermission.deleteMany(query, { transacting });
-
-  return true;
+  const response = await _removeCustomPermission(userAgentId, data, { transacting });
+  await removeAllItemsCache();
+  return response;
 }
 
 module.exports = { removeCustomUserAgentPermission };
