c1a123221b5a1572359f48c33dbb501898b170b1
MIGUELez11 <miguel@leemons.io>
2023-06-13 12:49:03 +0200

    fix(examples/docker): Do not force user to use redis

--- examples/docker/config/cache.js
+++ examples/docker/config/cache.js
@@ -1,12 +1,16 @@
-module.exports = {
-  connection: {
-    connector: 'redis',
-    client: {
-      host: process.env['REDIS_HOST'],
-      port: process.env['REDIS_PORT'],
-      name: process.env['REDIS_DB_NAME'],
-      database: process.env['REDIS_DB_INDEX'],
+if (process.env['REDIS_HOST']) {
+  module.exports = {
+    connection: {
+      connector: 'redis',
+      client: {
+        host: process.env['REDIS_HOST'],
+        port: process.env['REDIS_PORT'],
+        name: process.env['REDIS_DB_NAME'],
+        database: process.env['REDIS_DB_INDEX'],
+      },
+      replicas: true,
     },
-    replicas: true,
-  },
-};
+  };
+}
+
+module.exports = {}
