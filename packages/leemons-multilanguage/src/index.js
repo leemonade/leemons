7f26c40a20264a8c78816a909b4692d257f3846b
Roberto Villares <robertovillaresrama@gmail.com>
2023-07-28 18:50:22 +0200

    feat(multilanguage package): add getTranslationKey method

--- packages/leemons-multilanguage/src/index.js
+++ packages/leemons-multilanguage/src/index.js
@@ -1,7 +1,9 @@
 const { addLocales } = require('./addLocales');
 const { addLocalesDeploy } = require('./addLocalesDeploy');
+const { getTranslationKey } = require('./getTranslationKey');
 
 module.exports = {
   addLocales,
   addLocalesDeploy,
+  getTranslationKey,
 };
