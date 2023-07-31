99020967f9c7b21774c692d25fdf5d622a339576
MIGUELez11 <miguel@leemons.io>
2023-07-31 13:33:20 +0200

    fix(calendar): Remove unnecessary Promise

--- packages/leemons-plugin-calendar/src/services/pluginEvents/class/onAcademicPortfolioRemoveClassTeachers.js
+++ packages/leemons-plugin-calendar/src/services/pluginEvents/class/onAcademicPortfolioRemoveClassTeachers.js
@@ -23,32 +23,28 @@ async function remove(classCalendar, teacher, { transacting }) {
   }
 }
 
-function onAcademicPortfolioRemoveClassTeachers(data, { classIds, classTeachers, transacting }) {
-  // eslint-disable-next-line no-async-promise-executor
-  return new Promise(async (resolve) => {
-    try {
-      const { table } = require('../../tables');
-      const classCalendars = await table.classCalendar.find(
-        { class_$in: classIds },
-        { transacting }
-      );
+async function onAcademicPortfolioRemoveClassTeachers(
+  data,
+  { classIds, classTeachers, transacting }
+) {
+  try {
+    const { table } = require('../../tables');
+    const classCalendars = await table.classCalendar.find({ class_$in: classIds }, { transacting });
 
-      const classCalendarsByClass = _.keyBy(classCalendars, 'class');
+    const classCalendarsByClass = _.keyBy(classCalendars, 'class');
 
-      const promises = [];
-      _.forEach(classIds, (classId) => {
-        _.forEach(classTeachers, ({ teacher }) => {
-          if (classCalendarsByClass[classId]) {
-            promises.push(remove(classCalendarsByClass[classId], teacher, { transacting }));
-          }
-        });
+    const promises = [];
+    _.forEach(classIds, (classId) => {
+      _.forEach(classTeachers, ({ teacher }) => {
+        if (classCalendarsByClass[classId]) {
+          promises.push(remove(classCalendarsByClass[classId], teacher, { transacting }));
+        }
       });
-      await Promise.all(promises);
-      resolve();
-    } catch (e) {
-      console.error(e);
-    }
-  });
+    });
+    await Promise.all(promises);
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 module.exports = { onAcademicPortfolioRemoveClassTeachers };
