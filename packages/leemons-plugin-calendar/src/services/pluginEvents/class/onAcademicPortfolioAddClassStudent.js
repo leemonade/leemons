3aa923a45fb7c1021418d125b31bc8626db6d293
MIGUELez11 <miguel@leemons.io>
2023-07-31 13:31:41 +0200

    fix(calendar): Remove unnecessary Promise

--- packages/leemons-plugin-calendar/src/services/pluginEvents/class/onAcademicPortfolioAddClassStudent.js
+++ packages/leemons-plugin-calendar/src/services/pluginEvents/class/onAcademicPortfolioAddClassStudent.js
@@ -1,32 +1,28 @@
-function onAcademicPortfolioAddClassStudent(data, { class: classId, student, transacting }) {
-  // eslint-disable-next-line no-async-promise-executor
-  return new Promise(async (resolve) => {
+async function onAcademicPortfolioAddClassStudent(data, { class: classId, student, transacting }) {
+  try {
+    const { table } = require('../../tables');
+    const [classCalendar] = await Promise.all([
+      table.classCalendar.findOne({ class: classId }, { transacting }),
+      leemons.plugin.services.calendar.grantAccessUserAgentToCalendar(
+        leemons.plugin.prefixPN(`class.${classId}`),
+        student,
+        'view',
+        { transacting }
+      ),
+    ]);
     try {
-      const { table } = require('../../tables');
-      const [classCalendar] = await Promise.all([
-        table.classCalendar.findOne({ class: classId }, { transacting }),
-        leemons.plugin.services.calendar.grantAccessUserAgentToCalendar(
-          leemons.plugin.prefixPN(`class.${classId}`),
-          student,
-          'view',
-          { transacting }
-        ),
-      ]);
-      try {
-        await leemons.plugin.services.calendar.grantAccessUserAgentToCalendar(
-          leemons.plugin.prefixPN(`program.${classCalendar.program}`),
-          student,
-          'view',
-          { transacting }
-        );
-      } catch (e) {
-        // console.error(e);
-      }
-      resolve();
+      await leemons.plugin.services.calendar.grantAccessUserAgentToCalendar(
+        leemons.plugin.prefixPN(`program.${classCalendar.program}`),
+        student,
+        'view',
+        { transacting }
+      );
     } catch (e) {
-      console.error(e);
+      // console.error(e);
     }
-  });
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 module.exports = { onAcademicPortfolioAddClassStudent };
