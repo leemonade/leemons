a0d892dcdf879ede58a6c9a5f07bdab37273f23f
Roberto Villares <robertovillaresrama@gmail.com>
2023-08-04 13:26:16 +0200

    feat(users): uncomment calls to leebrary microservice

--- plugins/leemons-plugin-users/backend/core/users/addUserAvatar.js
+++ plugins/leemons-plugin-users/backend/core/users/addUserAvatar.js
@@ -7,29 +7,18 @@ async function addUserAvatar({ user, avatar, ctx } = {}) {
   if (avatar) assetData.cover = avatar;
   let asset;
   if (user.avatarAsset) {
-    // TODO migration: Cuando se acabe de migrar el servicio de leebrary descomentar la llamada
-    /*
-      asset = await ctx.tx.call('leebrary.assets.update', {
-        data: { ...assetData, id: user.avatarAsset },
-        published: true,
-      });
-    */
+    asset = await ctx.tx.call('leebrary.assets.update', {
+      data: { ...assetData, id: user.avatarAsset },
+      published: true,
+    });
   } else {
-    // TODO migration: Cuando se acabe de migrar el servicio de leebrary descomentar la llamada
-    /*
-      asset = await ctx.tx.call('leebrary.assets.add', {
-        data: assetData,
-        published: true,
-      });
-    */
+    asset = await ctx.tx.call('leebrary.assets.add', {
+      data: assetData,
+      published: true,
+    });
   }
 
-  // TODO migration: Quitar mock
-  const coverUrl = 'http://google.es/foto';
-  // ? Sería necesario meter await
-  /*
-    const coverUrl = ctx.tx.call('leebrary.assets.getCoverUrl', asset.id);
-  */
+  const coverUrl = await ctx.tx.call('leebrary.assets.getCoverUrl', asset.id);
 
   const u = await ctx.tx.db.Users.findOneAndUpdate(
     { id: user.id },
