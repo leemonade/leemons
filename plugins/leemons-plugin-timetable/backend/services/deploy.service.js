7b27b54afb46eb32b1bddda7d056b2bb67b8a838
Roberto Villares <robertovillaresrama@gmail.com>
2023-08-07 10:50:07 +0200

    feat(timetable): update deploy.service

--- plugins/leemons-plugin-timetable/backend/services/deploy.service.js
+++ plugins/leemons-plugin-timetable/backend/services/deploy.service.js
@@ -10,8 +10,8 @@ const { addLocalesDeploy } = require('leemons-multilanguage');
 const { addPermissionsDeploy } = require('leemons-permissions');
 
 const { LeemonsMultiEventsMixin } = require('leemons-multi-events');
-const { addMenuItemsDeploy } = require('leemons-menu-builder');
-const { permissions, menuItems } = require('../config/constants');
+// const { addMenuItemsDeploy } = require('leemons-menu-builder');
+const { permissions } = require('../config/constants');
 const { getServiceModels } = require('../models');
 
 /** @type {ServiceSchema} */
@@ -25,18 +25,19 @@ module.exports = () => ({
     }),
     LeemonsDeploymentManagerMixin(),
   ],
-  multiEvents: [
-    {
-      events: ['menu-builder.init-main-menu', 'timetable.init-permissions'],
-      handler: async (ctx) => {
-        await addMenuItemsDeploy({
-          keyValueModel: ctx.tx.db.KeyValue,
-          item: menuItems,
-          ctx,
-        });
-      },
-    },
-  ],
+  //! Comentado porque aparecía comentado en el "viejo" events.js
+  // multiEvents: [
+  //   {
+  //     events: ['menu-builder.init-main-menu', 'timetable.init-permissions'],
+  //     handler: async (ctx) => {
+  // await addMenuItemsDeploy({
+  //   keyValueModel: ctx.tx.db.KeyValue,
+  //   item: menuItems,
+  //   ctx,
+  // });
+  //     },
+  //   },
+  // ],
   events: {
     'deployment-manager.install': async (ctx) => {
       // Permissions
@@ -49,11 +50,28 @@ module.exports = () => ({
       // Locales
       await addLocalesDeploy({
         keyValueModel: ctx.tx.db.KeyValue,
+        locale: ['es', 'en'],
+        i18nPath: path.resolve(__dirname, `../i18n/`),
+        ctx,
+      });
+      return null;
+    },
+    'multilanguage.newLocale': async (ctx) => {
+      await addLocalesDeploy({
+        keyValueModel: ctx.tx.db.KeyValue,
         locale: ctx.params.code,
         i18nPath: path.resolve(__dirname, `../i18n/`),
         ctx,
       });
       return null;
+    },
+    'users.init-permissions': async (ctx) => {
+      // Permissions
+      await addPermissionsDeploy({
+        keyValueModel: ctx.tx.db.KeyValue,
+        permissions: permissions.permissions,
+        ctx,
+      });
     },
   },
   created() {
