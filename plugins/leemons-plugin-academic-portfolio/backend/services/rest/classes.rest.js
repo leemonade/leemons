9d010298172ca8ab73667a91e37c0b3c163f2f12
paola-p <>
2023-08-08 11:55:31 +0200

    feat(academic-portfolio): migrate listClassRest controller and related functions within academic-portfolio

--- plugins/leemons-plugin-academic-portfolio/backend/services/rest/classes.rest.js
+++ plugins/leemons-plugin-academic-portfolio/backend/services/rest/classes.rest.js
@@ -5,12 +5,14 @@
  */
 
 const _ = require('lodash');
+const { LeemonsValidator } = require('leemons-validator');
 
 const { haveClasses } = require('../../core/classes/haveClasses');
 const { addClass } = require('../../core/classes/addClass');
 const { updateClass } = require('../../core/classes/updateClass');
 const { updateClassMany } = require('../../core/classes/updateClassMany');
 const { addInstanceClass } = require('../../core/classes/addInstanceClass');
+const { listClasses } = require('../../core/classes/listClasses');
 
 /** @type {ServiceSchema} */
 module.exports = {
@@ -70,6 +72,38 @@ module.exports = {
     async handler(ctx) {
       const _class = await addInstanceClass({ data: ctx.request.body, ctx });
       return { status: 200, class: _class };
+    },
+  },
+  listClassRest: {
+    rest: {
+      path: '/class',
+      method: 'GET',
+    },
+    async handler(ctx) {
+      const validator = new LeemonsValidator({
+        type: 'object',
+        properties: {
+          page: { type: ['number', 'string'] },
+          size: { type: ['number', 'string'] },
+          program: { type: 'string' },
+        },
+        required: ['page', 'size', 'program'],
+        additionalProperties: false,
+      });
+      if (validator.validate(ctx.params.query)) {
+        const { page, size, program, ...options } = ctx.params.query; // TODO @ask Confirmar que: ctx.request.query === ctx.params.query
+        const data = await listClasses({
+          page: parseInt(page, 10),
+          size: parseInt(size, 10),
+          program,
+          query: {
+            ...options,
+          },
+          ctx,
+        });
+        return { status: 200, data };
+      }
+      throw validator.error;
     },
   },
 };
