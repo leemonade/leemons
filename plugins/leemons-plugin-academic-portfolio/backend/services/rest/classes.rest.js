024b44376b9c8fe1c26ae319d77950413056a938
paola-p <>
2023-08-08 14:35:28 +0200

    feat(academic-portfolio): migrate postClassStudents, postClassTeacher, listStudentClasses controllers and related functions within academic-portfolio

--- plugins/leemons-plugin-academic-portfolio/backend/services/rest/classes.rest.js
+++ plugins/leemons-plugin-academic-portfolio/backend/services/rest/classes.rest.js
@@ -14,6 +14,9 @@ const { updateClassMany } = require('../../core/classes/updateClassMany');
 const { addInstanceClass } = require('../../core/classes/addInstanceClass');
 const { listClasses } = require('../../core/classes/listClasses');
 const { listSubjectClasses } = require('../../core/classes/listSubjectClasses');
+const { addClassStudentsMany } = require('../../core/classes/addClassStudentsMany');
+const { addClassTeachersMany } = require('../../core/classes/addClassTeachersMany');
+const { listStudentClasses } = require('../../core/classes/listStudentClasses');
 
 /** @type {ServiceSchema} */
 module.exports = {
@@ -61,7 +64,7 @@ module.exports = {
       method: 'PUT',
     },
     async handler(ctx) {
-      const classes = await updateClassMany({ data: ctx.params.data, ctx });
+      const classes = await updateClassMany({ data: ctx.params, ctx });
       return { status: 200, classes };
     },
   },
@@ -71,7 +74,7 @@ module.exports = {
       method: 'POST',
     },
     async handler(ctx) {
-      const _class = await addInstanceClass({ data: ctx.request.body, ctx });
+      const _class = await addInstanceClass({ data: ctx.params, ctx });
       return { status: 200, class: _class };
     },
   },
@@ -91,8 +94,8 @@ module.exports = {
         required: ['page', 'size', 'program'],
         additionalProperties: false,
       });
-      if (validator.validate(ctx.params.query)) {
-        const { page, size, program, ...options } = ctx.params.query; // TODO @ask Confirmar que: ctx.request.query === ctx.params.query
+      if (validator.validate(ctx.params)) {
+        const { page, size, program, ...options } = ctx.params;
         const data = await listClasses({
           page: parseInt(page, 10),
           size: parseInt(size, 10),
@@ -123,16 +126,79 @@ module.exports = {
           additionalProperties: false,
         });
 
-        if (validator.validate(ctx.params.query)) {
-          const { page, size, subject } = ctx.params.query;
+        if (validator.validate(ctx.params)) {
+          const { page, size, subject } = ctx.params;
 
           const data = await listSubjectClasses({
             page: parseInt(page, 10),
             size: parseInt(size, 10),
             subject,
             query: {
-              userSession: ctx.tx.meta.userSession, // TODO ask: Esto estaba así ctx.state.userSession, correcto?
+              userSession: ctx.meta.userSession,
             },
+            ctx,
+          });
+          return { status: 200, data };
+        }
+        throw validator.error;
+      },
+    },
+    postClassStudentsRest: {
+      rest: {
+        path: '/class/students',
+        method: 'POST',
+      },
+      async handler(ctx) {
+        if (process.env.NODE_ENV !== 'production') {
+          if (!_.isArray(ctx.params.students)) {
+            ctx.params.students = [];
+          }
+          ctx.params.students.push(ctx.meta.userSession.userAgents[0].id); // TODO ask Esto estaba así ctx.state.userSession.userAgents[0].id, correcto?
+        }
+        const _class = await addClassStudentsMany({ data: ctx.params, ctx });
+        return { status: 200, class: _class };
+      },
+    },
+    postClassTeachersRest: {
+      rest: {
+        path: '/class/teachers',
+        method: 'POST',
+      },
+      async handler(ctx) {
+        if (process.env.NODE_ENV !== 'production') {
+          if (!_.isArray(ctx.params.teachers)) {
+            ctx.params.teachers = [];
+          }
+          ctx.params.teachers.push({
+            teacher: ctx.meta.userSession.userAgents[0].id,
+            type: 'teacher',
+          });
+        }
+        const _class = await addClassTeachersMany({ data: ctx.params, ctx });
+        return { status: 200, class: _class };
+      },
+    },
+    listStudentClassesRest: {
+      rest: {
+        path: '/student/:id/classes',
+        method: 'GET',
+      },
+      async handler(ctx) {
+        const validator = new LeemonsValidator({
+          type: 'object',
+          properties: {
+            page: { type: ['number', 'string'] },
+            size: { type: ['number', 'string'] },
+          },
+          required: ['page', 'size'],
+          additionalProperties: false,
+        });
+        if (validator.validate(ctx.param)) {
+          const { page, size } = ctx.param;
+          const data = await listStudentClasses({
+            page: parseInt(page, 10),
+            size: parseInt(size, 10),
+            student: ctx.request.params.id,
             ctx,
           });
           return { status: 200, data };
