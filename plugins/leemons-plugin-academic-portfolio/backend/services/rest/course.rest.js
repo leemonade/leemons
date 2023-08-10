793feb57125129120626bdf84b3b605184df0d0a
paola-p <>
2023-08-10 10:17:22 +0200

    feat(academic-portfolio): migrate core/courses folder all functions

--- plugins/leemons-plugin-academic-portfolio/backend/services/rest/course.rest.js
+++ plugins/leemons-plugin-academic-portfolio/backend/services/rest/course.rest.js
@@ -4,5 +4,26 @@
  * @typedef {import('moleculer').Context} Context Moleculer's Context
  */
 
+const { updateCourse } = require('../../core/courses');
+
 /** @type {ServiceSchema} */
-module.exports = {};
+module.exports = {
+  postCourseRest: {
+    rest: {},
+    async handler() {
+      /*
+      const course = await courseService.addCourse(ctx.request.body);
+      ctx.status = 200;
+      ctx.body = { status: 200, course };
+      */
+      return { status: 400, message: 'Course creation disabled' };
+    },
+  },
+  putCourse: {
+    rest: {},
+    async handler(ctx) {
+      const course = await updateCourse(ctx.request.body);
+      return { status: 200, course };
+    },
+  },
+};
