64939fb23424ab1850280ecc9a37015f28b02188
paola-p <>
2023-08-10 10:37:03 +0200

    feat(academic-portfolio): migrate all controllers all and all functions for cycles

--- plugins/leemons-plugin-academic-portfolio/backend/core/cycle/updateCycle.js
+++ plugins/leemons-plugin-academic-portfolio/backend/core/cycle/updateCycle.js
@@ -1,23 +1,16 @@
-const { table } = require('../tables');
 const { validateUpdateCycle } = require('../../validations/forms');
 const { saveManagers } = require('../managers/saveManagers');
 
-async function updateCycle(data, { transacting: _transacting } = {}) {
-  return global.utils.withTransaction(
-    async (transacting) => {
-      await validateUpdateCycle(data, { transacting });
+async function updateCycle({ data, ctx }) {
+  await validateUpdateCycle(data);
 
-      const { id, managers, ..._data } = data;
+  const { id, managers, ..._data } = data;
 
-      const [cycle] = await Promise.all([
-        table.cycles.update({ id }, _data, { transacting }),
-        saveManagers(managers, 'cycle', id, { transacting }),
-      ]);
-      return cycle;
-    },
-    table.cycles,
-    _transacting
-  );
+  const [cycle] = await Promise.all([
+    ctx.tx.db.Cycles.findOneAndUpdate({ id }, _data, { new: true }),
+    saveManagers({ userAgents: managers, type: 'cycle', relationship: id, ctx }),
+  ]);
+  return cycle;
 }
 
 module.exports = { updateCycle };
