3d2bba6225468456a49617b1c2c2708d62d676be
Roberto Villares <robertovillaresrama@gmail.com>
2023-08-10 09:09:49 +0200

    feat(accademic-portfolio): courses, groups, knowledges, subject-type, subjects & substages methods

--- plugins/leemons-plugin-academic-portfolio/backend/core/subjects/removeSubjectByIds.js
+++ plugins/leemons-plugin-academic-portfolio/backend/core/subjects/removeSubjectByIds.js
@@ -1,48 +1,30 @@
 const _ = require('lodash');
-const { table } = require('../tables');
 const { removeSubjectCreditsBySubjectsIds } = require('./removeSubjectCreditsBySubjectsIds');
 
-async function removeSubjectByIds(ids, { userSession, soft, transacting: _transacting } = {}) {
-  return global.utils.withTransaction(
-    async (transacting) => {
-      const subjects = await table.subjects.find(
-        { id_$in: _.isArray(ids) ? ids : [ids] },
-        { transacting }
-      );
-      await leemons.events.emit('before-remove-subjects', { subjects, soft, transacting });
-      const assetService = leemons.getPlugin('leebrary').services.assets;
-      await Promise.all([
-        Promise.allSettled(
-          _.map(subjects, (subject) =>
-            assetService.remove(
-              { id: subject.image },
-              {
-                userSession,
-                transacting,
-              }
-            )
-          )
-        ),
-        Promise.allSettled(
-          _.map(subjects, (subject) =>
-            assetService.remove(
-              { id: subject.icon },
-              {
-                userSession,
-                transacting,
-              }
-            )
-          )
-        ),
-      ]);
-      await removeSubjectCreditsBySubjectsIds(_.map(subjects, 'id'), { soft, transacting });
-      await table.subjects.deleteMany({ id_$in: _.map(subjects, 'id') }, { soft, transacting });
-      await leemons.events.emit('after-remove-subjects', { subjects, soft, transacting });
-      return true;
-    },
-    table.subjects,
-    _transacting
-  );
+async function removeSubjectByIds({ ids, soft, ctx }) {
+  const subjects = await ctx.tx.db.Subjects.find({ id: _.isArray(ids) ? ids : [ids] }).lean();
+  await ctx.tx.emit('before-remove-subjects', { subjects, soft });
+
+  await Promise.all([
+    Promise.allSettled(
+      _.map(subjects, (subject) =>
+        ctx.tx.call('leebrary.assets.remove', {
+          id: { id: subject.image },
+        })
+      )
+    ),
+    Promise.allSettled(
+      _.map(subjects, (subject) =>
+        ctx.tx.call('leebrary.assets.remove', {
+          id: { id: subject.icon },
+        })
+      )
+    ),
+  ]);
+  await removeSubjectCreditsBySubjectsIds({ subjectIds: _.map(subjects, 'id'), soft, ctx });
+  await ctx.tx.db.Subjects.deleteMany({ id: _.map(subjects, 'id'), soft });
+  await ctx.tx.emit('after-remove-subjects', { subjects, soft });
+  return true;
 }
 
 module.exports = { removeSubjectByIds };
