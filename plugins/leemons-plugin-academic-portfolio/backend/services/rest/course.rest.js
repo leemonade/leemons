c9c6b29091b64a02714c887621014fb9699c5b36
paola-p <>
2023-08-10 10:25:24 +0200

    feat(academic-portfolio): migrate all controllers, services and core folder for courses

--- plugins/leemons-plugin-academic-portfolio/backend/services/rest/course.rest.js
+++ plugins/leemons-plugin-academic-portfolio/backend/services/rest/course.rest.js
@@ -3,27 +3,60 @@
  * @typedef {import('moleculer').ServiceSchema} ServiceSchema Moleculer's Service Schema
  * @typedef {import('moleculer').Context} Context Moleculer's Context
  */
-
-const { updateCourse } = require('../../core/courses');
+const { LeemonsValidator } = require('leemons-validator');
+const { updateCourse, listCourses } = require('../../core/courses');
 
 /** @type {ServiceSchema} */
 module.exports = {
   postCourseRest: {
-    rest: {},
+    rest: {
+      path: '/course',
+      method: 'POST',
+    },
     async handler() {
       /*
-      const course = await courseService.addCourse(ctx.request.body);
-      ctx.status = 200;
-      ctx.body = { status: 200, course };
+      const course = await courseService.addCourse(ctx.params;
+      return { status: 200, course };
       */
       return { status: 400, message: 'Course creation disabled' };
     },
   },
-  putCourse: {
-    rest: {},
+  putCourseRest: {
+    rest: {
+      path: '/course',
+      method: 'PUT',
+    },
     async handler(ctx) {
-      const course = await updateCourse(ctx.request.body);
+      const course = await updateCourse({ data: ctx.params, ctx });
       return { status: 200, course };
+    },
+  },
+  listCourseRest: {
+    rest: {
+      path: '/course',
+      method: 'GET',
+    },
+    async handler(ctx) {
+      const validator = new LeemonsValidator({
+        type: 'object',
+        properties: {
+          page: { type: ['number', 'string'] },
+          size: { type: ['number', 'string'] },
+          program: { type: 'string' },
+        },
+        required: ['page', 'size', 'program'],
+        additionalProperties: false,
+      });
+      if (validator.validate(ctx.params)) {
+        const { page, size, program } = ctx.params;
+        const data = await listCourses({
+          page: parseInt(page, 10),
+          size: parseInt(size, 10),
+          program,
+        });
+        return { status: 200, data };
+      }
+      throw validator.error;
     },
   },
 };
