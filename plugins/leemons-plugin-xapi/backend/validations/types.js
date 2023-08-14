7610ba9465f8f0015adf8eaab6f7c6e28fc80e50
Roberto Villares <robertovillaresrama@gmail.com>
2023-08-14 08:59:52 +0200

    feat(xapi): migrate folder structure and all models

---
+++ plugins/leemons-plugin-xapi/backend/validations/types.js
@@ -0,0 +1,68 @@
+const textSchema = {
+  type: 'string',
+  minLength: 1,
+  maxLength: 65000,
+};
+
+const numberSchema = {
+  type: 'number',
+};
+
+const stringSchema = {
+  type: 'string',
+  minLength: 1,
+  maxLength: 255,
+};
+
+const stringSchemaNullable = {
+  type: 'string',
+  minLength: 1,
+  maxLength: 255,
+  nullable: true,
+};
+
+const arrayStringSchema = {
+  type: 'array',
+  items: {
+    type: 'string',
+  },
+};
+
+const arrayStringRequiredSchema = {
+  type: 'array',
+  items: {
+    type: 'string',
+  },
+  minItems: 1,
+};
+
+const dateSchema = {
+  type: 'string',
+  format: 'date-time',
+};
+
+const booleanSchema = {
+  type: 'boolean',
+};
+
+const integerSchema = {
+  type: 'integer',
+};
+
+const integerSchemaNullable = {
+  type: 'integer',
+  nullable: true,
+};
+
+module.exports = {
+  dateSchema,
+  textSchema,
+  numberSchema,
+  stringSchema,
+  booleanSchema,
+  integerSchema,
+  arrayStringSchema,
+  stringSchemaNullable,
+  integerSchemaNullable,
+  arrayStringRequiredSchema,
+};
