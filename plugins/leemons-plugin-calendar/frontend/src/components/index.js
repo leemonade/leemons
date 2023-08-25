ab1b331cdd7fcf427824477661aaf02f668892b2
fermarinsanchez <fermarinsanchez@yahoo.es>
2023-08-25 11:30:07 +0200

    feat(calendar): add Kanban components to calendar plugin
    Ref: #task/ReactApp-1

--- plugins/leemons-plugin-calendar/frontend/src/components/index.js
+++ plugins/leemons-plugin-calendar/frontend/src/components/index.js
@@ -2,3 +2,5 @@ export * from './CalendarSubNavFilters';
 export * from './CalendarEventModal';
 export * from './EventDetailPanel';
 export * from './CalendarNewEventModal';
+export * from './KanbanTaskCard';
+export * from './KanbanFilters';
