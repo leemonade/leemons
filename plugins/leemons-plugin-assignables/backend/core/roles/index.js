c33620b9431827bc70d0a4104a05b7ee5d58dc27
MIGUELez11 <miguel@leemons.io>
2023-08-29 11:27:38 +0200

    fix(assignables): Add index.js to roles

---
+++ plugins/leemons-plugin-assignables/backend/core/roles/index.js
@@ -0,0 +1,11 @@
+const { registerRole } = require('./registerRole');
+const { getRole } = require('./getRole');
+const { listRoles } = require('./listRoles');
+const { unregisterRole } = require('./unregisterRole');
+
+module.exports = {
+  registerRole,
+  getRole,
+  listRoles,
+  unregisterRole,
+};
