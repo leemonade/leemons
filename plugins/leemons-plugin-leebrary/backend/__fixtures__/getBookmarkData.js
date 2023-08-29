3954ffeeb1a5096afb25dade712cee1196500aeb
paola-p <>
2023-08-29 18:31:14 +0200

    feat(leebrary): add __fixtures__ folder with getAssets.js and getBookmarkData.js for testing

---
+++ plugins/leemons-plugin-leebrary/backend/__fixtures__/getBookmarkData.js
@@ -0,0 +1,28 @@
+module.exports = function getBookmarkData() {
+  return {
+  dataInput: {
+      name: 'Tecnología de aprendizaje colaborativo para pedagogías activas',
+      description: 'La única herramienta que da soporte a las metodologías de enseñanza más exitosas: Aprendizaje por proyectos, Role Playing, Cooperativo, Flipped Classroom, Design Thinking…',
+      color: '#993333',
+      url: 'https://www.leemons.io/',
+      program: null,
+      subjects: null,
+      icon: 'https://uploads-ssl.webflow.com/63d7ab145d360ca1dd2d418f/63da71ec56119768e6afc9b8_Favicon.png',
+      categoryId: '0d144de4-023d-49f6-a959-5f163dd47cbd',
+      tags: ['Leemons'],
+      categoryKey: 'bookmarks',
+    },
+    dataInputWithEmptyFields: {
+      name: null,
+      color: '#993333',
+      url: 'https://www.leemons.io/',
+      program: null,
+      subjects: null,
+      icon: null,
+      categoryId: '0d144de4-023d-49f6-a959-5f163dd47cbd',
+      tags: ['Leemons'],
+      categoryKey: 'bookmarks',
+    },
+    cover: 'https://uploads-ssl.webflow.com/63d7ab145d360ca1dd2d418f/63da71ec56119768e6afc9b8_Favicon.png',
+  };
+}
