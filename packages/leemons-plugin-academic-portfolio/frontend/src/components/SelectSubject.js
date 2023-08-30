d68783038c7572e6177bb8e797a5078667740088
Fernando Marín Sánchez <46426271+fermarinsanchez@users.noreply.github.com>
2023-08-30 12:05:15 +0200

    refactor: Use the BulletSubject to replace the old components
    Ref: #issue/BubblesDS-0

--- packages/leemons-plugin-academic-portfolio/frontend/src/components/SelectSubject.js
+++ packages/leemons-plugin-academic-portfolio/frontend/src/components/SelectSubject.js
@@ -1,11 +1,13 @@
 import { getClassIcon } from '@academic-portfolio/helpers/getClassIcon';
-import { Box, ImageLoader, Select, Text } from '@bubbles-ui/components';
+import { Box, Select, Text, BulletSubject } from '@bubbles-ui/components';
 import React from 'react';
 
 export function SubjectItem({ subject, isValueComponent, ...props }) {
   if (!subject) {
     return null;
   }
+
+  const iconSubject = typeof subject?.icon === 'string' ? subject.icon : getClassIcon({ subject });
 
   return (
     <Box {...props}>
@@ -19,30 +21,12 @@ export function SubjectItem({ subject, isValueComponent, ...props }) {
           width: isValueComponent && '22ch',
         })}
       >
-        <Box
-          sx={() => ({
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            minWidth: 20,
-            minHeight: 20,
-            maxWidth: 20,
-            maxHeight: 20,
-            borderRadius: '50%',
-            backgroundColor: subject?.color,
-          })}
-        >
-          <ImageLoader
-            sx={() => ({
-              borderRadius: 0,
-              filter: 'brightness(0) invert(1)',
-            })}
-            forceImage
-            width={16}
-            height={16}
-            src={typeof subject?.icon === 'string' ? subject.icon : getClassIcon({ subject })}
-          />
-        </Box>
+        <BulletSubject
+          color={subject?.color}
+          icon={iconSubject}
+          altText={subject?.name}
+          size={'sm'}
+        />
         <Text truncated>{subject.label}</Text>
       </Box>
     </Box>
