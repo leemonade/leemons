c280d02804352d875517c5ac8d5332113313f3ec
Roberto Villares <robertovillaresrama@gmail.com>
2023-09-12 13:34:44 +0200

    feat (leemons-leebrary): migrate and add testing to core/bookmarks

---
+++ plugins/leemons-plugin-leebrary/backend/core/bookmarks/remove/remove.unit.test.js
@@ -0,0 +1,120 @@
+const { it, expect, beforeAll, afterAll, beforeEach } = require('@jest/globals');
+const { generateCtx, createMongooseConnection } = require('leemons-testing');
+const { newModel } = require('leemons-mongodb');
+
+const { remove } = require('./remove');
+const { bookmarksSchema } = require('../../../models/bookmarks');
+
+let mongooseConnection;
+let disconnectMongoose;
+let ctx;
+let bookmark;
+
+jest.mock('../getByAsset');
+const { getByAsset } = require('../getByAsset');
+
+jest.mock('../../files/remove');
+const { remove: removeFiles } = require('../../files/remove');
+
+beforeAll(async () => {
+  const { mongoose, disconnect } = await createMongooseConnection();
+
+  mongooseConnection = mongoose;
+  disconnectMongoose = disconnect;
+});
+
+afterAll(async () => {
+  await disconnectMongoose();
+
+  mongooseConnection = null;
+  disconnectMongoose = null;
+});
+
+beforeEach(async () => {
+  await mongooseConnection.dropDatabase();
+  ctx = generateCtx({
+    models: {
+      Bookmarks: newModel(mongooseConnection, 'ExampleModel', bookmarksSchema),
+    },
+  });
+  bookmark = {
+    id: 'bookmarkId',
+    asset: 'assetId',
+    url: 'https://test1.com',
+    icon: 'icon1',
+  };
+
+  await ctx.tx.db.Bookmarks.create(bookmark);
+});
+
+it('Should remove the bookmark', async () => {
+  // Arrange
+
+  getByAsset.mockResolvedValue(bookmark);
+
+  // Act
+  const response = await remove({ assetId: bookmark.asset, ctx });
+  const removedBookmark = await ctx.tx.db.Bookmarks.findOne({ id: bookmark.id });
+
+  // Assert
+  expect(removeFiles).toBeCalledWith({ fileIds: bookmark.icon, assetId: bookmark.asset, ctx });
+  expect(response).toEqual({ acknowledged: true, deletedCount: 1 });
+  expect(removedBookmark).toBe(null);
+});
+
+it('Should soft remove the bookmark and icon file', async () => {
+  // Arrange
+
+  getByAsset.mockResolvedValue(bookmark);
+
+  // Act
+  const response = await remove({ assetId: bookmark.asset, soft: true, ctx });
+  const removedBookmark = await ctx.tx.db.Bookmarks.findOne(
+    {},
+    { id: bookmark.id },
+    { excludeDeleted: false }
+  );
+
+  // Assert
+  expect(response.deletedCount).not.toBeDefined();
+  expect(removedBookmark).toBeDefined();
+});
+
+it('Should soft remove the bookmark without icon file', async () => {
+  // Arrange
+
+  getByAsset.mockResolvedValue({ ...bookmark, icon: undefined });
+
+  // Act
+  const response = await remove({ assetId: bookmark.asset, soft: true, ctx });
+  const removedBookmark = await ctx.tx.db.Bookmarks.findOne(
+    {},
+    { id: bookmark.id },
+    { excludeDeleted: false }
+  );
+
+  // Assert
+  expect(response.deletedCount).not.toBeDefined();
+  expect(removedBookmark).toBeDefined();
+});
+
+it('Should throw an error when the bookmark is not found', async () => {
+  // Arrange
+  getByAsset.mockResolvedValue(null);
+  const testFunc = async () => remove({ assetId: 'nonexistentAssetId', ctx });
+
+  // Act and Assert
+  await expect(testFunc()).rejects.toThrow('Bookmark not found');
+});
+
+it('Should throw an error when deletion fails', async () => {
+  // Arrange
+  getByAsset.mockResolvedValue(bookmark);
+  removeFiles.mockImplementation(() => {
+    throw new Error();
+  });
+  const testFunc = async () => remove({ assetId: bookmark.asset, ctx });
+
+  // Act and Assert
+  await expect(testFunc()).rejects.toThrow(`Failed to remove bookmark`);
+});
