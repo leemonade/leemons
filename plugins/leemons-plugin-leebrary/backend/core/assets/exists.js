a412b07acc3130fea8b4d60e0692d0a7ba281a6d
paola-p <>
2023-09-12 10:07:06 +0200

    feat(library): migrate exists + add its test suite and tiny refactor

--- plugins/leemons-plugin-leebrary/backend/core/assets/exists.js
+++ plugins/leemons-plugin-leebrary/backend/core/assets/exists.js
@@ -1,4 +1,13 @@
+/**
+ * Check if an asset exists by its ID
+ *
+ * @param {string} assetId - The ID of the asset
+ * @param {object} transacting - The transaction object
+ * @returns {Promise<boolean>} - Returns true if the asset exists, false otherwise
+ */
+
 async function exists({ assetId, ctx } = {}) {
+  if (!assetId?.length) return false;
   const count = await ctx.tx.db.Assets.countDocuments({ id: assetId });
   return count > 0;
 }
