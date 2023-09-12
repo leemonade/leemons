34caf92c239763f8fc48c7549c4e1a06c107a31c
paola-p <>
2023-09-12 14:18:46 +0200

    feat(library): migrate core/assets/getIndexables.js + add test suite

--- plugins/leemons-plugin-leebrary/backend/core/assets/getIndexables.js
+++ plugins/leemons-plugin-leebrary/backend/core/assets/getIndexables.js
@@ -1,7 +1,17 @@
-const { tables } = require('../tables');
+/**
+ * Retrieves indexable assets based on provided asset IDs and selected columns.
+ *
+ * @async
+ * @function getIndexables
+ * @param {Object} params - Parameters for the function.
+ * @param {Array.<string>} params.assetIds - Array of asset IDs to retrieve. Default is an empty array.
+ * @param {Array.<string>|string} params.columns - Array of column names to select in the query.
+ * @param {MoleculerContext} params.ctx - Moleculer context.
+ * @returns {Promise<Array.<Object>>} Array of indexable assets.
+ */
 
-async function getIndexables(assetIds = [], { columns, transacting } = {}) {
-  return tables.assets.find({ id_$in: assetIds, indexable: true }, { columns, transacting });
+async function getIndexables({ assetIds = [], columns, ctx }) {
+  return ctx.tx.db.Assets.find({ id: assetIds, indexable: true }).select(columns).lean();
 }
 
 module.exports = { getIndexables };
