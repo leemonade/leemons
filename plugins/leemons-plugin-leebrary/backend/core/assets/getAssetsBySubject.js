77c7588ac8d623bc2d1a71f940aa6452aa3d0466
paola-p <>
2023-09-12 12:18:10 +0200

    feat(library): migrate core/assets/getAssetsBySubject + add unit tests

--- plugins/leemons-plugin-leebrary/backend/core/assets/getAssetsBySubject.js
+++ plugins/leemons-plugin-leebrary/backend/core/assets/getAssetsBySubject.js
@@ -1,17 +1,27 @@
-const { isArray, map, uniq } = require('lodash');
-const { tables } = require('../tables');
+const { map, uniq } = require('lodash');
+const { normalizeItemsArray } = require('../shared');
 
-async function getAssetsBySubject(subject, { assets, transacting }) {
-  const subjects = isArray(subject) ? subject : [subject];
-  const query = {
-    subject_$in: subjects,
-  };
-  if (isArray(assets) && assets.length) {
-    query.asset_$in = assets;
-  } else {
+/**
+ * Get assets by subject
+ * @param {Object} params - The parameters object
+ * @param {Array|any} params.subject - The subject(s)
+ * @param {Array} params.assets - The assets
+ * @param {MoleculerContext} params.ctx - The Moleculer context
+ * @returns {Promise<Array>} - Returns an array of asset IDs
+ */
+async function getAssetsBySubject({ subject, assets, ctx }) {
+  const subjects = normalizeItemsArray(subject);
+  const assetsArray = normalizeItemsArray(assets);
+
+  if (!assetsArray.length) {
     return [];
   }
-  const _assets = await tables.assetsSubjects.find(query, { columns: ['asset'], transacting });
+
+  const query = {
+    subject: subjects,
+    asset: assetsArray,
+  };
+  const _assets = await ctx.tx.db.AssetsSubjects.find(query).select(['asset']).lean();
   return uniq(map(_assets, 'asset'));
 }
 
