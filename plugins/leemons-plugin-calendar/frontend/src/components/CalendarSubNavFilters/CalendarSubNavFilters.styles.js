44f4aaae5b28ba480edea00800a1b972e7149448
fermarinsanchez <fermarinsanchez@yahoo.es>
2023-09-13 09:02:01 +0200

    fix(CalendarSubNavFilters): Fix styles in SegmentedControlButtons Ref: #task/Calendar-2

--- plugins/leemons-plugin-calendar/frontend/src/components/CalendarSubNavFilters/CalendarSubNavFilters.styles.js
+++ plugins/leemons-plugin-calendar/frontend/src/components/CalendarSubNavFilters/CalendarSubNavFilters.styles.js
@@ -61,13 +61,21 @@ export const CalendarSubNavFiltersStyles = createStyles((theme, { mainColor, lig
     },
     segmentLabelActive: {
       color: '#307AE8 !important',
-      border: '1px solid #307AE8',
     },
     segmentActive: {
+      color: '#307AE8 !important',
+      border: '1px solid #307AE8',
       backgroundColor: lightMode ? theme.colors.mainWhite : 'rgba(255, 255, 255, 0.25)',
-      // backgroundColor: theme.colors.interactive02d,
+      '&:first-child': {
+        borderRadius: '4px 4px 0 0',
+      },
+      '&:last-child': {
+        borderRadius: '0 0 4px 4px',
+      },
       top: 4,
       left: 4,
+      boxShadow: 'none',
+      width: '131px !important',
     },
     segmentControl: {
       border: 'none!important',
