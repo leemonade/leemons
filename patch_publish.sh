fc28486c545dd5af5fe3b6b1d1d03f0af4dfe80a
Jaime Gómez Cimarro <jaime@leemons.io>
2023-09-14 10:31:57 +0200

    fix: Triying fix version replace.

--- patch_publish.sh
+++ patch_publish.sh
@@ -47,7 +47,8 @@ do
 
     # Busca y reemplaza la versión en todos los archivos package.json donde el nombre del paquete aparece como una dependencia
     #find . -name package.json -exec sed -i "s/\"$pkg_name\": \"[^\"]*\"/\"$pkg_name\": \"$pkg_version\"/g" {} \;
-    find . -name package.json -exec sed -i "s#\"$pkg_name\": \"[^\"]*\"#\"$pkg_name\": \"$pkg_version\"#g" {} \;
+    #find . -name package.json -exec sed -i "s#\"$pkg_name\": \"[^\"]*\"#\"$pkg_name\": \"$pkg_version\"#g" {} \;
+    find . -name package.json -exec sed -i "s|\"$pkg_name\": \"[^\"]*\"|\"$pkg_name\": \"$pkg_version\"|g" {} \;
 
     # Vuelve al directorio original
     cd -
