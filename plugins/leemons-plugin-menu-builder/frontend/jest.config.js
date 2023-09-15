4ee0024c7d05068a27ed671b42abafe03fca6d41
fermarinsanchez <fermarinsanchez@yahoo.es>
2023-09-15 23:51:31 +0200

    feat(MainNavBar): Working on Jest config Ref: #epic/NavMenu-1

--- plugins/leemons-plugin-menu-builder/frontend/jest.config.js
+++ plugins/leemons-plugin-menu-builder/frontend/jest.config.js
@@ -1,20 +1,23 @@
-/** @type {import('jest').Config} */
+// /** @type {import('jest').Config} */
 
 console.log('El archivo de configuración de Jest se está cargando.');
 console.log('directory', __dirname);
 
 module.exports = {
-  rootDir: __dirname,
+  verbose: true,
+  bail: true,
   collectCoverage: false,
-  collectCoverageFrom: ['./src/**/*.{js,jsx}'],
+  collectCoverageFrom: ['src/**/*.{js,jsx}'],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
+  transform: {
+    '\\.(css|less|scss|sass)$': '<rootDir>/___mocks___/css-mock.js',
+    '^.+\\.jsx?$': 'babel-jest',
+  },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  // transform: {
-  //   '^.+\\.js?$': 'babel-jest',
-  // },
-  transformIgnorePatterns: ['/node_modules/', '/bubbles/'],
+  transformIgnorePatterns: ['/node_modules/', '.*\\.css', '../.../.../node_modules/(?!(chalk)/)'],
   moduleNameMapper: {
-    '^@bubbles-ui/components$': '<rootDir>/../../../node_modules/@bubbles-ui/components',
+    '\\.(css|less|scss|sass)$': '<rootDir>/___mocks___/css-mock.js',
+    uuid: require.resolve('uuid'),
   },
 };
