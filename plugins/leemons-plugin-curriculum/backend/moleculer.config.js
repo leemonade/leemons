35eafad8e91f8601045869c0d1b3aecb1ec4ce5d
MIGUELez11 <miguel@leemons.io>
2023-09-19 15:38:59 +0200

    fix: remove all the nodeIDs from the molculer.config
    Avoid nodeID duplicated on multiple pods running in the same nats

--- plugins/leemons-plugin-curriculum/backend/moleculer.config.js
+++ plugins/leemons-plugin-curriculum/backend/moleculer.config.js
@@ -32,7 +32,7 @@ module.exports = {
   // Namespace of nodes to segment your nodes on the same network.
   namespace: '',
   // Unique node identifier. Must be unique in a namespace.
-  nodeID: 'leemons-curriculum',
+  nodeID: null,
   // Custom metadata store. Store here what you want. Accessing: `this.broker.metadata`
   metadata: {},
 
--- plugins/leemons-plugin-onboarding/backend/moleculer.config.js
+++ plugins/leemons-plugin-onboarding/backend/moleculer.config.js
@@ -29,7 +29,7 @@ module.exports = {
   // Namespace of nodes to segment your nodes on the same network.
   namespace: '',
   // Unique node identifier. Must be unique in a namespace.
-  nodeID: 'leemons-onboarding',
+  nodeID: null,
   // Custom metadata store. Store here what you want. Accessing: `this.broker.metadata`
   metadata: {},
 
