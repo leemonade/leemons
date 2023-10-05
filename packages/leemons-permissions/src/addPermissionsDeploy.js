dc35fc82342cf0e89d65f586b62a8c37bc6a006e
Jaime Gómez Cimarro <cerberupo@gmail.com>
2023-10-05 18:15:48 +0200

    feat: Added log

--- packages/leemons-permissions/src/addPermissionsDeploy.js
+++ packages/leemons-permissions/src/addPermissionsDeploy.js
@@ -1,11 +1,19 @@
 const { hasKey, setKey } = require('@leemons/mongodb-helpers');
 
 async function addPermissionsDeploy({ keyValueModel, permissions, ctx }) {
+  console.time('Create Permissions');
+  console.time('Has Key Permissions');
   if (!(await hasKey(keyValueModel, `permissions`))) {
+    console.timeEnd('Has Key Permissions');
+    console.time('users.permissions.addMany');
     await ctx.tx.call('users.permissions.addMany', permissions);
+    console.timeEnd('users.permissions.addMany');
+    console.time('keyValueModel');
     await setKey(keyValueModel, `permissions`);
+    console.timeEnd('keyValueModel');
   }
   ctx.tx.emit('init-permissions');
+  console.timeEnd('Create Permissions');
 }
 
 module.exports = { addPermissionsDeploy };
--- packages/leemons-widgets/src/addWidgetItemsDeploy.js
+++ packages/leemons-widgets/src/addWidgetItemsDeploy.js
@@ -2,10 +2,14 @@ const _ = require('lodash');
 const { hasKey, setKey } = require('@leemons/mongodb-helpers');
 
 async function addWidgetItemsDeploy({ keyValueModel, items, ctx }) {
+  console.time('Create widgets items');
+  console.time('has widgets items KEY');
   if (
     !(await hasKey(keyValueModel, `widgets-items-zones`)) ||
     process.env.RELOAD_WIDGETS_ON_EVERY_INSTALL === 'true'
   ) {
+    console.timeEnd('has widgets items KEY');
+    console.time('widgets items allSettled');
     await Promise.allSettled(
       _.map(items, (config) =>
         ctx.tx.call('widgets.widgets.setItemToZone', {
@@ -18,9 +22,11 @@ async function addWidgetItemsDeploy({ keyValueModel, items, ctx }) {
         })
       )
     );
+    console.timeEnd('widgets items allSettled');
     await setKey(keyValueModel, `widgets-items-zones`);
   }
   ctx.tx.emit('init-widget-items');
+  console.timeEnd('Create widgets items');
 }
 
 module.exports = { addWidgetItemsDeploy };
