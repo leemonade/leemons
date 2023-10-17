08018437c9301318614607d9e152b55a1a8938ec
Jaime Gómez Cimarro <cerberupo@gmail.com>
2023-10-17 12:49:44 +0200

    feat(assignables): Migrated addTeachersToAssignableInstance function

--- plugins/leemons-plugin-assignables/backend/core/teachers/addTeachersToAssignableInstance.js
+++ plugins/leemons-plugin-assignables/backend/core/teachers/addTeachersToAssignableInstance.js
@@ -1,11 +1,35 @@
 //! Solo está el encabezamiento para poder hacer test
 
-async function addTeachersToAssignableInstance(
+const { addPermissionToUser } = require('../permissions/instances/users');
+
+async function addTeachersToAssignableInstance({
   teachers,
-  { id: assignableInstanceId, assignable: assignableId },
-  { transacting } = {}
-) {
-  //
+  id: assignableInstanceId,
+  assignable: assignableId,
+  ctx,
+}) {
+  const teachersIds = teachers.map(({ id }) => id);
+  await addPermissionToUser({
+    assignableInstance: assignableInstanceId,
+    assignable: assignableId,
+    userAgents: teachersIds,
+    role: 'teacher',
+    ctx,
+  });
+
+  // EN: Save the teachers
+  // ES: Guarda los profesores
+  return ctx.tx.db.Teachers.insertMany(
+    teachers.map(({ teacher, type }) => ({
+      assignableInstance: assignableInstanceId,
+      teacher,
+      type,
+    })),
+    {
+      new: true,
+      lean: true,
+    }
+  );
 }
 
 module.exports = { addTeachersToAssignableInstance };
