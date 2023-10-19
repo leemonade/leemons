c3e1311290a2531518d972b456110e448c9c341f
paola-p <>
2023-10-19 17:26:04 +0200

    fix(assignables): eliminate duplacated code

--- plugins/leemons-plugin-assignables/backend/core/ongoing/helpers/activitiesData/getInstancesData.unit.test.js
+++ plugins/leemons-plugin-assignables/backend/core/ongoing/helpers/activitiesData/getInstancesData.unit.test.js
@@ -1,18 +1,10 @@
-const {
-  it,
-  expect,
-  beforeAll,
-  afterAll,
-  beforeEach,
-} = require('@jest/globals');
+const { it, expect, beforeAll, afterAll, beforeEach } = require('@jest/globals');
 const { generateCtx, createMongooseConnection } = require('@leemons/testing');
 const { newModel } = require('@leemons/mongodb');
 
 const { getInstancesData } = require('./getInstancesData');
 const { instancesSchema } = require('../../../../models/instances');
-const {
-  getInstanceObject,
-} = require('../../../../__fixtures__/getInstanceObject');
+const { getInstanceObject } = require('../../../../__fixtures__/getInstanceObject');
 
 // MOCKS
 jest.mock('./getAssignablesData');
@@ -35,69 +27,67 @@ afterAll(async () => {
   disconnectMongoose = null;
 });
 
-beforeEach(async () => {
-  await mongooseConnection.dropDatabase();
-  jest.resetAllMocks();
-});
-
 const mockInstanceObj = getInstanceObject();
 mockInstanceObj.metadata = JSON.stringify(mockInstanceObj.metadata);
 mockInstanceObj.relatedAssignableInstances = JSON.stringify(
   mockInstanceObj.relatedAssignableInstances
 );
-
-it('Should return instances data correctly', async () => {
-  // Arrange
-  const instances = ['assignableInstanceOneId', 'assignableInstanceTwoId'];
-
-  const ctx = generateCtx({
+const instances = ['assignableInstanceOneId', 'assignableInstanceTwoId'];
+let ctx;
+let initialValues = [
+  {
+    ...mockInstanceObj,
+    id: instances[0],
+    assignable: 'assignableOneId',
+  },
+  { ...mockInstanceObj, id: instances[1], assignable: 'assignableTwoId' },
+];
+beforeEach(async () => {
+  await mongooseConnection.dropDatabase();
+  jest.resetAllMocks();
+  ctx = generateCtx({
     models: {
       Instances: newModel(mongooseConnection, 'Instances', instancesSchema),
     },
   });
-
-  const initialValues = [
-    {
-      ...mockInstanceObj,
-      id: instances[0],
-      assignable: 'assignableOneId',
-    },
-    { ...mockInstanceObj, id: instances[1], assignable: 'assignableTwoId' },
-  ];
   await ctx.db.Instances.create(initialValues);
+});
 
-  const mockAssignablesData = {
-    [initialValues[0].assignable]: {
-      asset: {},
-      id: initialValues[0].assignable,
-      role: 'task',
-    },
-    [initialValues[1].assignable]: {
-      asset: {},
-      id: initialValues[1].assignable,
-      role: 'feedback',
-    },
-  };
+const mockAssignablesData = {
+  [initialValues[0].assignable]: {
+    asset: {},
+    id: initialValues[0].assignable,
+    role: 'task',
+  },
+  [initialValues[1].assignable]: {
+    asset: {},
+    id: initialValues[1].assignable,
+    role: 'feedback',
+  },
+};
+const expectedResult = {
+  [instances[0]]: {
+    id: instances[0],
+    assignable: mockAssignablesData[initialValues[0].assignable],
+    metadata: JSON.parse(initialValues[0].metadata),
+    alwaysAvailable: true,
+    requiresScoring: false,
+    allowFeedback: false,
+  },
+  [instances[1]]: {
+    id: instances[1],
+    assignable: mockAssignablesData[initialValues[1].assignable],
+    metadata: JSON.parse(initialValues[1].metadata),
+    alwaysAvailable: true,
+    requiresScoring: false,
+    allowFeedback: false,
+  },
+};
+
+it('Should return instances data correctly', async () => {
+  // Arrange
   getAssignablesData.mockResolvedValue(mockAssignablesData);
 
-  const expectedResult = {
-    [instances[0]]: {
-      id: instances[0],
-      assignable: mockAssignablesData[initialValues[0].assignable],
-      metadata: JSON.parse(initialValues[0].metadata),
-      alwaysAvailable: true,
-      requiresScoring: false,
-      allowFeedback: false,
-    },
-    [instances[1]]: {
-      id: instances[1],
-      assignable: mockAssignablesData[initialValues[1].assignable],
-      metadata: JSON.parse(initialValues[1].metadata),
-      alwaysAvailable: true,
-      requiresScoring: false,
-      allowFeedback: false,
-    },
-  };
   // Act
   const response = await getInstancesData({ instances, ctx });
 
@@ -111,59 +101,16 @@ it('Should return instances data correctly', async () => {
 
 it('Should return related instances data correctly', async () => {
   // Arrange
-  const instances = ['assignableInstanceOneId', 'assignableInstanceTwoId'];
-
-  const ctx = generateCtx({
-    models: {
-      Instances: newModel(mongooseConnection, 'Instances', instancesSchema),
-    },
-  });
-
-  const initialValues = [
-    {
-      ...mockInstanceObj,
-      id: instances[0],
-      assignable: 'assignableOne',
-    },
-    { ...mockInstanceObj, id: instances[1], assignable: 'assignableTwo' },
-  ];
-  await ctx.db.Instances.create(initialValues);
-  const mockAssignablesData = {
-    [initialValues[0].assignable]: {
-      asset: {},
-      id: initialValues[0].assignable,
-      role: 'task',
-    },
-    [initialValues[1].assignable]: {
-      asset: {},
-      id: initialValues[1].assignable,
-      role: 'feedback',
-    },
-  };
-
   getAssignablesData.mockResolvedValue(mockAssignablesData);
-  const expectedResult = {
+
+  const expectedResultAlt = {
     [instances[0]]: {
-      id: instances[0],
-      assignable: mockAssignablesData[initialValues[0].assignable],
-      metadata: JSON.parse(initialValues[0].metadata),
-      alwaysAvailable: true,
-      requiresScoring: false,
-      allowFeedback: false,
-      relatedAssignableInstances: JSON.parse(
-        initialValues[0].relatedAssignableInstances
-      ),
+      ...expectedResult[instances[0]],
+      relatedAssignableInstances: JSON.parse(initialValues[0].relatedAssignableInstances),
     },
     [instances[1]]: {
-      id: instances[1],
-      assignable: mockAssignablesData[initialValues[1].assignable],
-      metadata: JSON.parse(initialValues[1].metadata),
-      alwaysAvailable: true,
-      requiresScoring: false,
-      allowFeedback: false,
-      relatedAssignableInstances: JSON.parse(
-        initialValues[1].relatedAssignableInstances
-      ),
+      ...expectedResult[instances[1]],
+      relatedAssignableInstances: JSON.parse(initialValues[1].relatedAssignableInstances),
     },
   };
   // Act
@@ -175,10 +122,8 @@ it('Should return related instances data correctly', async () => {
 
   // Assert
   expect(getAssignablesData).toBeCalledWith({
-    assignables: expect.arrayContaining(
-      initialValues.map((item) => item.assignable)
-    ),
+    assignables: expect.arrayContaining(initialValues.map((item) => item.assignable)),
     ctx,
   });
-  expect(response).toMatchObject(expectedResult);
+  expect(response).toMatchObject(expectedResultAlt);
 });
