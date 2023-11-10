30f15ff3b76110cbe9af473d02d285b4354fa321
rvillares <rvillares@bornan.net>
2023-11-10 08:56:25 +0100

    feat(leemons-validator): revert controllerValidator to ajv version

--- packages/leemons-validator/src/controllerValidator.js
+++ packages/leemons-validator/src/controllerValidator.js
@@ -1,64 +1,72 @@
+const Ajv = require('ajv');
+const addFormats = require('ajv-formats');
+const addKeywords = require('ajv-keywords');
+
 const {
   Validators,
   Errors: { ValidationError },
 } = require('moleculer');
 
-const Validator = require('fastest-validator');
-
-const _ = require('lodash');
-
 class ControllerValidator extends Validators.Base {
-  constructor(opts) {
-    super(opts);
-    this.validator = new Validator(this.opts);
+  constructor(options = {}) {
+    super();
+    this.validator = new Ajv({ ...options, allErrors: true });
+
+    addKeywords(this.validator);
+    addFormats(this.validator);
+    this.fallbackValidator = new Validators.Fastest();
   }
 
-  /**
-   * Compile a validation schema to a checker function.
-   * Need a clone because FV manipulate the schema (removing $$... props)
-   *
-   * @param {any} schema
-   * @returns {Function}
-   */
   compile(schema) {
-    const moleculerSchema = this.convertSchemaToMoleculer(schema);
-    return this.validator.compile(_.cloneDeep(moleculerSchema));
+    const validate = this.validator.compile(schema);
+    return (params) => this.validate(params, validate);
   }
 
-  /**
-   * Validate params against the schema
-   * @param {any} params
-   * @param {any} schema
-   * @returns {boolean}
-   */
-  validate(params, schema) {
-    const res = this.validator.validate(params, _.cloneDeep(schema));
-    if (res !== true) throw new ValidationError('Parameters validation error!', null, res);
-
-    return true;
-  }
-
-  /**
-   * Convert the specific validation schema to
-   * the Moleculer (fastest-validator) validation schema format.
-   *
-   * @param {any} schema
-   * @returns {Object}
-   */
   // eslint-disable-next-line class-methods-use-this
-  convertSchemaToMoleculer(ajvSchema) {
-    if (!ajvSchema.properties) return {};
+  async validate(params, validate) {
+    const isValid = await validate(params);
+    if (!isValid) throw new ValidationError('Parameters validation error!', null, validate.errors);
+    return isValid;
+  }
 
-    const fastestSchema = {};
-    Object.keys(ajvSchema.properties).forEach((key) => {
-      fastestSchema[key] = { type: ajvSchema.properties[key].type };
-      if (ajvSchema.required.includes(key)) {
-        fastestSchema[key].optional = false;
-      } else {
-        fastestSchema[key].optional = true;
-      }
-    });
-    return fastestSchema;
+  /**
+   * Register validator as a middleware
+   *
+   * @memberof ParamValidator
+   */
+  middleware() {
+    const self = this;
+
+    const processCheckResponse = function check(ctx, handler, res) {
+      if (res === true) return handler(ctx);
+      return Promise.reject(new ValidationError('Parameters validation error!', null, res));
+    };
+
+    return {
+      name: 'Validator',
+      localAction: function validatorMiddleware(handler, action) {
+        if (!action.params?.properties || typeof action.params?.properties !== 'object') {
+          // no schema to validate for => just return back the handler directly
+          if (!action.params || typeof action.params !== 'object') return handler;
+
+          // fallback to the fastest validator (moleculer's default validator)
+          const checkFn = self.fallbackValidator.compile(action.params);
+          return async function validateContextParams(ctx) {
+            const res = await checkFn(ctx.params);
+            if (res !== true) return processCheckResponse(ctx, handler, res);
+            return handler(ctx);
+          };
+        }
+
+        // Wrap a param validator when the params are specified
+        const checkFn = self.compile(action.params);
+        return async function validateContextParams(ctx) {
+          const res = await checkFn(ctx.params != null ? ctx.params : {});
+          if (res !== true) return processCheckResponse(ctx, handler, res);
+          return handler(ctx);
+        };
+      },
+    };
   }
 }
 
