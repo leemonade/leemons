ddc056eea2067af61b4fd917de1aa0d97f26e919
MIGUELez11 <miguel@leemons.io>
2023-12-19 12:40:18 +0100

    fix: Add support for . separated roles

--- plugins/leemons-plugin-assignables/frontend/src/hooks/useRolesLocalizations.js
+++ plugins/leemons-plugin-assignables/frontend/src/hooks/useRolesLocalizations.js
@@ -2,7 +2,7 @@ import { useMemo } from 'react';
 import prefixPN from '@assignables/helpers/prefixPN';
 import useTranslateLoader from '@multilanguage/useTranslateLoader';
 import { unflatten } from '@common';
-import { get } from 'lodash';
+import { escapeRegExp, get } from 'lodash';
 
 export default function useRolesLocalizations(roles) {
   // key is array
@@ -11,9 +11,23 @@ export default function useRolesLocalizations(roles) {
 
   return useMemo(() => {
     if (translations && translations.items) {
-      const res = unflatten(translations.items);
+      const res = {};
+      Object.entries(translations.items).forEach(([key, value]) => {
+        const match = new RegExp(
+          `^${escapeRegExp(prefixPN('roles'))}\\.(?<role>.*)\\.(?<modifier>.*)`,
+          'gm'
+        ).exec(key);
 
-      return get(res, prefixPN('roles'), {});
+        const role = match?.groups?.role;
+        const modifier = match?.groups?.modifier;
+        if (!res[role]) {
+          res[role] = {};
+        }
+
+        res[role][modifier] = value;
+      });
+
+      return res;
     }
 
     return {};
