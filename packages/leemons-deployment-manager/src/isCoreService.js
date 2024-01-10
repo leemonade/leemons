9877cc1fe93d77535d3b62d351f01f1196aee253
MIGUELez11 <miguel@leemons.io>
2024-01-10 22:16:40 +0100

    chore: Remove unused var

--- packages/leemons-deployment-manager/src/isCoreService.js
+++ packages/leemons-deployment-manager/src/isCoreService.js
@@ -1,7 +1,7 @@
 // const { getPluginNameFromServiceName } = require('@leemons/service-name-parser');
 
 // TODO [!!!] Investigar si nos pueden llegar a llamar usando $node pasandose la seguridad
-const ignoreNames = ['$node']; // 'deployment-manager', 'gateway'
+// const ignoreNames = ['$node']; // 'deployment-manager', 'gateway'
 
 function isCoreService(serviceName) {
   return false;
