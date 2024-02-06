25b3431b25d94abbe1d8a74951357835c22b6dca
Miguel Ángel Florido Cifuentes <miguel@leemons.io>
2024-02-06 22:56:40 +0100

    fix(assignables): check assignable-direct and assignable-parent permissions
    Reviewed-by: @MIGUELez11
    Refs: #153

--- plugins/leemons-plugin-assignables/backend/core/permissions/assignables/users/getUserPermissions/getParentAssignables.js
+++ plugins/leemons-plugin-assignables/backend/core/permissions/assignables/users/getUserPermissions/getParentAssignables.js
@@ -1,41 +1,22 @@
-const { uniq, map, groupBy } = require('lodash');
-
 /**
  * Retrieves the parent assignables based on the given ids and context.
  *
  * @param {Object} params - The parameters for retrieving the parent assignables.
  * @param {Array} params.ids - The array of ids to filter the parent assignables.
  * @param {MoleculerContext} params.ctx - The Moleculer context.
- * @returns {Object} - The parent assignables grouped by asset.
+ * @returns {Array} - The parent assignables.
  */
-async function getParentAssignables({ ids, ctx }) {
+function getParentAssignables({ ids, ctx }) {
   if (!ids.length) {
-    return {};
+    return [];
   }
-  const parentAssignables = await ctx.tx.db.Assignables.find({
+  return ctx.tx.db.Assignables.find({
     $or: ids.map((id) => ({
       'submission.activities.activity': id,
     })),
   })
-    .select({ asset: true, submission: true })
+    .select({ asset: true, id: true, _id: false })
     .lean();
-
-  return groupBy(
-    parentAssignables.flatMap((parent) => {
-      const submission = parent.submission ?? {};
-      const activities = uniq(map(submission.activities, 'activity'));
-
-      if (!activities.length) {
-        return null;
-      }
-
-      return activities.map((activity) => ({
-        asset: parent.asset,
-        activity,
-      }));
-    }),
-    'asset'
-  );
 }
 
 module.exports = {
