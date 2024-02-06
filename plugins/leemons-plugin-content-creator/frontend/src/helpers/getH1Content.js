6d33c526241fffdcb20f5faa7a34a9d26e347973
Fernando Marín Sánchez <46426271+fermarinsanchez@users.noreply.github.com>
2024-02-06 12:56:12 +0100

    feat(AssetMetadataContentCreator): Index H1 and H2 tags on LibraryDetailDrawer
    Reviewed-by: @MIGUELez11
    Refs: #150

--- plugins/leemons-plugin-content-creator/frontend/src/helpers/getH1Content.js
+++
@@ -1,8 +0,0 @@
-const extractH1Content = (htmlString) => {
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(htmlString, 'text/html');
-  const h1Elements = doc.getElementsByTagName('h1');
-  return Array.from(h1Elements).map((h1) => h1.textContent);
-};
-
-export default extractH1Content;
