174ba9a47306ed644f8b36e4b705a2037c0a7d4d
Miguel Ángel Florido Cifuentes <miguel@leemons.io>
2024-02-09 01:52:41 +0100

    chore(tasks): merge condition as the action is the same

--- plugins/leemons-plugin-tasks/frontend/src/request/task/saveTask.js
+++ plugins/leemons-plugin-tasks/frontend/src/request/task/saveTask.js
@@ -15,9 +15,7 @@ export default async function saveTaskRequest(taskId, task) {
 
   if (cover instanceof File) {
     formData[assetCoverPropertyName] = await uploadFileAsMultipart(cover, { name: cover.name });
-  } else if (typeof cover === 'string') {
-    formData[assetCoverPropertyName] = cover;
-  } else if (_.isNull(cover)) {
+  } else if (_.isString(cover) || _.isNull(cover)) {
     formData[assetCoverPropertyName] = cover;
   }
 
