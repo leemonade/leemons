93033daad2e53e6473ae3e3a6d0ed89e7d38bca0
fermarinsanchez <fermarinsanchez@yahoo.es>
2024-02-22 12:37:24 +0100

    fix(LoginForm): modify Regex to accept capitalized letters and transform email value to lower case before send it to server.

--- plugins/leemons-plugin-users/frontend/src/components/LoginForm/LoginForm.js
+++ plugins/leemons-plugin-users/frontend/src/components/LoginForm/LoginForm.js
@@ -33,7 +33,7 @@ export const LOGIN_FORM_DEFAULT_PROPS = {
 };
 
 const EMAIL_REGEX =
-  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i;
 
 const LoginForm = ({
   labels,
@@ -69,9 +69,16 @@ const LoginForm = ({
     recoveryProps.as = 'a';
     recoveryProps.href = recoverUrl;
   }
+  const handleFormSubmit = (data) => {
+    const dataParsed = {
+      email: data.email.toLowerCase(),
+      password: data.password,
+    };
+    onSubmit(dataParsed);
+  };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(handleFormSubmit)}>
       <ContextContainer title={labels.title} {...props} data-cypress-id="loginForm">
         {formError && <Alert severity="error">{formError}</Alert>}
 
