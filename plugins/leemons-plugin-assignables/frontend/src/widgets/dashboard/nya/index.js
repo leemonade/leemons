2896db41c5fb5da014bd8e962c3c17b59c7da144
MIGUELez11 <miguel@leemons.io>
2024-03-05 17:06:57 +0100

    feat(assigns): add empty state to NYA

--- plugins/leemons-plugin-assignables/frontend/src/widgets/dashboard/nya/index.js
+++ plugins/leemons-plugin-assignables/frontend/src/widgets/dashboard/nya/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Box } from '@bubbles-ui/components';
+import { Box, ContextContainer, Button } from '@bubbles-ui/components';
 
+import { Link } from 'react-router-dom';
 import useClassData from '@assignables/hooks/useClassDataQuery';
 import { useIsStudent } from '@academic-portfolio/hooks';
+import { ChevRightIcon } from '@bubbles-ui/icons/outline';
 import { ActivityCarousel, EvaluationsCarousel, Header } from './components';
 import {
   useEvaluatedActivities,
@@ -11,6 +13,8 @@ import {
   useNyaLocalizations,
   useNyaStyles,
 } from './hooks';
+
+import { EmptyState } from './components/EmptyState/EmptyState';
 
 export default function NYA({ classe, program }) {
   const localizations = useNyaLocalizations();
@@ -22,6 +26,25 @@ export default function NYA({ classe, program }) {
   const activitiesClassData = useClassData(activities.activities, localizations);
   const evaluationsClassData = useClassData(evaluations.activities, localizations);
   const { classes } = useNyaStyles();
+
+  const isEmpty = isStudent ? !activities?.length && !evaluations?.length : !activities?.length;
+
+  if (isEmpty) {
+    return (
+      <ContextContainer
+        title={localizations?.nya?.emptyState?.title}
+        titleRightZone={
+          <Link to={'/private/assignables/ongoing'}>
+            <Button variant="link" rightIcon={<ChevRightIcon />}>
+              {localizations?.nya?.seeAllActivities}
+            </Button>
+          </Link>
+        }
+      >
+        <EmptyState />
+      </ContextContainer>
+    );
+  }
 
   return (
     <Box className={classes.root}>
