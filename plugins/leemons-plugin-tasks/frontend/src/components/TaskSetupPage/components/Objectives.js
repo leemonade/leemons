2e414ffa5f7a51f42230897fba8dd3e8091831dd
Fernando Marín Sánchez <46426271+fermarinsanchez@users.noreply.github.com>
2024-03-18 15:02:13 +0100

    refactor(tasks): redisign custom objectives form
    Ticket: task/Tasks-51
    Reviewed-by: @MIGUELez11
    Refs: #267

--- plugins/leemons-plugin-tasks/frontend/src/components/TaskSetupPage/components/Objectives.js
+++ plugins/leemons-plugin-tasks/frontend/src/components/TaskSetupPage/components/Objectives.js
@@ -1,54 +1,124 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Controller, useFormContext } from 'react-hook-form';
-import { HtmlText, InputWrapper, TableInput, Textarea, Button } from '@bubbles-ui/components';
+import {
+  HtmlText,
+  InputWrapper,
+  TableInput,
+  TextInput,
+  Button,
+  Box,
+  Text,
+} from '@bubbles-ui/components';
 import { AddCircleIcon } from '@bubbles-ui/icons/solid';
 import useTableInputLabels from '../../../helpers/useTableInputLabels';
+import { ObjectivesStyles } from './Objectives.styles';
 
-export default function Objectives({ form, name, label, required }) {
+export default function Objectives({ form, name, label, labels, required }) {
   const tableInputLabels = useTableInputLabels();
   const localForm = useFormContext();
+  const [newObjective, setNewObjective] = useState('');
+  const { classes } = ObjectivesStyles();
 
   const {
     control,
     formState: { errors },
+    setValue,
   } = form ?? localForm;
   const curriculumObjectives = form.watch(name);
+  const curriculumObjectivesExists =
+    Array.isArray(curriculumObjectives) && curriculumObjectives.length > 0;
+  const objectivesWithId = curriculumObjectivesExists
+    ? curriculumObjectives?.map((objective, index) => ({
+        ...objective,
+        id: index + 1,
+      }))
+    : [];
 
-  const columns = useMemo(() => [
-    {
-      Header: '',
-      accessor: 'objective',
-      input: { node: <Textarea />, rules: { required: true } },
-      valueRender: (value) => <HtmlText>{value}</HtmlText>,
-    },
-  ]);
+  const addNewObjective = () => {
+    const updatedObjectives = [
+      ...(curriculumObjectivesExists ? curriculumObjectives : []),
+      {
+        id: (Array.isArray(curriculumObjectives) ? curriculumObjectives.length : 0) + 1,
+        objective: newObjective,
+      },
+    ];
+    setValue(name, updatedObjectives);
+    setNewObjective('');
+  };
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: labels?.numberHeader,
+        accessor: 'id',
+        valueRender: (value) => <Text>{value}</Text>,
+        style: { width: 80, paddingLeft: 10 },
+      },
+      {
+        Header: labels?.objectiveHeader,
+        accessor: 'objective',
+        input: {
+          node: <TextInput />,
+          rules: { required: true },
+        },
+        valueRender: (value) => <HtmlText>{value}</HtmlText>,
+        style: { paddingLeft: 10 },
+      },
+    ],
+    [labels]
+  );
 
   return (
     <InputWrapper label={label}>
+      <Box className={classes.root}>
+        <Controller
+          name={name}
+          control={control}
+          rules={{ required: true }}
+          render={({ field: { onChange, value, ...field } }) => (
+            <TextInput
+              {...field}
+              value={newObjective ?? ''}
+              label={labels.inputLabel}
+              onChange={(e) => setNewObjective(e)}
+              placeholder={labels.inputPlaceholder}
+              sx={() => ({ width: 360 })}
+            />
+          )}
+        />
+        <Button
+          variant="link"
+          size="md"
+          leftIcon={<AddCircleIcon />}
+          disabled={newObjective.length === 0}
+          onClick={addNewObjective}
+          className={classes.button}
+        >
+          {tableInputLabels.add}
+        </Button>
+      </Box>
       <Controller
         control={control}
         name={name}
         rules={{ validate: (value) => !required || value?.length > 0 }}
         render={({ field }) => (
-          <TableInput
-            {...field}
-            columns={columns}
-            editable
-            error={errors.objectives && tableInputLabels.required}
-            data={curriculumObjectives || []}
-            labels={tableInputLabels}
-            renderActionButton={({ disabled, onAdd }) => (
-              <Button
-                variant="link"
-                leftIcon={<AddCircleIcon />}
-                disabled={disabled}
-                onClick={onAdd}
-              >
-                {tableInputLabels.add}
-              </Button>
-            )}
-          />
+          <Box>
+            <TableInput
+              {...field}
+              columns={columns}
+              sortable={false}
+              editable={false}
+              isOneInput
+              resetOnAdd
+              canAdd={false}
+              showHeaders={objectivesWithId?.length > 0}
+              error={errors.objectives && tableInputLabels.required}
+              data={objectivesWithId || []}
+              labels={tableInputLabels}
+              actionLabel
+            />
+          </Box>
         )}
       />
     </InputWrapper>
@@ -57,6 +127,7 @@ export default function Objectives({ form, name, label, required }) {
 
 Objectives.propTypes = {
   label: PropTypes.string.isRequired,
+  labels: PropTypes.object,
   required: PropTypes.bool,
   name: PropTypes.string.isRequired,
   form: PropTypes.any,
