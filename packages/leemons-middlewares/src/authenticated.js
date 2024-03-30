58c987849ef79dadd8d2a855c99f3e6255422e05
Johan Sosa <sosa.johan@gmail.com>
2024-03-30 17:52:53 +0100

    feat(errors): Added new `ignoreStack` flag when calling LeemonsError
    - Also refactor the `autenticated.js` middleware in order to improve its maintainability and reduce cognitive complexity.

--- packages/leemons-middlewares/src/authenticated.js
+++ packages/leemons-middlewares/src/authenticated.js
@@ -1,6 +1,61 @@
 const _ = require('lodash');
 const { LeemonsError } = require('@leemons/error');
 
+function handleUnauthorizedAccess(
+  ctx,
+  continueEvenThoughYouAreNotLoggedIn,
+  message = 'Authorization required'
+) {
+  if (continueEvenThoughYouAreNotLoggedIn) {
+    ctx.meta.userSession = null;
+    return;
+  }
+  throw new LeemonsError(ctx, { httpStatusCode: 401, message, ignoreStack: true });
+}
+
+async function authenticateWithToken(ctx, token, forceOnlyUser) {
+  const user = await ctx.tx.call('users.auth.detailForJWT', {
+    jwtToken: token,
+    forceOnlyUser,
+  });
+  if (user) {
+    ctx.meta.userSession = user;
+  }
+  return user;
+}
+
+async function authenticateWithMultipleTokens(ctx) {
+  ctx.meta.authorization = _.compact(ctx.meta.authorization);
+  const user = await authenticateWithToken(ctx, ctx.meta.authorization[0], true);
+  const userAgents = await Promise.all(
+    _.map(ctx.meta.authorization, (auth) =>
+      ctx.tx.call('users.auth.detailForJWT', {
+        jwtToken: auth,
+        forceOnlyUser: false,
+        forceOnlyUserAgent: true,
+      })
+    )
+  );
+  if (user && userAgents.length) {
+    user.userAgents = userAgents;
+    ctx.meta.userSession = user;
+    return user;
+  }
+  return null;
+}
+
+async function authenticateUser(ctx) {
+  if (_.isString(ctx.meta.authorization)) {
+    return authenticateWithToken(ctx, ctx.meta.authorization, false);
+  }
+
+  if (_.isArray(ctx.meta.authorization) && ctx.meta.authorization.length) {
+    return authenticateWithMultipleTokens(ctx);
+  }
+
+  return null;
+}
+
 module.exports =
   ({ continueEvenThoughYouAreNotLoggedIn } = {}) =>
   async (ctx) => {
@@ -8,58 +63,21 @@ module.exports =
       return;
     }
     if (!ctx.meta.authorization) {
-      if (continueEvenThoughYouAreNotLoggedIn) {
-        ctx.meta.userSession = null;
-        return;
-      }
-
-      throw new LeemonsError(ctx, {
-        httpStatusCode: 401,
-        message: '[LeemonsMiddlewareAuthenticated] No authorization header',
-      });
+      handleUnauthorizedAccess(
+        ctx,
+        continueEvenThoughYouAreNotLoggedIn,
+        '[LeemonsMiddlewareAuthenticated] No authorization header'
+      );
+      return;
     }
 
     try {
-      if (_.isString(ctx.meta.authorization)) {
-        const user = await ctx.tx.call('users.auth.detailForJWT', {
-          jwtToken: ctx.meta.authorization,
-        });
-        if (user) {
-          ctx.meta.userSession = user;
-          return;
-        }
-      } else if (_.isArray(ctx.meta.authorization) && ctx.meta.authorization.length) {
-        ctx.meta.authorization = _.compact(ctx.meta.authorization);
-        const user = await ctx.tx.call('users.auth.detailForJWT', {
-          jwtToken: ctx.meta.authorization[0],
-          forceOnlyUser: true,
-        });
-        const userAgents = await Promise.all(
-          _.map(ctx.meta.authorization, (auth) =>
-            ctx.tx.call('users.auth.detailForJWT', {
-              jwtToken: auth,
-              forceOnlyUser: false,
-              forceOnlyUserAgent: true,
-            })
-          )
-        );
-        if (user && userAgents.length) {
-          user.userAgents = userAgents;
-          ctx.meta.userSession = user;
-          return;
-        }
+      const user = await authenticateUser(ctx);
+      if (!user) {
+        handleUnauthorizedAccess(ctx, continueEvenThoughYouAreNotLoggedIn);
       }
-      if (continueEvenThoughYouAreNotLoggedIn) {
-        ctx.meta.userSession = null;
-        return;
-      }
-      throw new LeemonsError(ctx, { httpStatusCode: 401, message: 'Authorization required' });
     } catch (err) {
       ctx.logger.error(err);
-      if (continueEvenThoughYouAreNotLoggedIn) {
-        ctx.meta.userSession = null;
-        return;
-      }
-      throw new LeemonsError(ctx, { httpStatusCode: 401, message: 'Authorization required' });
+      handleUnauthorizedAccess(ctx, continueEvenThoughYouAreNotLoggedIn);
     }
   };
