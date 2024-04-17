5abfe8da772beae706f1b185d60de0113ab2d3b6
paola-p <>
2024-04-17 16:26:00 +0200

    fix(calendar): fix weird implementation of onAcademicPortfolioRemovePrograms

--- plugins/leemons-plugin-calendar/backend/core/pluginEvents/program/onAcademicPortfolioRemovePrograms.js
+++ plugins/leemons-plugin-calendar/backend/core/pluginEvents/program/onAcademicPortfolioRemovePrograms.js
@@ -1,5 +1,4 @@
 async function remove({ id, soft, ctx }) {
-  // eslint-disable-next-line global-require
   const programCalendar = await ctx.tx.db.ProgramCalendar.findOne({
     program: id,
   }).lean();
@@ -14,22 +13,12 @@ async function remove({ id, soft, ctx }) {
   }
 }
 
-async function onAcademicPortfolioRemovePrograms({
-  // data // unused old param,
-  soft,
-  programs,
-  ctx,
-}) {
-  // eslint-disable-next-line no-async-promise-executor
-  return new Promise(async (resolve) => {
-    try {
-      await Promise.all(programs.map(({ id }) => remove({ id, soft, ctx })));
-      resolve();
-    } catch (e) {
-      // eslint-disable-next-line no-console
-      console.error(e);
-    }
-  });
+async function onAcademicPortfolioRemovePrograms({ soft, programs, ctx }) {
+  try {
+    await Promise.all(programs.map(({ id }) => remove({ id, soft, ctx })));
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 module.exports = { onAcademicPortfolioRemovePrograms };
