252d549e8d7bf0abbcba56d39a2afb97f706ddac
Fernando Marín Sánchez <46426271+fermarinsanchez@users.noreply.github.com>
2024-04-24 19:52:42 +0200

    fix(academic-portfolio): Add a Empty state in portfolio tree view if centers don't have programs yet.

--- plugins/leemons-plugin-academic-portfolio/frontend/src/pages/private/AcademicTreePage.js
+++ plugins/leemons-plugin-academic-portfolio/frontend/src/pages/private/AcademicTreePage.js
@@ -6,6 +6,7 @@ import {
   Select,
   TotalLayoutContainer,
   TotalLayoutHeader,
+  TotalLayoutStepContainer,
   Stack,
   ImageLoader,
   Box,
@@ -20,6 +21,8 @@ import prefixPN from '@academic-portfolio/helpers/prefixPN';
 import { CourseView } from '@academic-portfolio/components/AcademicTree/CourseView/CourseView';
 import { KnowledgeView } from '@academic-portfolio/components/AcademicTree/KnowledgeView/KnowledgeView';
 import TreeBox from '@academic-portfolio/components/AcademicTree/TreeBox';
+import { EmptyState } from '@academic-portfolio/components/EmptyState';
+import { OpenIcon } from '@bubbles-ui/icons/outline';
 import { GroupView } from '../../components/AcademicTree/GroupView/GroupView';
 import SubjectView from '../../components/AcademicTree/SubjectView/SubjectView';
 
@@ -70,8 +73,11 @@ const AcademicTreePage = () => {
   });
 
   const isLoading = useMemo(
-    () => areCentersLoading || areCenterProgramsLoading || isAcademicTreeLoading,
-    [areCenterProgramsLoading, isAcademicTreeLoading]
+    () =>
+      areCentersLoading ||
+      areCenterProgramsLoading ||
+      (programSelectData.length > 0 && isAcademicTreeLoading),
+    [areCenterProgramsLoading, isAcademicTreeLoading, programSelectData]
   );
 
   const generateGUID = () => uuidv4();
@@ -288,25 +294,44 @@ const AcademicTreePage = () => {
           </TotalLayoutHeader>
         }
       >
-        <Stack
-          ref={scrollRef}
-          spacing={4}
-          sx={{
-            overflowY: 'auto',
-            backgroundColor: '#f8f9fb',
-            width: '100%',
-            maxWidth: 1400,
-            margin: 'auto',
-            paddingTop: 24,
-            paddingInline: 24,
-          }}
-        >
-          {TreeBoxMemo}
-          <Box sx={{ width: 192 }}></Box>
-          <Stack direction="column" sx={{ width: 'calc(100% - 192px)' }} ref={viewRef}>
-            {viewToRender}
+        {programSelectData.length < 1 ? (
+          <Stack
+            justifyContent="center"
+            ref={scrollRef}
+            style={{ overflow: 'auto' }}
+            fullWidth
+            fullHeight
+          >
+            <TotalLayoutStepContainer scrollRef={scrollRef}>
+              <EmptyState
+                description={t('noProgramEmptyStateDescription')}
+                actionLabel={t('noProgramEmptyStateAction')}
+                Icon={<OpenIcon />}
+                onClick={() => history.push('/private/academic-portfolio/programs')}
+              />
+            </TotalLayoutStepContainer>
           </Stack>
-        </Stack>
+        ) : (
+          <Stack
+            ref={scrollRef}
+            spacing={4}
+            sx={{
+              overflowY: 'auto',
+              backgroundColor: '#f8f9fb',
+              width: '100%',
+              maxWidth: 1400,
+              margin: 'auto',
+              paddingTop: 24,
+              paddingInline: 24,
+            }}
+          >
+            {TreeBoxMemo}
+            <Box sx={{ width: 192 }}></Box>
+            <Stack direction="column" sx={{ width: 'calc(100% - 192px)' }} ref={viewRef}>
+              {viewToRender}
+            </Stack>
+          </Stack>
+        )}
       </TotalLayoutContainer>
       <EnrollmentDrawer
         scrollRef={scrollRef}
