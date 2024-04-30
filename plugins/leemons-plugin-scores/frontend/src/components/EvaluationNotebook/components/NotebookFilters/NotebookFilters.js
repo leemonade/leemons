83480e33e3ceb19ba81c94d635ff3f5fe5250fde
MIGUELez11 <miguel@leemons.io>
2024-04-30 12:45:16 +0200

    feat(scores): open weighting drawer directly in EvaluationNotebook

--- plugins/leemons-plugin-scores/frontend/src/components/EvaluationNotebook/components/NotebookFilters/NotebookFilters.js
+++ plugins/leemons-plugin-scores/frontend/src/components/EvaluationNotebook/components/NotebookFilters/NotebookFilters.js
@@ -1,18 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { Box, Button, SearchInput, Select, Stack, Switch } from '@bubbles-ui/components';
 import { CalculatorIcon } from '@bubbles-ui/icons/solid';
 import { Controller, useForm, useWatch } from 'react-hook-form';
-import { Link } from 'react-router-dom';
 
 import useTranslateLoader from '@multilanguage/useTranslateLoader';
 import { prefixPN } from '@scores/helpers';
+import { WeightConfigDrawer } from '@scores/components/Weights/components/WeightConfigDrawer';
 import useSearchTypes from './hooks/useSearchTypes';
 import useOnChange from './hooks/useOnChange';
 
 export default function NotebookFilters({ filters, onChange, value }) {
   const [t] = useTranslateLoader(prefixPN('evaluationNotebook.filters'));
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
   const form = useForm({
     defaultValues: {
       searchType: 'student',
@@ -45,46 +47,53 @@ export default function NotebookFilters({ filters, onChange, value }) {
   useOnChange({ onChange, control: form.control });
 
   return (
-    <Stack direction="row" justifyContent="space-between" alignItems="baseline">
-      <Stack direction="row" spacing={4}>
-        <Controller
-          name="searchType"
-          control={form.control}
-          render={({ field }) => <Select {...field} data={searchTypes} />}
-        />
-        <Controller
-          name="search"
-          control={form.control}
-          defaultValue=""
-          render={({ field }) => (
-            <SearchInput
-              {...field}
-              placeholder={t('search', { type: t(`searchTypes.${searchType}`).toLowerCase() })}
-              sx={{ width: 220 }}
-            />
-          )}
-        />
-      </Stack>
-      <Stack direction="row" spacing={4} alignItems="baseline">
-        <Controller
-          name="showNonEvaluable"
-          control={form.control}
-          defaultValue={false}
-          render={({ field }) => (
-            <Switch {...field} checked={field.value} label={t('showNonEvaluable')} />
-          )}
-        />
-        <Box>
-          <Link
-            to={`/private/scores/weights?program=${filters?.program}&class=${filters?.class?.id}`}
-          >
-            <Button variant="linkInline" leftIcon={<CalculatorIcon />}>
+    <>
+      <WeightConfigDrawer
+        class={drawerIsOpen ? filters?.class : null}
+        onClose={() => setDrawerIsOpen(false)}
+      />
+
+      <Stack direction="row" justifyContent="space-between" alignItems="baseline">
+        <Stack direction="row" spacing={4}>
+          <Controller
+            name="searchType"
+            control={form.control}
+            render={({ field }) => <Select {...field} data={searchTypes} />}
+          />
+          <Controller
+            name="search"
+            control={form.control}
+            defaultValue=""
+            render={({ field }) => (
+              <SearchInput
+                {...field}
+                placeholder={t('search', { type: t(`searchTypes.${searchType}`).toLowerCase() })}
+                sx={{ width: 220 }}
+              />
+            )}
+          />
+        </Stack>
+        <Stack direction="row" spacing={4} alignItems="baseline">
+          <Controller
+            name="showNonEvaluable"
+            control={form.control}
+            defaultValue={false}
+            render={({ field }) => (
+              <Switch {...field} checked={field.value} label={t('showNonEvaluable')} />
+            )}
+          />
+          <Box>
+            <Button
+              variant="linkInline"
+              leftIcon={<CalculatorIcon />}
+              onClick={() => setDrawerIsOpen(true)}
+            >
               {t('goToWeighting')}
             </Button>
-          </Link>
-        </Box>
+          </Box>
+        </Stack>
       </Stack>
-    </Stack>
+    </>
   );
 }
 
