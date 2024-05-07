87755399b1a380ff1f8ef051e0bbeefa10703a85
paola-p <>
2024-05-07 18:13:55 +0200

    fix(academic-portfolio): Handle asset creation for image and icon when duplicating a subject

--- plugins/leemons-plugin-academic-portfolio/backend/core/subjects/duplicateSubjectByIds.js
+++ plugins/leemons-plugin-academic-portfolio/backend/core/subjects/duplicateSubjectByIds.js
@@ -1,6 +1,55 @@
 const _ = require('lodash');
 const { duplicateSubjectCreditsBySubjectsIds } = require('./duplicateSubjectCreditsBySubjectsIds');
 
+const handleImageAndIcon = async ({ image, subjectId, icon, ctx }) => {
+  const imageData = {
+    indexable: false,
+    public: true,
+    name: subjectId,
+  };
+  const iconData = _.clone(imageData);
+  if (image) imageData.cover = image;
+  if (icon) iconData.cover = icon;
+  const [assetImage, assetIcon] = await Promise.all([
+    ctx.tx.call('leebrary.assets.add', {
+      asset: imageData,
+      options: {
+        permissions: [
+          {
+            canEdit: true,
+            isCustomPermission: true,
+            permissionName: ctx.prefixPN('programs'),
+            actionNames: ['update', 'admin'],
+          },
+        ],
+        published: true,
+      },
+    }),
+    ctx.tx.call('leebrary.assets.add', {
+      asset: { ...iconData, indexable: false },
+      options: {
+        permissions: [
+          {
+            canEdit: true,
+            isCustomPermission: true,
+            permissionName: ctx.prefixPN('programs'),
+            actionNames: ['update', 'admin'],
+          },
+        ],
+        published: true,
+      },
+    }),
+  ]);
+  await ctx.tx.db.Subjects.findOneAndUpdate(
+    { id: subjectId },
+    {
+      image: assetImage.id,
+      icon: assetIcon.id,
+    },
+    { new: true, lean: true }
+  );
+};
+
 async function duplicateSubjectByIds({ ids, duplications: dup = {}, preserveName = false, ctx }) {
   const duplications = dup;
 
@@ -10,7 +59,7 @@ async function duplicateSubjectByIds({ ids, duplications: dup = {}, preserveName
   // ES: Empezamos la duplicación de los items
   // EN: Start the duplication of the items
   const newSubjects = await Promise.all(
-    _.map(subjects, ({ id, _id, __v, updatedAt, createdAt, ...item }) =>
+    _.map(subjects, ({ id, _id, __v, updatedAt, createdAt, image, icon, ...item }) =>
       ctx.tx.db.Subjects.create({
         ...item,
         name: preserveName ? item.name : `${item.name} (1)`,
@@ -23,6 +72,15 @@ async function duplicateSubjectByIds({ ids, duplications: dup = {}, preserveName
       }).then((mongooseDoc) => mongooseDoc.toObject())
     )
   );
+
+  newSubjects.forEach(async ({ id }, i) => {
+    await handleImageAndIcon({
+      image: subjects[i].image,
+      subjectId: id,
+      icon: subjects[i].icon,
+      ctx,
+    });
+  });
 
   // ES: Añadimos los items duplicados de tal forma que el indice es el id original y el valor es el nuevo item duplicado
   // EN: Add the duplicated items in such a way that the index is the original id and the value is the new duplicated item
