fb7ca0a42200dc93d22be6b9a9f12e2142ba4a2d
Fernando Marín Sánchez <46426271+fermarinsanchez@users.noreply.github.com>
2024-05-09 20:28:54 +0200

    refactor(LibraryCardEmbbed): - Style LibraryCardEmbbed. - LibraryCardEmbbed on AssetPickerDrawer - Resources and activities table elements looks like LibraryCardEmbbed on Module setup

--- plugins/leemons-plugin-learning-paths/frontend/src/components/ModuleSetup/components/StructureData/components/ModuleComposer/components/ResourceRenderer/ResourceRenderer.js
+++ plugins/leemons-plugin-learning-paths/frontend/src/components/ModuleSetup/components/StructureData/components/ModuleComposer/components/ResourceRenderer/ResourceRenderer.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Box, createStyles, Text, TextClamp } from '@bubbles-ui/components';
-
-import { LocaleDate } from '@common';
+import { Box, createStyles, Text, TextClamp, Badge } from '@bubbles-ui/components';
+import { capitalize } from 'lodash';
+import useRolesLocalizations from '@assignables/hooks/useRolesLocalizations';
 import { AvatarActivity } from '../../../AvatarActivity';
 
 export const useResourceRendererStyles = createStyles((theme) => {
@@ -32,8 +32,29 @@ export const useResourceRendererStyles = createStyles((theme) => {
   };
 });
 
-export function ResourceRenderer({ activity, localizations }) {
+export function ResourceRenderer({ activity }) {
   const { classes } = useResourceRendererStyles();
+  const roleLocalizations = useRolesLocalizations([activity?.role ?? activity?.providerData?.role]);
+  const getAssetBadgeType = () => {
+    if (activity?.role && activity?.role !== 'leebrary.asset') {
+      return capitalize(
+        roleLocalizations?.[activity?.role ?? activity?.providerData?.role]?.singular
+      );
+    }
+
+    const typeMappings = {
+      image: 'Image',
+      bookmark: ['video'].includes(activity?.asset?.mediaType) ? 'Video' : 'Bookmark',
+      'content-creator': 'Content creator',
+      file: activity?.asset?.fileExtension === 'pdf' ? 'PDF' : 'File',
+      video: 'Video',
+      audio: 'Audio',
+      document: activity?.asset?.fileExtension === 'pdf' ? 'PDF' : 'Document',
+    };
+
+    return typeMappings[activity?.asset?.fileType] || 'File';
+  };
+  const badgeCategory = getAssetBadgeType();
   return (
     <Box className={classes.root}>
       <AvatarActivity activity={activity} />
@@ -41,19 +62,7 @@ export function ResourceRenderer({ activity, localizations }) {
         <TextClamp lines={1}>
           <Text className={classes.activityName}>{activity.asset.name}</Text>
         </TextClamp>
-        <Text className={classes.lastUpdate}>
-          {localizations?.lastUpdate}:{' '}
-          <LocaleDate
-            date={activity.updatedAt || null}
-            options={{
-              year: 'numeric',
-              month: '2-digit',
-              day: '2-digit',
-              hour: '2-digit',
-              minute: '2-digit',
-            }}
-          />
-        </Text>
+        <Badge size="xs" label={badgeCategory} closable={false} radius={'default'} />
       </Box>
     </Box>
   );
@@ -62,8 +71,16 @@ export function ResourceRenderer({ activity, localizations }) {
 ResourceRenderer.propTypes = {
   localizations: PropTypes.object,
   activity: PropTypes.shape({
+    providerData: PropTypes.shape({
+      role: PropTypes.string,
+    }),
+    role: PropTypes.string,
     asset: PropTypes.shape({
       name: PropTypes.string,
+      fileType: PropTypes.string,
+      mediaType: PropTypes.string,
+      fileExtension: PropTypes.string,
+      cover: PropTypes.string,
     }),
     updatedAt: PropTypes.instanceOf(Date),
   }),
