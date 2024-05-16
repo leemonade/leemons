b87538cd3d105be31a298345a23e38af38a8a43b
Fernando Marín Sánchez <46426271+fermarinsanchez@users.noreply.github.com>
2024-05-16 18:14:01 +0200

    fix(calendar): parse calendar.name correctly if calendar is user calendar

--- plugins/leemons-plugin-calendar/frontend/src/helpers/getCalendarNameWithConfigAndSession.js
+++ plugins/leemons-plugin-calendar/frontend/src/helpers/getCalendarNameWithConfigAndSession.js
@@ -1,6 +1,8 @@
 export default function getCalendarNameWithConfigAndSession(calendar, config, session) {
   let label = calendar.name;
   if (config && config.userCalendar && config.userCalendar.id === calendar.id)
-    label = session?.name + (session?.surnames || '');
+    label = `${session?.surnames !== null ? session?.surnames : ''}${
+      session.surnames ? ', ' : ''
+    } ${session?.name}`;
   return label;
 }
