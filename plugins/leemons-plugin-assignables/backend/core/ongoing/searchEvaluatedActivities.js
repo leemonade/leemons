3ddf3e4143c4bcea656030ac238defd6954e7874
Miguel Ángel Florido Cifuentes <miguel@leemons.io>
2024-05-24 18:30:31 +0200

    feat(assignables): hide evaluated NYA Card if grade reviewed by student
    Ticket: task/Assigns-110
    Reviewed-by: @MIGUELez11
    Refs: #419

--- plugins/leemons-plugin-assignables/backend/core/ongoing/searchEvaluatedActivities.js
+++ plugins/leemons-plugin-assignables/backend/core/ongoing/searchEvaluatedActivities.js
@@ -17,6 +17,7 @@ const { returnModulesData } = require('./helpers/filters/returnModulesData');
 const {
   filterModuleInstancesByHavingAllActivities,
 } = require('./helpers/filters/filterModuleInstancesByHavingAllActivities');
+const filterAssignationsByGradesNotViewed = require('./helpers/filters/filterAssignationsByGradesNotViewed');
 
 /**
  *
@@ -30,7 +31,9 @@ module.exports = async function searchEvaluatedActivities({ query, ctx }) {
   let assignations = await getStudentAssignations({ ctx });
   let instances = map(assignations, 'instance');
 
-  const modules = filterInstancesByIsModule({ instances });
+  let modules = filterInstancesByIsModule({ instances });
+  let modulesAssignations = filterAssignationsByInstance({ assignations, instances: modules });
+
   instances = filterInstancesByNotModule({ instances });
 
   const instanceSubjectsProgramsAndClasses = await getInstanceSubjectsProgramsAndClasses({
@@ -48,8 +51,8 @@ module.exports = async function searchEvaluatedActivities({ query, ctx }) {
 
   const dates = await getActivitiesDates({
     instances,
-    assignations,
-    filters: { progress: 'evaluated' },
+    assignations: assignations.concat(modulesAssignations),
+    filters: { progress: 'evaluated', gradeWasViewed: true },
     ctx,
   });
 
@@ -62,8 +65,14 @@ module.exports = async function searchEvaluatedActivities({ query, ctx }) {
     ctx,
   });
 
-  instances = groupInstancesInModules({ instances: map(assignations, 'instance'), modules });
+  assignations = filterAssignationsByGradesNotViewed({ assignations, dates });
+  modulesAssignations = filterAssignationsByGradesNotViewed({
+    assignations: modulesAssignations,
+    dates,
+  });
+  modules = map(modulesAssignations, 'instance');
 
+  instances = groupInstancesInModules({ instances: map(assignations, 'instance'), modules });
   instances = filterModuleInstancesByHavingAllActivities({ instances });
 
   instances = sortInstancesByDates({
