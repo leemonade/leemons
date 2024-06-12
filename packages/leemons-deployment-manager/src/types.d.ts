a96e0290437399569a72ffaab70ab9432aa36d6c
MIGUELez11 <miguel@leemons.io>
2024-06-12 13:01:39 +0200

    fix(leemons-deployment-manager): Add ctx.meta.userSession to typings

--- packages/leemons-deployment-manager/src/types.d.ts
+++ packages/leemons-deployment-manager/src/types.d.ts
@@ -5,10 +5,22 @@ type DB = {
   [modelName: string]: Model
 }
 
+interface UserAgent {
+  id: string;
+}
+interface UserSession {
+  userAgents: UserAgent[];
+}
+
 export type Context = MoleculerContext & {
   db: DB
   tx: {
     db: DB
     emit: MoleculerContext['emit']
+    call: MoleculerContext['call']
+  },
+  meta: MoleculerContext['meta'] & {
+    deploymentID: string;
+    userSession: UserSession;
   }
 }
