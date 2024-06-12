5010fbcaf59491c7d1c14ebbec78cf498452292e
paola-p <>
2024-06-12 12:47:08 +0200

    fix(bulk-data): handle errors during bulk-data import in getLoadStatus function

--- plugins/leemons-plugin-bulk-data/backend/core/importHandlers/importBulkData.js
+++ plugins/leemons-plugin-bulk-data/backend/core/importHandlers/importBulkData.js
@@ -35,8 +35,12 @@ const LOAD_PHASES = {
   TASKS: 'tasks',
   WIDGETS: 'widgets',
 };
+
+const ERROR = 'error';
+
 const PHASES = Object.keys(LOAD_PHASES).map((key) => LOAD_PHASES[key]);
 let currentPhase = null;
+let lastPhaseOnError = null;
 
 function getLoadProgress() {
   if (!currentPhase) {
@@ -50,6 +54,16 @@ function getLoadProgress() {
 }
 
 function getLoadStatus() {
+  if (currentPhase === ERROR) {
+    return {
+      status: 200,
+      inProgressPhase: ERROR,
+      currentPhase: ERROR,
+      overallProgress: `0%`,
+      lastPhaseOnError,
+    };
+  }
+
   const progress = getLoadProgress();
   const workingPhaseIndex = PHASES.indexOf(currentPhase) + 1;
 
@@ -89,6 +103,11 @@ async function shareAssetsWithProfile({ profileId, assets, ctx }) {
   await Promise.all(promises);
 }
 
+const throwAnError = async () => {
+  await setTimeout(10000);
+  throw new Error('Operation timed out after 10 seconds.');
+};
+
 async function importBulkData({
   docPath,
   preConfig = {},
@@ -107,169 +126,178 @@ async function importBulkData({
 
   currentPhase = null;
 
-  if (fs.existsSync(docPath)) {
-    // ································································
-    // APP INITIALIZATION
+  try {
+    if (fs.existsSync(docPath)) {
+      // ································································
+      // APP INITIALIZATION
 
-    if (!skipAppInitialization) {
+      if (!skipAppInitialization) {
+        // ·······························································
+        // LOCALES
+
+        ctx.logger.debug(chalk`{cyan.bold BULK} {gray Init Platform & locales ...}`);
+        await initLocales({ file: docPath, ctx });
+        currentPhase = LOAD_PHASES.LOCALES;
+
+        await initPlatform({ file: docPath, ctx });
+        ctx.logger.info(chalk`{cyan.bold BULK} Platform initialized`);
+        currentPhase = LOAD_PHASES.PLATFORM;
+
+        // ·······························································
+        // PROVIDERS
+
+        ctx.logger.debug(chalk`{cyan.bold BULK} {gray Init Providers ...}`);
+        await initProviders({ file: docPath, ctx });
+        ctx.logger.info(chalk`{cyan.bold BULK} Providers initialized`);
+        currentPhase = LOAD_PHASES.PROVIDERS;
+
+        // ·······························································
+        // CENTERS, PROFILES
+
+        ctx.logger.debug(chalk`{cyan.bold BULK} {gray Starting Admin plugin ...}`);
+        await initAdmin({ file: docPath, ctx });
+        ctx.logger.info(chalk`{cyan.bold BULK} COMPLETED Admin plugin`);
+        currentPhase = LOAD_PHASES.ADMIN;
+
+        ctx.logger.debug(chalk`{cyan.bold BULK} {gray Starting Users plugin ...}`);
+        config.centers = await initCenters({ file: docPath, ctx });
+        currentPhase = LOAD_PHASES.CENTERS;
+
+        config.profiles = await initProfiles({ file: docPath, ctx });
+        currentPhase = LOAD_PHASES.PROFILES;
+      }
+
       // ·······························································
-      // LOCALES
+      // USERS
 
-      ctx.logger.debug(chalk`{cyan.bold BULK} {gray Init Platform & locales ...}`);
-      await initLocales({ file: docPath, ctx });
-      currentPhase = LOAD_PHASES.LOCALES;
-
-      await initPlatform({ file: docPath, ctx });
-      ctx.logger.info(chalk`{cyan.bold BULK} Platform initialized`);
-      currentPhase = LOAD_PHASES.PLATFORM;
-
-      // ·······························································
-      // PROVIDERS
-
-      ctx.logger.debug(chalk`{cyan.bold BULK} {gray Init Providers ...}`);
-      await initProviders({ file: docPath, ctx });
-      ctx.logger.info(chalk`{cyan.bold BULK} Providers initialized`);
-      currentPhase = LOAD_PHASES.PROVIDERS;
+      if (!config.users) {
+        config.users = await initUsers({
+          file: docPath,
+          centers: config.centers,
+          profiles: config.profiles,
+          ctx,
+        });
+        ctx.logger.info(chalk`{cyan.bold BULK} COMPLETED Users plugin`);
+        currentPhase = LOAD_PHASES.USERS;
+      }
 
       // ·······························································
-      // CENTERS, PROFILES
+      // GRADES
 
-      ctx.logger.debug(chalk`{cyan.bold BULK} {gray Starting Admin plugin ...}`);
-      await initAdmin({ file: docPath, ctx });
-      ctx.logger.info(chalk`{cyan.bold BULK} COMPLETED Admin plugin`);
-      currentPhase = LOAD_PHASES.ADMIN;
+      ctx.logger.debug(chalk`{cyan.bold BULK} {gray Starting Academic Rules plugin ...}`);
+      config.grades = await initGrades({ file: docPath, centers: config.centers, ctx });
+      ctx.logger.info(chalk`{cyan.bold BULK} COMPLETED Academic Rules plugin`);
+      currentPhase = LOAD_PHASES.GRADES;
 
-      ctx.logger.debug(chalk`{cyan.bold BULK} {gray Starting Users plugin ...}`);
-      config.centers = await initCenters({ file: docPath, ctx });
-      currentPhase = LOAD_PHASES.CENTERS;
+      // await throwAnError();
 
-      config.profiles = await initProfiles({ file: docPath, ctx });
-      currentPhase = LOAD_PHASES.PROFILES;
+      // ·······························································
+      // MEDIA LIBRARY
+
+      ctx.logger.debug(chalk`{cyan.bold BULK} {gray Starting Leebrary plugin ...}`);
+      config.assets = await initLibrary({ file: docPath, config, ctx });
+
+      if (shareLibraryAssetsWithTeacherProfile) {
+        await shareAssetsWithProfile({
+          profileId: config.profiles.teacher?.id,
+          profileSysName: config.profiles.teacher?.sysName,
+          assets: Object.values(config.assets),
+          ctx,
+        });
+      }
+
+      ctx.logger.info(chalk`{cyan.bold BULK} COMPLETED Leebrary plugin`);
+      currentPhase = LOAD_PHASES.LIBRARY;
+
+      // ·······························································
+      // ACADEMIC PORTFOLIO -> Da error por duplicación de userAgentPermisions, expected & handled in academic portfolio
+
+      ctx.logger.debug(chalk`{cyan.bold BULK} {gray Starting Academic Portfolio plugin ...}`);
+      config.programs = await initAcademicPortfolio({ file: docPath, config, skipEnrollment, ctx });
+      await setTimeout(1000);
+      await ctx.logger.info(chalk`{cyan.bold BULK} COMPLETED Academic Portfolio plugin`);
+      currentPhase = LOAD_PHASES.AP;
+
+      ctx.logger.debug(chalk`{cyan.bold BULK} {gray Updating Leebrary plugin with AP conf ...}`);
+      await updateLibrary({ file: docPath, config, ctx });
+      ctx.logger.info(chalk`{cyan.bold BULK} UPDATED Leebrary plugin`);
+
+      // ·······························································
+      // CALENDAR & KANBAN
+
+      ctx.logger.debug(chalk`{cyan.bold BULK} {gray Starting Calendar plugin ...}`);
+      await initCalendar({ file: docPath, config, ctx });
+      ctx.logger.info(chalk`{cyan.bold BULK} COMPLETED Calendar plugin`);
+      currentPhase = LOAD_PHASES.CALENDAR;
+
+      // ·······························································
+      // ACADEMIC CALENDAR
+
+      ctx.logger.debug(chalk`{cyan.bold BULK} {gray Starting Academic Calendar plugin ...}`);
+      config.regionalCalendars = await initAcademicCalendar({ file: docPath, config, ctx });
+
+      await initProgramCalendars({ file: docPath, config, ctx });
+
+      ctx.logger.info(chalk`{cyan.bold BULK} COMPLETED Academic Clendar plugin`);
+      currentPhase = LOAD_PHASES.ACADEMIC_CALENDAR;
+
+      // ·······························································
+      // TESTS & QBANKS
+
+      ctx.logger.debug(chalk`{cyan.bold BULK} {gray Starting Tests plugin ...}`);
+      const { tests, qbanks } = await initTests({ file: docPath, config, ctx });
+      config.tests = tests;
+      config.qbanks = qbanks;
+
+      if (shareLibraryAssetsWithTeacherProfile) {
+        const testsAssets = Object.values(config.tests).map((item) => item.asset);
+        const qbanksAssets = Object.values(config.qbanks).map((item) => item.asset);
+        await shareAssetsWithProfile({
+          profileId: config.profiles.teacher?.id,
+          profileSysName: config.profiles.teacher?.sysName,
+          assets: [...testsAssets, ...qbanksAssets],
+          ctx,
+        });
+      }
+
+      ctx.logger.info(chalk`{cyan.bold BULK} COMPLETED Tests plugin`);
+      currentPhase = LOAD_PHASES.TESTS;
+
+      // ·······························································
+      // TASKS
+
+      ctx.logger.debug(chalk`{cyan.bold BULK} {gray Starting Tasks plugin ...}`);
+      config.tasks = await initTasks({ file: docPath, config, ctx });
+
+      if (shareLibraryAssetsWithTeacherProfile) {
+        const { task: tasks } = await ctx.call('tasks.tasks.getRest', {
+          id: Object.values(config.tasks).map((task) => task.fullId),
+        });
+
+        const tasksAssets = tasks.map((item) => item.asset);
+        await shareAssetsWithProfile({
+          profileId: config.profiles.teacher?.id,
+          profileSysName: config.profiles.teacher?.sysName,
+          assets: [...tasksAssets], // If resources and/or statementImage change to be not-public add them here
+          ctx,
+        });
+      }
+
+      ctx.logger.info(chalk`{cyan.bold BULK} COMPLETED Tasks plugin`);
+      currentPhase = LOAD_PHASES.TASKS;
+
+      // ·······························································
+      // WIDGETS
+
+      await initWidgets({ ctx });
+      currentPhase = LOAD_PHASES.WIDGETS;
+      ctx.logger.info(chalk`{cyan.bold BULK} COMPLETED Widgets plugin`);
     }
-
-    // ·······························································
-    // USERS
-
-    if (!config.users) {
-      config.users = await initUsers({
-        file: docPath,
-        centers: config.centers,
-        profiles: config.profiles,
-        ctx,
-      });
-      ctx.logger.info(chalk`{cyan.bold BULK} COMPLETED Users plugin`);
-      currentPhase = LOAD_PHASES.USERS;
-    }
-
-    // ·······························································
-    // GRADES
-
-    ctx.logger.debug(chalk`{cyan.bold BULK} {gray Starting Academic Rules plugin ...}`);
-    config.grades = await initGrades({ file: docPath, centers: config.centers, ctx });
-    ctx.logger.info(chalk`{cyan.bold BULK} COMPLETED Academic Rules plugin`);
-    currentPhase = LOAD_PHASES.GRADES;
-
-    // ·······························································
-    // MEDIA LIBRARY
-
-    ctx.logger.debug(chalk`{cyan.bold BULK} {gray Starting Leebrary plugin ...}`);
-    config.assets = await initLibrary({ file: docPath, config, ctx });
-
-    if (shareLibraryAssetsWithTeacherProfile) {
-      await shareAssetsWithProfile({
-        profileId: config.profiles.teacher?.id,
-        profileSysName: config.profiles.teacher?.sysName,
-        assets: Object.values(config.assets),
-        ctx,
-      });
-    }
-
-    ctx.logger.info(chalk`{cyan.bold BULK} COMPLETED Leebrary plugin`);
-    currentPhase = LOAD_PHASES.LIBRARY;
-
-    // ·······························································
-    // ACADEMIC PORTFOLIO -> Da error por duplicación de userAgentPermisions, expected & handled in academic portfolio
-
-    ctx.logger.debug(chalk`{cyan.bold BULK} {gray Starting Academic Portfolio plugin ...}`);
-    config.programs = await initAcademicPortfolio({ file: docPath, config, skipEnrollment, ctx });
-    await setTimeout(1000);
-    await ctx.logger.info(chalk`{cyan.bold BULK} COMPLETED Academic Portfolio plugin`);
-    currentPhase = LOAD_PHASES.AP;
-
-    ctx.logger.debug(chalk`{cyan.bold BULK} {gray Updating Leebrary plugin with AP conf ...}`);
-    await updateLibrary({ file: docPath, config, ctx });
-    ctx.logger.info(chalk`{cyan.bold BULK} UPDATED Leebrary plugin`);
-
-    // ·······························································
-    // CALENDAR & KANBAN
-
-    ctx.logger.debug(chalk`{cyan.bold BULK} {gray Starting Calendar plugin ...}`);
-    await initCalendar({ file: docPath, config, ctx });
-    ctx.logger.info(chalk`{cyan.bold BULK} COMPLETED Calendar plugin`);
-    currentPhase = LOAD_PHASES.CALENDAR;
-
-    // ·······························································
-    // ACADEMIC CALENDAR
-
-    ctx.logger.debug(chalk`{cyan.bold BULK} {gray Starting Academic Calendar plugin ...}`);
-    config.regionalCalendars = await initAcademicCalendar({ file: docPath, config, ctx });
-
-    await initProgramCalendars({ file: docPath, config, ctx });
-
-    ctx.logger.info(chalk`{cyan.bold BULK} COMPLETED Academic Clendar plugin`);
-    currentPhase = LOAD_PHASES.ACADEMIC_CALENDAR;
-
-    // ·······························································
-    // TESTS & QBANKS
-
-    ctx.logger.debug(chalk`{cyan.bold BULK} {gray Starting Tests plugin ...}`);
-    const { tests, qbanks } = await initTests({ file: docPath, config, ctx });
-    config.tests = tests;
-    config.qbanks = qbanks;
-
-    if (shareLibraryAssetsWithTeacherProfile) {
-      const testsAssets = Object.values(config.tests).map((item) => item.asset);
-      const qbanksAssets = Object.values(config.qbanks).map((item) => item.asset);
-      await shareAssetsWithProfile({
-        profileId: config.profiles.teacher?.id,
-        profileSysName: config.profiles.teacher?.sysName,
-        assets: [...testsAssets, ...qbanksAssets],
-        ctx,
-      });
-    }
-
-    ctx.logger.info(chalk`{cyan.bold BULK} COMPLETED Tests plugin`);
-    currentPhase = LOAD_PHASES.TESTS;
-
-    // ·······························································
-    // TASKS
-
-    ctx.logger.debug(chalk`{cyan.bold BULK} {gray Starting Tasks plugin ...}`);
-    config.tasks = await initTasks({ file: docPath, config, ctx });
-
-    if (shareLibraryAssetsWithTeacherProfile) {
-      const { task: tasks } = await ctx.call('tasks.tasks.getRest', {
-        id: Object.values(config.tasks).map((task) => task.fullId),
-      });
-
-      const tasksAssets = tasks.map((item) => item.asset);
-      await shareAssetsWithProfile({
-        profileId: config.profiles.teacher?.id,
-        profileSysName: config.profiles.teacher?.sysName,
-        assets: [...tasksAssets], // If resources and/or statementImage change to be not-public add them here
-        ctx,
-      });
-    }
-
-    ctx.logger.info(chalk`{cyan.bold BULK} COMPLETED Tasks plugin`);
-    currentPhase = LOAD_PHASES.TASKS;
-
-    // ·······························································
-    // WIDGETS
-
-    await initWidgets({ ctx });
-    currentPhase = LOAD_PHASES.WIDGETS;
-    ctx.logger.info(chalk`{cyan.bold BULK} COMPLETED Widgets plugin`);
+  } catch (error) {
+    console.error('Error in importBulkData =>', error);
+    lastPhaseOnError = currentPhase;
+    currentPhase = ERROR;
+    throw error;
   }
 }
 
