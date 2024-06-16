e385e9263140941d573a75ace2ef6b9b1148788e
Fernando Marín Sánchez <46426271+fermarinsanchez@users.noreply.github.com>
2024-06-16 11:22:00 +0200

    fix(assignables): add optional chaining to usePendingEvaluationsCount.

--- plugins/leemons-plugin-assignables/frontend/src/hooks/assignableInstance/usePendingEvaluationsCount.js
+++ plugins/leemons-plugin-assignables/frontend/src/hooks/assignableInstance/usePendingEvaluationsCount.js
@@ -5,7 +5,7 @@ import { useMemo } from 'react';
 function usePendingEvaluationsCount({ instance }) {
   const moduleTotal = useMemo(() => {
     let requireScoringCounter = 0;
-    const activitiesStatus = instance.students[0]?.metadata?.moduleStatus;
+    const activitiesStatus = instance?.students?.[0]?.metadata?.moduleStatus || [];
     if (activitiesStatus) {
       activitiesStatus.forEach((activity) => {
         if (activity.requiresScoring) {
