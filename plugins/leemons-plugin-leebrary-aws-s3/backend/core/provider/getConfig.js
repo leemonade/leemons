31d331d857a4bcda309fbf8badcd86484d43d974
Miguel Ángel Florido Cifuentes <miguel@leemons.io>
2024-07-22 11:08:08 +0200

    refactor: Use the new @leemons/aws package on all aws integrations

--- plugins/leemons-plugin-leebrary-aws-s3/backend/core/provider/getConfig.js
+++ plugins/leemons-plugin-leebrary-aws-s3/backend/core/provider/getConfig.js
@@ -1,3 +1,5 @@
+const { getAWSCredentials } = require('@leemons/aws/src');
+
 /**
  * This function retrieves the first configuration object from the database.
  * If no configuration objects are found, it returns null.
@@ -8,23 +10,25 @@
  */
 async function getConfig({ ctx } = {}) {
   let configs = [];
-  if (
-    process.env.AWS_REGION &&
-    process.env.AWS_ACCESS_KEY &&
-    process.env.AWS_SECRET_ACCESS_KEY &&
-    process.env.AWS_S3_BUCKET
-  ) {
+
+  const credentials = await getAWSCredentials({ prefix: 'S3', ctx });
+
+  if (credentials) {
     configs = [
       {
         id: 'leebrary-aws-s3',
         deploymentID: ctx.meta.deploymentID,
         bucket: process.env.AWS_S3_BUCKET,
-        region: process.env.AWS_REGION,
-        accessKey: process.env.AWS_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        region: credentials.region,
+        accessKey: credentials.accessKeyId,
+        secretAccessKey: credentials.secretAccessKey,
       },
     ];
   } else {
+    console.error('=============================================');
+    console.error('[AWS S3] No credentials found in @leemons/aws');
+    console.error('=============================================');
+
     configs = await ctx.tx.db.Config.find({}).lean();
   }
   if (configs.length > 0) return configs[0];
