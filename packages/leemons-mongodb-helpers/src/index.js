de27e056fbfcc6f849c393063de9eae1f18a3b6b
Miguel Ángel Florido Cifuentes <miguel@leemons.io>
2024-08-23 10:37:51 +0200

    fix(mongodb-helpers): Export EMPTY_PAGINATED_RESULT

--- packages/leemons-mongodb-helpers/src/index.js
+++ packages/leemons-mongodb-helpers/src/index.js
@@ -1,4 +1,4 @@
-const { mongoDBPaginate } = require('./mongoDBPaginate');
+const { mongoDBPaginate, EMPTY_PAGINATED_RESULT } = require('./mongoDBPaginate');
 const { getKeyValueModel } = require('./key-value/getKeyValueModel');
 const { getKey } = require('./key-value/getKey');
 const { hasKey } = require('./key-value/hasKey');
@@ -7,6 +7,7 @@ const { hasKeys } = require('./key-value/hasKeys');
 
 module.exports = {
   mongoDBPaginate,
+  EMPTY_PAGINATED_RESULT,
   getKeyValueModel,
   getKey,
   hasKey,
