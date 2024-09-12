2775af09ddff3975d5ec92783438c0f4c54a9ef7
Miguel Ángel Florido Cifuentes <miguel@leemons.io>
2024-09-12 11:28:01 +0200

    fix(mongodb): Added the right type to MixinOptions.models

--- packages/leemons-mongodb/src/types.d.ts
+++ packages/leemons-mongodb/src/types.d.ts
@@ -59,7 +59,7 @@ type MixinOptions = {
   autoLRN?: boolean;
   debugTransaction?: boolean;
   forceLeemonsDeploymentManagerMixinNeedToBeImported?: boolean;
-  models: Record<string, Model<unknown>>;
+  models: Record<string, Model>;
 };
 
 export function LeemonsMongoDBMixin(options?: MixinOptions): Partial<ServiceSchema>;
