65108e709e5560de2a33c1190c970b737784892b
Miguel Ángel Florido Cifuentes <miguel@leemons.io>
2024-09-25 11:11:48 +0200

    fix(assignables): Do not assume widgets exists as an empty array
    Ticket: task/Assigns-139
    Reviewed-by: @MIGUELez11
    Refs: #552

--- plugins/leemons-plugin-assignables/frontend/src/components/Assignment/components/ActivityDatesPicker/hooks/useDatesPickerOptions/useDatesPickerOptions.js
+++ plugins/leemons-plugin-assignables/frontend/src/components/Assignment/components/ActivityDatesPicker/hooks/useDatesPickerOptions/useDatesPickerOptions.js
@@ -1,27 +1,33 @@
-import { useMemo } from "react";
+import { useMemo } from 'react';
 
-import { get } from "lodash";
+import { get } from 'lodash';
 
-import useWidgetLocalizations from "./hooks/useWidgetLocalizations";
-import useWidgets from "./hooks/useWidgets";
-
+import useWidgetLocalizations from './hooks/useWidgetLocalizations';
+import useWidgets from './hooks/useWidgets';
 
 export default function useDatesPickerOptions() {
-  const {data: widgets, isLoading: isLoadingWidgets} = useWidgets();
-  const {data: localizations, isLoading: isLoadingLocalizations} = useWidgetLocalizations({widgets});
+  const { data: widgets, isLoading: isLoadingWidgets } = useWidgets();
+  const { data: localizations, isLoading: isLoadingLocalizations } = useWidgetLocalizations({
+    widgets,
+  });
 
-
-  const options = useMemo(() => widgets.map((widget) => ({
-      value: widget.id,
-      label: get(localizations?.items, widget.labelKey),
-    })),
+  const options = useMemo(
+    () =>
+      widgets.map((widget) => ({
+        value: widget.id,
+        label: get(localizations?.items, widget?.labelKey),
+      })),
     [widgets, localizations?.items]
   );
 
-  const components = useMemo(() => widgets.reduce((acc, widget) => {
-    acc[widget.id] = widget.component;
-    return acc;
-  }, {}), [widgets]);
+  const components = useMemo(
+    () =>
+      widgets.reduce((acc, widget) => {
+        acc[widget.id] = widget.component;
+        return acc;
+      }, {}),
+    [widgets]
+  );
 
-  return {options, components, isLoading: isLoadingWidgets || isLoadingLocalizations};
+  return { options, components, isLoading: isLoadingWidgets || isLoadingLocalizations };
 }
