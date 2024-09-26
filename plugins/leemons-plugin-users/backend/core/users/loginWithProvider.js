a23f30aad5e9ddc0c510ddfb2c20bc76711578de
Miguel Ángel Florido Cifuentes <miguel@leemons.io>
2024-09-26 19:02:57 +0200

    fix(cognito): search users email ignoring casing
    Reviewed-by: @MIGUELez11
    Refs: #559

--- plugins/leemons-plugin-users/backend/core/users/loginWithProvider.js
+++ plugins/leemons-plugin-users/backend/core/users/loginWithProvider.js
@@ -1,7 +1,9 @@
 const { LeemonsError } = require('@leemons/error');
+
 const { getProvider } = require('../providers/getProvider');
-const { generateJWTToken } = require('./jwt/generateJWTToken');
+
 const { isSuperAdmin } = require('./isSuperAdmin');
+const { generateJWTToken } = require('./jwt/generateJWTToken');
 
 /**
  *
@@ -31,7 +33,7 @@ async function verifyIsCallingFromProvider({ ctx }) {
 async function loginWithProvider({ email, ctx }) {
   await verifyIsCallingFromProvider({ ctx });
 
-  const user = await ctx.tx.db.Users.findOne({ email }).lean();
+  const user = await ctx.tx.db.Users.findOne({ email: new RegExp(`^${email}$`, 'i') }).lean();
 
   const [token, userAgents] = await Promise.all([
     generateJWTToken({
