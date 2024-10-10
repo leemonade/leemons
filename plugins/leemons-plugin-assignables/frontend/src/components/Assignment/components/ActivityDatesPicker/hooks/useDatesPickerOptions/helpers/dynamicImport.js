7acf88cf0f56437fead2a9e451cc29e19f0ed3cc
JohanFX <sosa.johan@gmail.com>
2024-10-10 18:56:36 +0200

    feat(build): add TypeScript support and optimize build process
    Reviewed-by: @MIGUELez11 @johan-fx
    Refs: #592
    Co-authored-by: Miguel Ángel Florido Cifuentes <miguel@leemons.io>

--- plugins/leemons-plugin-assignables/frontend/src/components/Assignment/components/ActivityDatesPicker/hooks/useDatesPickerOptions/helpers/dynamicImport.js
+++ plugins/leemons-plugin-assignables/frontend/src/components/Assignment/components/ActivityDatesPicker/hooks/useDatesPickerOptions/helpers/dynamicImport.js
@@ -1,20 +1,23 @@
-import React from "react";
+import React from 'react';
 
-import { LoadingOverlay } from "@bubbles-ui/components";
-import loadable from "@loadable/component";
+import { LoadingOverlay } from '@bubbles-ui/components';
+import loadable from '@loadable/component';
 
-export default function dynamicImport({pluginName, path, preload}) {
-  const component = loadable(async () => {
-    try {
-      /* webpackInclude: /src\/widgets\/assignables\/assignmentForm\/datePicker\/.*\.js/ */
-      return await import(`@app/plugins/${pluginName}/src/widgets/${path}.js`)
-    } catch (error) {
-      /* webpackInclude: /dist\/widgets\/assignables\/assignmentForm\/datePicker\/.*\.js/ */
-      return await import(`@app/plugins/${pluginName}/dist/widgets/${path}.js`)
+export default function dynamicImport({ pluginName, path, preload }) {
+  const component = loadable(
+    async () => {
+      try {
+        /* webpackInclude: /src\/widgets\/assignables\/assignmentForm\/datePicker\/.*\.js/ */
+        return await import(`@app/plugins/${pluginName}/src/widgets/${path}.js`);
+      } catch (error) {
+        /* webpackInclude: /src\/widgets\/assignables\/assignmentForm\/datePicker\/.*\.tsx/ */
+        return await import(`@app/plugins/${pluginName}/src/widgets/${path}.tsx`);
+      }
+    },
+    {
+      fallback: <LoadingOverlay visible />,
     }
-  }, {
-    fallback: <LoadingOverlay visible />
-  })
+  );
 
   if (preload) {
     component.preload();
