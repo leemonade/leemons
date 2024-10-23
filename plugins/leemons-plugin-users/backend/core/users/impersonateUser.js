4debb1002f54fca92370fb952c63ed162a287765
Miguel Ángel Florido Cifuentes <miguel@leemons.io>
2024-10-23 11:58:22 +0200

    feat(users): add xAPI statement to record when an admin has impersonated a user

--- plugins/leemons-plugin-users/backend/core/users/impersonateUser.js
+++ plugins/leemons-plugin-users/backend/core/users/impersonateUser.js
@@ -1,6 +1,32 @@
 const { LeemonsError } = require('@leemons/error');
 
+const { getUserFullName } = require('../../../../../packages/leemons-users/src');
+
 const { generateJWTToken } = require('./jwt/generateJWTToken');
+
+async function addXapiStatement({ user, ctx }) {
+  await ctx.tx.call('xapi.xapi.addLogStatement', {
+    statement: {
+      actor: ctx.meta.userSession.userAgents[0].id,
+      verb: {
+        id: 'https://adlnet.gov/expapi/verbs/impersonated',
+        display: {
+          'en-US': 'impersonated',
+        },
+      },
+      object: {
+        objectType: 'User',
+        name: getUserFullName({ userSession: user }),
+        mbox: `mailto:${user.email}`,
+        openid: `${ctx.meta.hostname}/api/users/users/${user.id}/detail/page`,
+        account: {
+          homePage: ctx.meta.hostname,
+          name: user.id,
+        },
+      },
+    },
+  });
+}
 
 async function impersonateUser({ id, ctx }) {
   const user = await ctx.tx.db.Users.findOne({ id }).lean();
@@ -12,6 +38,8 @@ async function impersonateUser({ id, ctx }) {
       httpStatusCode: 404,
     });
   }
+
+  await addXapiStatement({ user, ctx });
 
   return await generateJWTToken({ payload: { id: user.id }, ctx });
 }
