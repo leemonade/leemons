dae263108f6f8221e6c0fb10a12f382e22902878
Miguel Ángel Florido Cifuentes <miguel@leemons.io>
2024-10-30 09:05:11 +0100

    fix(leebrary): only show the latest resource version when searching
    Ticket: issue/MainLibrary-316
    Reviewed-by: @MIGUELez11
    Refs: #633

--- plugins/leemons-plugin-common/backend/core/versionControl/versions/getVersion.js
+++ plugins/leemons-plugin-common/backend/core/versionControl/versions/getVersion.js
@@ -1,25 +1,17 @@
 const { LeemonsError } = require('@leemons/error');
+const { keyBy } = require('lodash');
+
 const get = require('../currentVersions/get');
 const { parseId, parseVersion, stringifyVersion, stringifyId } = require('../helpers');
 
 const specialVersions = ['latest', 'current', 'published', 'draft'];
 
-async function getVersionMany({ ids, published, ignoreMissing = false, ctx }) {
-  if (!ids.length) return [];
-  const parsedIds = await parseId({ id: ids, verifyVersion: false, ctx });
-
-  const uuids = parsedIds.map((id) => id.uuid);
-
-  // EN: Verify ownership (get throws an error if not owned)
-  // ES: Verificar propiedad (get lanza un error si no es propiedad)
-
-  const uuidsInfo = await get({ uuid: uuids, ctx });
-
+async function getVersionsInfo({ ids, published, ignoreMissing, uuidsInfo, ctx }) {
   const query = {};
   let sortQuery = {};
 
   // TODO: Add more difficult searches (between versions, greather than, etc)
-  query.$or = parsedIds.map(({ version, uuid }) => {
+  query.$or = ids.map(({ version, uuid }) => {
     const subQuery = {
       uuid,
     };
@@ -59,8 +51,123 @@ async function getVersionMany({ ids, published, ignoreMissing = false, ctx }) {
       version: stringifyVersion({ ...version, ctx }),
     })
   );
+
   if (!versionsFound?.length && !ignoreMissing) {
     throw new LeemonsError(ctx, { message: 'Versions not found' });
+  }
+
+  return versionsFound;
+}
+
+async function getLatestVersion({ versions, published, ctx }) {
+  const pipeline = [
+    {
+      $match: {
+        uuid: { $in: versions },
+        deploymentID: ctx.meta.deploymentID,
+        isDeleted: false,
+        published,
+      },
+    },
+    {
+      $sort: { major: -1, minor: -1, patch: -1 },
+    },
+    {
+      $group: {
+        _id: '$uuid',
+        major: { $first: '$major' },
+        minor: { $first: '$minor' },
+        patch: { $first: '$patch' },
+      },
+    },
+  ];
+
+  const latestUnpublishedVersions = await ctx.tx.db.Versions.aggregate(pipeline);
+
+  return keyBy(latestUnpublishedVersions, '_id');
+}
+
+async function getCurrentInfo({ versions, uuidsInfo, ctx }) {
+  const uuidsInfoByUuid = keyBy(uuidsInfo, 'uuid');
+
+  const unpublishedVersions = [];
+  const publishedVersions = [];
+
+  versions.forEach(({ uuid, published }) => {
+    if (published) {
+      if (!uuidsInfoByUuid[uuid].current) {
+        publishedVersions.push(uuid);
+      }
+    } else {
+      unpublishedVersions.push(uuid);
+    }
+  });
+
+  const [latestUnpublishedVersions, latestPublishedVersions] = await Promise.all([
+    getLatestVersion({
+      versions: unpublishedVersions,
+      published: false,
+      ctx,
+    }),
+    getLatestVersion({
+      versions: publishedVersions,
+      published: true,
+      ctx,
+    }),
+  ]);
+
+  return versions.map((version) => {
+    const latestUnpublishedVersion = latestUnpublishedVersions[version.uuid]
+      ? stringifyVersion({
+          ...latestUnpublishedVersions[version.uuid],
+          ctx,
+        })
+      : null;
+
+    const latestPublishedVersion =
+      uuidsInfoByUuid[version.uuid]?.current ??
+      (latestPublishedVersions[version.uuid]
+        ? stringifyVersion({
+            ...latestPublishedVersions[version.uuid],
+            ctx,
+          })
+        : null);
+
+    return {
+      ...version,
+      isCurrentVersionOfPublishedState: version.published
+        ? latestPublishedVersion === version.version
+        : latestUnpublishedVersion === version.version,
+    };
+  });
+}
+
+async function getVersionMany({
+  ids,
+  published,
+  ignoreMissing = false,
+  getCurrentInfo: shouldGetCurrentInfo = false,
+  ctx,
+}) {
+  if (!ids.length) return [];
+  const parsedIds = await parseId({ id: ids, verifyVersion: false, ctx });
+
+  const uuids = parsedIds.map((id) => id.uuid);
+
+  // EN: Verify ownership (get throws an error if not owned)
+  // ES: Verificar propiedad (get lanza un error si no es propiedad)
+  const uuidsInfo = await get({ uuid: uuids, ctx });
+
+  let versionsFound = await getVersionsInfo({
+    ids: parsedIds,
+    published,
+    ignoreMissing,
+    uuidsInfo,
+    ctx,
+  });
+
+  if (shouldGetCurrentInfo) {
+    versionsFound = await getCurrentInfo({ versions: versionsFound, uuidsInfo, ctx });
   }
 
   return parsedIds.map(({ version, uuid }) => {
@@ -80,15 +187,32 @@ async function getVersionMany({ ids, published, ignoreMissing = false, ctx }) {
     const finalVersion = stringifyVersion({ ...versionFound, ctx });
     const fullId = stringifyId({ id: uuid, version: finalVersion, ctx });
 
-    return { uuid, version: finalVersion, fullId, published: Boolean(versionFound.published) };
+    const response = {
+      uuid,
+      version: finalVersion,
+      fullId,
+      published: Boolean(versionFound.published),
+    };
+
+    if (shouldGetCurrentInfo) {
+      response.isCurrentVersionOfPublishedState = versionFound.isCurrentVersionOfPublishedState;
+    }
+
+    return response;
   });
 }
 
-module.exports = async function getVersion({ id, published, ignoreMissing, ctx }) {
+module.exports = async function getVersion({ id, published, ignoreMissing, getCurrentInfo, ctx }) {
   const isArray = Array.isArray(id);
   const ids = isArray ? id : [id];
 
-  const idVerions = await getVersionMany({ ids, published, ignoreMissing, ctx });
+  const idVerions = await getVersionMany({
+    ids,
+    published,
+    ignoreMissing,
+    getCurrentInfo,
+    ctx,
+  });
 
   return isArray ? idVerions : idVerions[0];
 };
