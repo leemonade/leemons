92cc4258aef848fdff7fd353caca8ec70ba038ec
Álvaro Orduna León <4264243+alorle@users.noreply.github.com>
2024-11-07 17:37:38 +0100

    feat(users): sort admin's user table by surnames and name
    Signed-off-by: Álvaro Orduna León <4264243+alorle@users.noreply.github.com>

--- plugins/leemons-plugin-users/frontend/src/pages/private/users/ListUsers/index.js
+++ plugins/leemons-plugin-users/frontend/src/pages/private/users/ListUsers/index.js
@@ -49,6 +49,7 @@ import useProvider from '@users/request/hooks/queries/useProvider';
 
 function ListUsers() {
   const [t] = useTranslateLoader(prefixPN('list_users'));
+  const lang = (navigator.language || navigator.userLanguage).split('-')[0];
   const [store, render] = useStore({
     page: 0,
     size: 10,
@@ -93,6 +94,8 @@ function ListUsers() {
     const { data } = await listUsersRequest({
       page: store.page,
       size: store.size,
+      sort: { surnames: 1, name: 1 },
+      collation: { locale: lang },
       query,
     });
 
