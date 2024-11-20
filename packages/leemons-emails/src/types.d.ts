f273ca163dfb79fbf9397846843d98990cf12620
Johan Sosa <sosa.johan@gmail.com>
2024-11-21 00:48:15 +0100

    feat(emails): Improve `leemons-emails` package types

--- packages/leemons-emails/src/types.d.ts
+++ packages/leemons-emails/src/types.d.ts
@@ -1,1 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function LeemonsEmailsMixin(): any;
+
+export interface EmailTypes {
+  active: 'active';
+}
+
+export function getEmailTypes(): EmailTypes;
