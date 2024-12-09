e1b1cbdd834b839c546fe7760da379224a6caa49
paola-p <>
2024-12-09 14:48:58 +0100

    fix(tests): truncate question table question column in qbank and test creation

--- plugins/leemons-plugin-tests/frontend/src/helpers/getQuestionForTable.js
+++ plugins/leemons-plugin-tests/frontend/src/helpers/getQuestionForTable.js
@@ -3,6 +3,14 @@ import { camelCase } from 'lodash';
 
 import { QUESTION_TYPES } from '@tests/pages/private/questions-banks/questionConstants';
 
+const removeNewLines = (text = '') => {
+  return text
+    .replace(/<br\s*\/?>/gi, ' ')
+    .replace(/<\/p>\s*<p>/gi, ' ')
+    .replace(/\n/g, ' ')
+    .replace(/\s+/g, ' ');
+};
+
 // eslint-disable-next-line import/prefer-default-export
 export function getQuestionForTable(question, t, styles) {
   let responsesAmount = '-';
@@ -22,17 +30,13 @@ export function getQuestionForTable(question, t, styles) {
   return {
     ...question,
     question: (
-      <Box className={styles?.tableCell}>
-        <HtmlText>{question.stem.text}</HtmlText>
+      <Box style={{ width: 220 }} className={styles?.tableCell}>
+        <HtmlText truncateLines={2}>{removeNewLines(question.stem.text)}</HtmlText>
       </Box>
     ),
-    responses: (
-      <Box style={{ minWidth: '100px' }} className={styles?.tableCell}>
-        {responsesAmount}
-      </Box>
-    ),
+    responses: <Box className={styles?.tableCell}>{responsesAmount}</Box>,
     type: (
-      <Box style={{ minWidth: '150px' }} className={styles?.tableCell}>
+      <Box style={{ width: 150 }} className={styles?.tableCell}>
         {t(typeTranslationKey)}
       </Box>
     ),
