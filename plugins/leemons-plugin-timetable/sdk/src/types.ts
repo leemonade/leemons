274440a108185387077bbf211088595f895c52e5
Johan Sosa <sosa.johan@gmail.com>
2025-04-02 19:31:58 +0200

    chore: update timetable plugin dependencies and introduce SDK structure
    - Upgrade dependencies in the leemons-plugin-timetable backend to versions 0.0.42 and 0.0.72 for improved compatibility and functionality.
    - Create a new SDK for the timetable plugin, including package.json and TypeScript configuration files for both ESM and CommonJS builds, enhancing modularity and maintainability.
    - Define initial types for timetable management, ensuring type safety and clarity in the SDK.
    
    These changes lay the groundwork for the timetable management functionality, contributing to a more organized and scalable codebase within the Leemons ecosystem.

---
+++ plugins/leemons-plugin-timetable/sdk/src/types.ts
@@ -0,0 +1,31 @@
+import type { Class, ClassID } from '@leemons/academic-portfolio';
+import type { LRN } from '@leemons/lrn';
+
+export type TimetableID = LRN<'timetable', 'Timetable'>;
+
+/**
+ * @file plugins/leemons-plugin-timetable/backend/models/timetable.js
+ */
+export interface Timetable {
+  id: TimetableID;
+  deploymentID: string;
+  class: ClassID;
+  day: 'monday' | 'tuesday' | 'wednesday' | 'thursday' | 'friday' | 'saturday' | 'sunday';
+  dayWeek: 1 | 2 | 3 | 4 | 5 | 6 | 7;
+  // Hour of day (eg: '08:00')
+  start: string;
+  // Hour of day (eg: '10:00')
+  end: string;
+  // Duration in minutes
+  duration: number;
+
+  isDeleted: boolean;
+
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date | null;
+}
+
+export interface ClassWithTimetable extends Class {
+  schedule: Timetable[];
+}
