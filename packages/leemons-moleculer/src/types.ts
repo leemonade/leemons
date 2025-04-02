7449db2a20613a90a9901067118baeb6613de68e
Johan Sosa <sosa.johan@gmail.com>
2025-04-02 02:07:21 +0200

    chore: initialize leemons-moleculer package with TypeScript configuration and core functionality
    - Create package.json for the leemons-moleculer package, defining dependencies, scripts, and entry points for CommonJS and ESM.
    - Add TypeScript configuration files for both CommonJS and ESM builds to enhance type safety and maintainability.
    - Implement the core Runner class in src/runner.ts, which manages service execution and configuration loading.
    - Define types in src/types.ts to improve type safety across the package.
    - Introduce a command-line interface entry point in src/bin/leemons-runner.ts for executing services.
    
    This initialization establishes the leemons-moleculer package as a foundational component of the Leemons ecosystem, ensuring better type support and modularity for future development.

---
+++ packages/leemons-moleculer/src/types.ts
@@ -0,0 +1,123 @@
+import { UserSession } from '@leemons/users';
+import {
+  EventSchema,
+  LoggerInstance,
+  ActionSchema as MoleculerActionSchema,
+  Context as MoleculerContext,
+  GenericObject as MoleculerGenericObject,
+  ServiceSchema as MoleculerServiceSchema,
+  Service,
+  ServiceSettingSchema,
+} from 'moleculer';
+import { Model } from 'mongoose';
+
+type DB<Models extends Record<string, Model<any>>> = {
+  [modelName in keyof Models]: Models[modelName];
+};
+
+export type GenericObject = MoleculerGenericObject;
+
+export interface EventParams {
+  event: string;
+  params: any;
+}
+
+export type Meta<M extends object = Record<string, never>> = M &
+  GenericObject & {
+    deploymentID: string;
+    userSession: UserSession;
+    $statusCode?: number;
+    $statusMessage?: string;
+    $location?: string;
+    $responseType?: string;
+    $responseHeaders?: Record<string, string>;
+    relationshipID?: string;
+    transactionID?: string;
+  };
+
+export interface ExtendedContext<
+  P = any,
+  M extends object = Record<string, never>,
+  L extends GenericObject = GenericObject,
+  Models extends Record<string, Model<any>> = Record<string, never>
+> extends Omit<MoleculerContext<P, Meta<M>, L>, 'params'> {
+  db: DB<Models>;
+  tx: {
+    db: DB<Models>;
+    emit: MoleculerContext['emit'];
+    call: MoleculerContext['call'];
+  };
+  callerPlugin: string;
+  callerPluginV?: string;
+  socket: any;
+  logger: LoggerInstance;
+  cache: any;
+  service: Service;
+  action: {
+    name: string;
+    [key: string]: any;
+  };
+  __leemonsDeploymentManagerCall: MoleculerContext['call'];
+  __leemonsDeploymentManagerEmit: MoleculerContext['emit'];
+  prefixPN: (string?: string) => string;
+  prefixPNV: (string?: string) => string;
+  params?: P extends EventParams ? P : any;
+  meta: Meta<M> & {
+    transactionID?: string;
+    transactionExecutionId?: string;
+    debugTransaction?: boolean;
+    waitToRollbackFinishOnError?: boolean;
+  };
+  id: string;
+  event: EventSchema | null;
+  deploymentID?: string;
+  lrn?: string;
+}
+
+export type Context<
+  P = any,
+  M extends object = Record<string, never>,
+  L extends GenericObject = GenericObject,
+  Models extends Record<string, Model<any>> = Record<string, never>
+> = ExtendedContext<P, M, L, Models>;
+
+export type AnyContext = Context<any, Record<string, never>, GenericObject, Record<string, never>>;
+
+// Define the custom ActionHandler type
+export type ActionHandler<C = Context> = (ctx: C) => Promise<any> | any;
+
+export type ActionHookBefore<C = Context> = (ctx: C) => Promise<void> | void;
+export type ActionHookAfter<C = Context> = (ctx: C) => Promise<void> | void;
+export type ActionHookError<C = Context> = (ctx: C, err: Error) => Promise<void> | void;
+
+// Extend the existing ActionSchema interface
+export interface ActionSchema<C = Context> extends Omit<MoleculerActionSchema, 'handler'> {
+  handler?: ActionHandler<C>;
+}
+
+// Extend the ServiceActionsSchema to use CustomActionSchema
+export type ServiceActionsSchema<S = ServiceSettingSchema> = {
+  [key: string]: ActionSchema<S> | ActionHandler<S> | boolean;
+} & ThisType<Service<S>>;
+
+// Extend the ServiceSchema to use CustomServiceActionsSchema
+export interface ServiceSchema<S = ServiceSettingSchema>
+  extends Omit<MoleculerServiceSchema<S>, 'actions'> {
+  actions?: ServiceActionsSchema<S>;
+  hooks?: {
+    before?: {
+      [key: string]: ActionHookBefore | ActionHookBefore[];
+    };
+    after?: {
+      [key: string]: ActionHookAfter | ActionHookAfter[];
+    };
+    error?: {
+      [key: string]: ActionHookError | ActionHookError[];
+    };
+  };
+}
+
+export enum EVENT_TYPES {
+  ONCE_PER_INSTALL = 'once-per-install',
+  ONCE = 'once',
+}
