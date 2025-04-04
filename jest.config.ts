5e5d8341117196426c65e47920cda3ffc7141205
Johan Sosa <sosa.johan@gmail.com>
2025-04-04 11:40:48 +0200

    chore: standardize string quotes and improve code formatting across multiple files
    - Update string quotes in various files to use double quotes for consistency, enhancing readability and maintaining a uniform coding style.
    - Refactor import statements and file patterns in configuration files for improved clarity and alignment with coding standards.
    - Add additional entries to the ignore list in biome.json and .cursorignore to prevent unnecessary indexing of build artifacts.
    - These changes contribute to a cleaner and more maintainable codebase within the Leemons ecosystem, ensuring better adherence to coding conventions.

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,11 @@
-import { JestConfigWithTsJest } from 'ts-jest';
+import { JestConfigWithTsJest } from "ts-jest";
 
 const jestConfig: JestConfigWithTsJest = {
   projects: [
     // '<rootDir>/packages/*/jest.config.js',
-    '<rootDir>/plugins/*/*/jest.config.js',
+    "<rootDir>/plugins/*/*/jest.config.js",
     // '<rootDir>/private-plugins/*/*/jest.config.js',
-    '<rootDir>/private-plugins/*/*/jest.config.ts',
+    "<rootDir>/private-plugins/*/*/jest.config.ts",
   ],
 };
 
